e1436f6300173b2e4d3e349c825463a868531613
Johannes Holke <holke@ins.uni-bonn.de>
2015-06-16 12:51:56 +0200

    example adapt

--- example/basic/basic.c
+++ example/basic/basic.c
@@ -27,6 +27,12 @@
 #include <t8_forest.h>
 
 static int
+t8_basic_adapt (t8_forest_t forest, t8_topidx_t which_tree,
+                t8_eclass_scheme_t * ts, t8_element_t * elements[]) {
+  return 0;
+}
+
+static int
 t8_basic_refine (t8_forest_t forest, t8_topidx_t which_tree,
                  t8_eclass_scheme_t * ts, t8_element_t * element)
 {
@@ -37,20 +43,24 @@ static void
 t8_basic_refine_test ()
 {
   t8_forest_t         forest;
-  t8_forest_t         forest_refine;
 
   t8_forest_init (&forest);
-  t8_forest_init (&forest_refine);
+  t8_forest_t         forest_coarsen;
+  t8_forest_t         forest_adapt;
+
+  t8_forest_init (&forest);
+  t8_forest_init (&forest_adapt);
+
 
   t8_forest_set_cmesh (forest, t8_cmesh_new_quad (sc_MPI_COMM_WORLD, 0));
   t8_forest_set_scheme (forest, t8_scheme_new_default ());
   t8_forest_set_level (forest, 1);
   t8_forest_commit (forest);
 
-  t8_forest_set_adapt (forest_refine, forest);
+  t8_forest_set_adapt_temp (forest_adapt, forest, t8_basic_adapt, 0);
+  t8_forest_commit (forest_adapt);
 
-  t8_forest_commit (forest_refine);
-  t8_forest_unref (&forest_refine);
+  t8_forest_unref (&forest_adapt);
 }
 
 static void
