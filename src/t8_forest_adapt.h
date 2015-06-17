1aa6f244db4cda8cf7e09a7cc779f0f1b733b5a9
Johannes Holke <holke@ins.uni-bonn.de>
2015-06-17 16:03:29 +0200

    define replace callback

--- src/t8_forest_adapt.h
+++ src/t8_forest_adapt.h
@@ -32,4 +32,33 @@
 #include <t8.h>
 #include <t8_forest.h>
 
+/* TODO: There is no user_data yet */
+/** Callback function prototype to replace one set of elements with another.
+ *
+ * This is used by the adapt routine when the elements of an existing, valid
+ * forest are changed.  The callback allows the user to make changes to newly
+ * initialized elements before the elements that they replace are destroyed.
+ *
+ * \param [in] forest      the forest
+ * \param [in] which_tree  the tree containing \a outgoing and \a incoming
+ * \param [in] ts          the eclass scheme of the tree
+ * \param [in] num_outgoing The number of outgoing elements.
+ * \param [in] outgoing     The outgoing elements: after the callback, the
+ *                          user_data will be destroyed. (at the current state there is no user data)
+ * \param [in] num_incoming The number of incoming elements.
+ * \param [in,out] incoming The incoming elements: prior to the callback,
+ *                          the user_data is allocated, and the forest_init_t callback,
+ *                          if it has been provided, will be called.
+ *
+ * If an element is being refined, num_outgoing will be 1 and num_incoming will
+ * be the number of children, and vice versa if a family is being coarsened.
+ */
+typedef void        (*t8_forest_replace_t) (t8_forest_t * forest,
+                                            t8_topidx_t which_tree,
+                                            t8_eclass_scheme_t * ts,
+                                            int num_outgoing,
+                                            t8_element_t * outgoing[],
+                                            int num_incoming,
+                                            t8_element_t * incoming[]);
+
 #endif /* !T8_FOREST_ADAPT_H! */
