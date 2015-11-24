c155f8bf438a29fe70adf40b6f19ee1cbc00d88b
Johannes Holke <holke@ins.uni-bonn.de>
2015-11-24 15:02:58 +0100

    first complete version of stash interface

--- src/t8_cmesh/t8_cmesh_stash.c
+++ src/t8_cmesh/t8_cmesh_stash.c
@@ -24,7 +24,6 @@
  * We define the data structures and routines for temporary storage before commit
  */
 
-
 #include <t8.h>
 #include <t8_eclass.h>
 #include <t8_cmesh/t8_cmesh_stash.h>
@@ -32,7 +31,7 @@
 void
 t8_stash_init (t8_stash_t * pstash)
 {
-  t8_stash_t        stash;
+  t8_stash_t          stash;
   T8_ASSERT (pstash != NULL);
 
   stash = *pstash = T8_ALLOC (t8_stash_struct_t, 1);
@@ -41,19 +40,22 @@ t8_stash_init (t8_stash_t * pstash)
   sc_array_init (&stash->joinfaces, sizeof (t8_stash_joinface_struct_t));
 }
 
-void              t8_stash_destroy (t8_stash_t * pstash)
+void
+t8_stash_destroy (t8_stash_t * pstash)
 {
-  t8_stash_t        stash;
+  t8_stash_t          stash;
   t8_stash_attribute_struct_t *attr;
-  size_t            attr_count;
+  size_t              attr_count;
 
   T8_ASSERT (pstash != NULL);
   stash = *pstash;
   sc_array_reset (&stash->classes);
   sc_array_reset (&stash->joinfaces);
-  for (attr_count = 0; attr_count < stash->attributes.elem_count;attr_count++) {
-    attr = (t8_stash_attribute_struct_t *) sc_array_index (&stash->attributes,
-                                                           attr_count);
+  for (attr_count = 0; attr_count < stash->attributes.elem_count;
+       attr_count++) {
+    attr =
+      (t8_stash_attribute_struct_t *) sc_array_index (&stash->attributes,
+                                                      attr_count);
     if (attr->is_owned) {
       T8_FREE (attr->attr_data);
     }
@@ -66,10 +68,10 @@ void              t8_stash_destroy (t8_stash_t * pstash)
 void
 t8_stash_add_class (t8_stash_t stash, t8_gloidx_t id, t8_eclass_t eclass)
 {
-  t8_stash_class_struct_t    *sclass;
+  t8_stash_class_struct_t *sclass;
 
   T8_ASSERT (stash != NULL);
-  sclass = (t8_stash_class_struct_t*) sc_array_push(&stash->classes);
+  sclass = (t8_stash_class_struct_t *) sc_array_push (&stash->classes);
   sclass->eclass = eclass;
   sclass->id = id;
 }
@@ -78,7 +80,7 @@ void
 t8_stash_add_facejoin (t8_stash_t stash, t8_gloidx_t id1, t8_gloidx_t id2,
                        int face1, int face2, int orientation)
 {
-  t8_stash_joinface_struct_t   *sjoin;
+  t8_stash_joinface_struct_t *sjoin;
 
   T8_ASSERT (stash != NULL);
   sjoin = sc_array_push (&stash->joinfaces);
@@ -91,9 +93,9 @@ t8_stash_add_facejoin (t8_stash_t stash, t8_gloidx_t id1, t8_gloidx_t id2,
 
 void
 t8_stash_add_attribute (t8_stash_t stash, t8_gloidx_t id, size_t size,
-                        void * attr, int copy)
+                        void *attr, int copy)
 {
-  t8_stash_attribute_struct_t      *sattr;
+  t8_stash_attribute_struct_t *sattr;
 
   T8_ASSERT (stash != NULL);
   sattr = sc_array_push (&stash->attributes);
@@ -104,4 +106,47 @@ t8_stash_add_attribute (t8_stash_t stash, t8_gloidx_t id, size_t size,
   if (!copy) {
     memcpy (sattr->attr_data, attr, size);
   }
+}
+
+size_t
+t8_stash_get_attribute_size (t8_stash_t stash, size_t index)
+{
+  return ((t8_stash_attribute_struct_t *)
+          sc_array_index (&stash->attributes, index))->attr_size;
+}
+
+void               *
+t8_stash_get_attribute (t8_stash_t stash, size_t index)
+{
+  return ((t8_stash_attribute_struct_t *)
+          sc_array_index (&stash->attributes, index))->attr_data;
+}
+
+t8_gloidx_t
+t8_stash_get_attribute_tree_id (t8_stash_t stash, size_t index)
+{
+  return ((t8_stash_attribute_struct_t *)
+          sc_array_index (&stash->attributes, index))->id;
+}
+
+int
+t8_stash_attribute_is_owned (t8_stash_t stash, size_t index)
+{
+  return ((t8_stash_attribute_struct_t *)
+          sc_array_index (&stash->attributes, index))->is_owned;
+}
+
+static int
+t8_stash_attribute_compare (const void *v1, const void *v2)
+{
+  t8_stash_attribute_struct_t *A1 = (t8_stash_attribute_struct_t *) v1;
+  t8_stash_attribute_struct_t *A2 = (t8_stash_attribute_struct_t *) v2;
+
+  return A1->id < A2->id ? -1 : A1->id > A2->id;
+}
+
+void
+t8_stash_attribute_sort (t8_stash_t stash)
+{
+  sc_array_sort (&stash->attributes, t8_stash_attribute_compare);
 }
--- src/t8_cmesh/t8_cmesh_stash.h
+++ src/t8_cmesh/t8_cmesh_stash.h
@@ -30,51 +30,65 @@
 #include <t8.h>
 #include <t8_eclass.h>
 
-typedef struct t8_stash * t8_stash_t;
+typedef struct t8_stash *t8_stash_t;
 
 typedef struct t8_stash_class
 {
-  t8_gloidx_t       id;
-  t8_eclass_t       eclass;
+  t8_gloidx_t         id;
+  t8_eclass_t         eclass;
 } t8_stash_class_struct_t;
 
 typedef struct t8_stash_joinface
 {
-  t8_gloidx_t       id1, id2;
-  int               face1, face2;
-  int               orientation;
+  t8_gloidx_t         id1, id2;
+  int                 face1, face2;
+  int                 orientation;
 } t8_stash_joinface_struct_t;
 
 typedef struct t8_stash_attribute
 {
-  t8_gloidx_t       id;
-  size_t            attr_size;
-  void             *attr_data;
-  int               is_owned;
+  t8_gloidx_t         id;
+  size_t              attr_size;
+  void               *attr_data;
+  int                 is_owned;
 } t8_stash_attribute_struct_t;
 
 typedef struct t8_stash
 {
-  sc_array_t      classes;
-  sc_array_t      joinfaces;
-  sc_array_t      attributes;
+  sc_array_t          classes;
+  sc_array_t          joinfaces;
+  sc_array_t          attributes;
 } t8_stash_struct_t;
 
 T8_EXTERN_C_BEGIN ();
 
-void              t8_stash_init (t8_stash_t * pstash);
+void                t8_stash_init (t8_stash_t * pstash);
 
-void              t8_stash_destroy (t8_stash_t * pstash);
+void                t8_stash_destroy (t8_stash_t * pstash);
 
-void              t8_stash_add_class (t8_stash_t stash, t8_gloidx_t id,
-                                      t8_eclass_t eclass);
+void                t8_stash_add_class (t8_stash_t stash, t8_gloidx_t id,
+                                        t8_eclass_t eclass);
 
-void              t8_stash_add_facejoin (t8_stash_t stash, t8_gloidx_t id1,
-                                         t8_gloidx_t id2, int face1, int face2,
-                                         int orientation);
+void                t8_stash_add_facejoin (t8_stash_t stash, t8_gloidx_t id1,
+                                           t8_gloidx_t id2, int face1,
+                                           int face2, int orientation);
 
-void              t8_stash_add_attribute (t8_stash_t stash, t8_gloidx_t id,
-                                          size_t size, void * attr, int copy);
+void                t8_stash_add_attribute (t8_stash_t stash, t8_gloidx_t id,
+                                            size_t size, void *attr,
+                                            int copy);
+
+size_t              t8_stash_get_attribute_size (t8_stash_t stash,
+                                                 size_t index);
+
+void               *t8_stash_get_attribute (t8_stash_t stash, size_t index);
+
+t8_gloidx_t         t8_stash_get_attribute_tree_id (t8_stash_t stash,
+                                                    size_t index);
+
+int                 t8_stash_attribute_is_owned (t8_stash_t stash,
+                                                 size_t index);
+
+void                t8_stash_attribute_sort (t8_stash_t stash);
 
 T8_EXTERN_C_END ();
 
