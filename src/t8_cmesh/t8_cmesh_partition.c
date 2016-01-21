1db38787d5f5a2dd3d7c0d4cf85fca8ae4e21671
Johannes Holke <holke@ins.uni-bonn.de>
2016-01-21 16:39:04 +0100

    first work on partition algorithm

--- src/t8_cmesh/t8_cmesh_partition.c
+++ src/t8_cmesh/t8_cmesh_partition.c
@@ -25,16 +25,50 @@
  * TODO: document this file
  */
 
+#include <t8_shmem.h>
 #include <t8_cmesh.h>
 #include "t8_cmesh_types.h"
 #include "t8_cmesh_trees.h"
 #include "t8_cmesh_partition.h"
 
+/* Given the trees_per_proc array compute the first local tree */
+static void
+t8_cmesh_scan_trees (t8_cmesh_t cmesh)
+{
+  int                 iproc;
+  int                 last_tree_shared;
+
+  T8_ASSERT (cmesh != NULL);
+  T8_ASSERT (cmesh->tree_per_proc != NULL);
+
+  cmesh->first_tree = 0;
+  for (iproc = 0; iproc < cmesh->mpirank; iproc++) {
+    last_tree_shared = cmesh->tree_per_proc[iproc] < 0;
+    cmesh->first_tree += last_tree_shared ? -cmesh->tree_per_proc[iproc] - 1
+      : cmesh->tree_per_proc[iproc];
+  }
+  /* TODO :An alternative is to use MPI_Scan with a self-defined operation.
+   *       This could be faster since MPI_Scan is O(log(P)) instead of O(P)
+   *       but it introduces a new synchronization point. */
+
+}
+
+static void
+t8_cmesh_partition_given (t8_cmesh_t cmesh, const struct t8_cmesh *cmesh_from,
+                          t8_locidx_t * trees_per_proc)
+{
+  T8_ASSERT (cmesh != NULL);
+  T8_ASSERT (!cmesh->committed);
+  T8_ASSERT (cmesh->set_partitioned);
+  T8_ASSERT (cmesh_from != NULL);
+  T8_ASSERT (cmesh_from->committed);
+}
+
 void
 t8_cmesh_partition (t8_cmesh_t cmesh)
 {
-  t8_cmesh_t        cmesh_from;
-  t8_gloidx_t       last_tree;
+  t8_cmesh_t          cmesh_from;
+  t8_gloidx_t         last_tree;
 
   T8_ASSERT (cmesh != NULL);
   T8_ASSERT (!cmesh->committed);
@@ -42,14 +76,49 @@ t8_cmesh_partition (t8_cmesh_t cmesh)
   T8_ASSERT (cmesh->set_from != NULL);
   T8_ASSERT (cmesh->set_from->committed);
 
-  cmesh_from = cmesh->set_from;
+  cmesh_from = (t8_cmesh_t) cmesh->set_from;
+  /**********************************************/
+  /*      Compute local number of trees         */
+  /*         and trees per proc array           */
+  /**********************************************/
   if (cmesh->set_level >= 0) {
     /* Compute first and last tree index */
+    T8_ASSERT (cmesh->tree_per_proc == NULL);
     t8_cmesh_uniform_bounds (cmesh_from, cmesh->set_level, &cmesh->first_tree,
-                             NULL, &last_tree, NULL, &cmesh->last_tree_shared);
-    cmesh->num_local_trees = last_tree - cmesh->first_tree + 1;
+                             NULL, &last_tree, NULL,
+                             &cmesh->last_tree_shared);
+    cmesh->num_local_trees = last_tree - cmesh->first_tree;
+    /* To compute the tree_per_proc correctly we have to invert the sign on the
+     * local tree count if last tree is shared */
+    if (cmesh->last_tree_shared) {
+      cmesh->num_local_trees = -cmesh->num_local_trees;
+    }
+    /* allocate and fill tree_per_proc array with number of trees per process */
+    /* We have to be careful with shared memory in the case where cmesh comm is
+     * duplicated, since we have to use the same communicator for allocating and freeing memory.
+     * Thus this function must only be called after cmesh communicator was duplicated. */
+    T8_ASSERT (cmesh->mpisize > 0);
+    cmesh->tree_per_proc = SC_SHMEM_ALLOC (t8_locidx_t, cmesh->mpisize,
+                                           cmesh->mpicomm);
+    sc_shmem_allgather (&cmesh->num_local_trees, 1, T8_MPI_LOCIDX,
+                        cmesh->tree_per_proc, 1, T8_MPI_LOCIDX,
+                        cmesh->mpicomm);
+    /* tree_to_proc was computed, reinvert the sign */
+    if (cmesh->last_tree_shared) {
+      T8_ASSERT (cmesh->num_local_trees < 0);
+      cmesh->num_local_trees = -cmesh->num_local_trees;
+    }
   }
   else {
     T8_ASSERT (cmesh->tree_per_proc != NULL);
+    cmesh->last_tree_shared = cmesh->tree_per_proc[cmesh->mpirank] < 0;
+    cmesh->num_local_trees = cmesh->last_tree_shared ?
+      -cmesh->tree_per_proc[cmesh->mpirank] :
+      cmesh->tree_per_proc[cmesh->mpirank];
+    t8_cmesh_scan_trees (cmesh);
   }
+  /***************************************************/
+  /*        Done with local num and trees_per_proc   */
+  /***************************************************/
+  t8_cmesh_partition_given (cmesh, cmesh->set_from, cmesh->tree_per_proc);
 }
