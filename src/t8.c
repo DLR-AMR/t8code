006bf74045a5aab07d5ca431298466570a704737
Johannes Holke <holke@ins.uni-bonn.de>
2016-01-22 18:44:30 +0100

    t8_sc_array with locidx

--- src/t8.c
+++ src/t8.c
@@ -33,6 +33,15 @@ t8_sc_array_index_topidx (sc_array_t * array, t8_topidx_t it)
   return array->array + array->elem_size * (size_t) it;
 }
 
+void        *
+t8_sc_array_index_locidx (sc_array_t * array, t8_locidx_t it)
+{
+  P4EST_ASSERT (it >= 0 && (size_t) it < array->elem_count);
+
+  return array->array + array->elem_size * (size_t) it;
+}
+
+
 int
 t8_get_package_id (void)
 {
