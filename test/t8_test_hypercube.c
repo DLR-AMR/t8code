6b5c4f69d23184778ecafdbc673a941edc54d959
Johannes Holke <holke@ins.uni-bonn.de>
2016-04-28 15:49:04 +0200

    add hypercube test

---
+++ test/t8_test_hypercube.c
@@ -0,0 +1,70 @@
+/*
+  This file is part of t8code.
+  t8code is a C library to manage a collection (a forest) of multiple
+  connected adaptive space-trees of general element types in parallel.
+
+  Copyright (C) 2010 The University of Texas System
+  Written by Carsten Burstedde, Lucas C. Wilcox, and Tobin Isaac
+
+  t8code is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  t8code is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with t8code; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#include <t8_cmesh.h>
+#include "t8_cmesh/t8_cmesh_trees.h"
+
+static void
+test_hypercube (sc_MPI_Comm mpic)
+{
+  int                 eci;
+  int                 retval;
+  int                 partition;
+  t8_cmesh_t          cmesh;
+
+  for (eci = T8_ECLASS_ZERO; eci < T8_ECLASS_COUNT; ++eci) {
+    for (partition = 0; partition < 2; partition++) {
+      t8_cmesh_init (&cmesh);
+      cmesh = t8_cmesh_new_hypercube (eci, mpic, 0, 0, partition);
+      retval = t8_cmesh_is_committed (cmesh);
+      SC_CHECK_ABORT (retval == 0, "Cmesh commit failed.");
+      retval = t8_cmesh_trees_is_face_consistend (cmesh, cmesh->trees);
+      SC_CHECK_ABORT (retval == 0, "Cmesh face consistency failed.");
+      t8_cmesh_destroy (&cmesh);
+    }
+  }
+}
+
+int
+main (int argc, char **argv)
+{
+  int                 mpiret;
+  sc_MPI_Comm         mpic;
+
+  mpiret = sc_MPI_Init (&argc, &argv);
+  SC_CHECK_MPI (mpiret);
+
+  mpic = sc_MPI_COMM_WORLD;
+  sc_init (mpic, 1, 1, NULL, SC_LP_PRODUCTION);
+  p4est_init (NULL, SC_LP_ESSENTIAL);
+  t8_init (SC_LP_DEFAULT);
+
+  test_hypercube (mpic);
+
+  sc_finalize ();
+
+  mpiret = sc_MPI_Finalize ();
+  SC_CHECK_MPI (mpiret);
+
+  return 0;
+}
