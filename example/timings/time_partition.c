bb064b015d95e83badf1dbd9b4c9d21fd7d70e98
Johannes Holke <holke@ins.uni-bonn.de>
2016-08-16 16:13:25 +0200

    start timing of cmesh_partition

---
+++ example/timings/time_partition.c
@@ -0,0 +1,115 @@
+/*
+  This file is part of t8code.
+  t8code is a C library to manage a collection (a forest) of multiple
+  connected adaptive space-trees of general element types in parallel.
+
+  Copyright (C) 2015 the developers
+
+  t8code is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  t8code is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with t8code; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#include <sc_refcount.h>
+#include <sc_flops.h>
+#include <sc_statistics.h>
+#include <sc_options.h>
+#include <p4est_connectivity.h>
+#include <t8_cmesh.h>
+
+void t8_time_cmesh_partition_brick (int x, int y, sc_MPI_Comm comm)
+{
+  t8_cmesh_t          cmesh;
+  t8_cmesh_t          cmesh_partition;
+  p4est_connectivity_t *p4est_conn;
+
+  sc_flopinfo_t       fi, snapshot;
+  sc_statinfo_t       stats[1];
+
+  /* Create p4est brick connectivity x times y */
+  p4est_conn = p4est_connectivity_new_brick (x, y, 0, 0);
+  /* Build t8 cmesh from connectivity. The cmesh is partitioned according
+   * to uniform level 0 refinement */
+  cmesh = t8_cmesh_new_from_p4est (p4est_conn, comm, 0, 1);
+  /* We do not need the p4est connectivity anymore, so we destroy it */
+  p4est_connectivity_destroy (p4est_conn);
+  /* Set up cmesh_partition to be a repartition of cmesh. */
+  t8_cmesh_init (&cmesh_partition);
+  t8_cmesh_set_derive (cmesh_partition, cmesh);
+  /* The new cmesh is partitioned according to a uniform level 1 refinement */
+  t8_cmesh_set_partition_uniform (cmesh_partition, 1);
+
+  /* Start timer */
+  sc_flops_start (&fi);
+  sc_flops_snap (&fi, &snapshot);
+  /* commit (= partition) the second cmesh */
+  t8_cmesh_commit (cmesh_partition, comm);
+  /* measure passed time */
+  sc_flops_shot (&fi, &snapshot);
+  sc_stats_set1 (&stats[0], snapshot.iwtime, "Partition");
+  /* print stats */
+  sc_stats_compute (sc_MPI_COMM_WORLD, 1, stats);
+  sc_stats_print (t8_get_package_id (), SC_LP_STATISTICS, 1, stats, 1, 1);
+  /* memory clean-up */
+  t8_cmesh_destroy (&cmesh);
+  t8_cmesh_destroy (&cmesh_partition);
+}
+
+int main (int argc, char *argv[])
+{
+  int                 mpiret, mpisize;
+  int                 first_argc;
+  int                 x_dim, y_dim;
+  int                 help = 0;
+  sc_options_t       *opt;
+
+  /* Initialize MPI, sc, p4est and t8code */
+  mpiret = sc_MPI_Init (&argc, &argv);
+  SC_CHECK_MPI (mpiret);
+
+  sc_init (sc_MPI_COMM_WORLD, 1, 1, NULL, SC_LP_ESSENTIAL);
+  p4est_init (NULL, SC_LP_ESSENTIAL);
+  t8_init (SC_LP_DEFAULT);
+
+  /* Setup for command line options */
+  opt = sc_options_new (argv[0]);
+
+  sc_options_add_int (opt, 'x', "x-dim", &x_dim, 1,
+                      "Number of mesh cells in x direction");
+  sc_options_add_int (opt, 'y', "y-dim", &y_dim, 1,
+                      "Number of mesh cells in y direction");
+  sc_options_add_switch (opt, 'h', "help", &help, "Display a short help message.");
+
+  /* parese command line options */
+  first_argc = sc_options_parse (t8_get_package_id (), SC_LP_DEFAULT,
+                                 opt, argc, argv);
+  /* check for wrong usae of arguments */
+  if (first_argc < 0 || first_argc != argc
+      || x_dim <= 0 || y_dim <= 0) {
+    sc_options_print_usage (t8_get_package_id (), SC_LP_ERROR, opt, NULL);
+    return 1;
+  }
+  if (help) {
+    /* Display help message */
+    sc_options_print_usage (t8_get_package_id (), SC_LP_ERROR, opt, NULL);
+  }
+  else {
+    /* Execute this part of the code if all options are correctly set */
+    t8_debugf ("Starting with x-dim = %i, y-dim = %i\n", x_dim, y_dim);
+    t8_time_cmesh_partition_brick (x_dim, y_dim, sc_MPI_COMM_WORLD);
+  }
+
+  sc_options_destroy (opt);
+  sc_finalize ();
+  return 0;
+}
