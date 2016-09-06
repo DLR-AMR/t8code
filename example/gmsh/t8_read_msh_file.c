91955983d9ebf7c5fcb558857184182b8a6abee5
Johannes Holke <holke@ins.uni-bonn.de>
2016-09-06 19:56:13 +0200

    use reading on one process in gmsh example

--- example/gmsh/t8_read_msh_file.c
+++ example/gmsh/t8_read_msh_file.c
@@ -27,47 +27,78 @@
 #include <t8_cmesh_readmshfile.h>
 #include <t8_cmesh_vtk.h>
 
-void
-t8_read_msh_file_build_cmesh (const char *prefix, int do_partition, int dim)
+static void
+t8_read_msh_file_vtk (t8_cmesh_t cmesh, const char *prefix)
 {
-  t8_cmesh_t          cmesh;
-  char                fileprefix[BUFSIZ];
   int                 mpirank, mpiret;
+  char                fileprefix[BUFSIZ];
 
   mpiret = sc_MPI_Comm_rank (sc_MPI_COMM_WORLD, &mpirank);
   SC_CHECK_MPI (mpiret);
 
+  snprintf (fileprefix, BUFSIZ, "%s_t8_msh_%04d", prefix, mpirank);
+  if (!t8_cmesh_vtk_write_file (cmesh, fileprefix, 1.)) {
+    t8_debugf ("Wrote to file %s\n", fileprefix);
+  }
+  else {
+    t8_debugf ("Error in writing cmesh vtk\n");
+  }
+}
+
+static              t8_cmesh_t
+t8_read_msh_partition (t8_cmesh_t cmesh, const char *prefix)
+{
+  t8_cmesh_t          p_mesh;
+  char                vtk_prefix[BUFSIZ];
+
+  t8_cmesh_init (&p_mesh);
+  t8_cmesh_set_derive (p_mesh, cmesh);
+  t8_cmesh_set_partition_uniform (p_mesh, 0);
+  t8_cmesh_commit (p_mesh, sc_MPI_COMM_WORLD);
+  t8_cmesh_unref (&cmesh);
+  snprintf (vtk_prefix, BUFSIZ, "%s_partition", prefix);
+  t8_read_msh_file_vtk (p_mesh, vtk_prefix);
+  return p_mesh;
+}
+
+static t8_cmesh_t
+t8_read_msh_file_build_cmesh (const char *prefix, int do_partition, int dim,
+                              int master)
+{
+  t8_cmesh_t          cmesh;
+  int                 partitioned_read;
+
+  /* If the master argument is positive, then we read the cmesh
+   * only on the master rank and is directly partitioned. */
+  partitioned_read = master >= 0;
   cmesh =
-    t8_cmesh_from_msh_file ((char *) prefix, do_partition,
-                            sc_MPI_COMM_WORLD, dim);
+    t8_cmesh_from_msh_file ((char *) prefix, partitioned_read,
+                            sc_MPI_COMM_WORLD, dim, master);
   if (cmesh != NULL) {
     t8_debugf ("Succesfully constructed cmesh from %s.msh file.\n", prefix);
     t8_debugf ("cmesh is of dimension %i and has %lli elements.\n",
                dim, (long long) t8_cmesh_get_num_trees (cmesh));
-    snprintf (fileprefix, BUFSIZ, "%s_t8_msh_%04d", prefix, mpirank);
-    if (!t8_cmesh_vtk_write_file (cmesh, fileprefix, 1.)) {
-      t8_debugf ("Wrote to file %s\n", fileprefix);
+    t8_read_msh_file_vtk (cmesh, prefix);
+    if (do_partition) {
+      cmesh = t8_read_msh_partition (cmesh, prefix);
     }
-    else {
-      t8_debugf ("Error in writing cmesh vtk\n");
-    }
-
-    t8_cmesh_destroy (&cmesh);
+    return cmesh;
   }
   else {
     t8_debugf ("An error occured while reading %s.msh file.\n", prefix);
+    return NULL;
   }
-  fflush (stdout);
 }
 
 int
 main (int argc, char *argv[])
 {
-  int                 mpiret, parsed, partition, dim;
+  int                 mpiret, parsed, partition, dim, master, mpisize;
   sc_options_t       *opt;
   const char         *prefix;
   char                usage[BUFSIZ];
   char                help[BUFSIZ];
+  t8_cmesh_t          cmesh;
 
   snprintf (usage, BUFSIZ, "Usage:\t%s <OPTIONS> <ARGUMENTS>",
             basename (argv[0]));
@@ -87,20 +118,28 @@ main (int argc, char *argv[])
   p4est_init (NULL, SC_LP_ESSENTIAL);
   t8_init (SC_LP_DEFAULT);
 
+  mpiret = sc_MPI_Comm_size (sc_MPI_COMM_WORLD, &mpisize);
+  SC_CHECK_MPI (mpiret);
+
   opt = sc_options_new (argv[0]);
   sc_options_add_string (opt, 'f', "prefix", &prefix, "", "The prefix of the"
                          "tetgen files.");
-  sc_options_add_bool (opt, 'p', "Partition", &partition, 0, "If true"
-                       "the generated cmesh is partitioned.");
+  sc_options_add_bool (opt, 'p', "Partition", &partition, 0, "If true "
+                       "the generated cmesh is repartitioned uniformly.");
   sc_options_add_int (opt, 'd', "dim", &dim, 2, "The dimension of the mesh");
+  sc_options_add_int (opt, 'm', "master", &master, -1, "If specified, the "
+                      "mesh is partitioned and all elements reside on process with "
+                      "rank master.");
   parsed =
     sc_options_parse (t8_get_package_id (), SC_LP_ERROR, opt, argc, argv);
-  if (parsed < 0 || strcmp (prefix, "") == 0 || 0 > dim || dim > 3) {
-    fprintf (stderr, "%s", help);
+  if (parsed < 0 || strcmp (prefix, "") == 0 || 0 > dim || dim > 3
+      || master < -1 || master >= mpisize) {
+    t8_global_errorf ("%s", help);
     return 1;
   }
   else {
-    t8_read_msh_file_build_cmesh (prefix, partition, dim);
+    cmesh = t8_read_msh_file_build_cmesh (prefix, partition, dim, master);
+    t8_cmesh_destroy (&cmesh);
     sc_options_print_summary (t8_get_package_id (), SC_LP_PRODUCTION, opt);
   }
 
