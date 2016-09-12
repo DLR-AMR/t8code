71c5d2f60825a83e076f052ad25a89225ea57aa7
Johannes Holke <holke@ins.uni-bonn.de>
2016-09-13 00:27:20 +0200

    new element_anchor function

--- src/t8_element.h
+++ src/t8_element.h
@@ -110,6 +110,9 @@ typedef void        (*t8_element_successor_t) (const t8_element_t * t,
 typedef void        (*t8_element_new_t) (void *ts_context,
                                          int length, t8_element_t ** elem);
 
+typedef void        (*t8_element_anchor_t) (const t8_element_t elem,
+                                            int anchor[3]);
+
 /** Deallocate space for the codimension-one boundary elements. */
 typedef void        (*t8_element_destroy_t) (void *ts_context,
                                              int length,
@@ -143,6 +146,7 @@ struct t8_eclass_scheme
   t8_element_linear_id_t elem_set_linear_id; /**< Initialize an element from a given linear id. */
   t8_element_successor_t elem_successor; /**< Compute the successor of a given element */
 
+  t8_element_anchor_t elem_anchor; /**< Compute the anchor node of a given element */
   /* these element routines have a context for memory allocation */
   t8_element_new_t    elem_new;         /**< Allocate space for one or more elements. */
   t8_element_destroy_t elem_destroy;    /**< Deallocate space for one or more elements. */
@@ -380,6 +384,10 @@ void                t8_element_set_linear_id (t8_eclass_scheme_t * ts,
 void                t8_element_successor (t8_eclass_scheme_t * ts,
                                           const t8_element_t * elem1,
                                           t8_element_t * elem2, int level);
+
+void                t8_element_anchor (t8_eclass_scheme_t * ts,
+                                       const t8_element_t * elem,
+                                       int anchor[3]);
 
 /** Allocate memory for an array of elements of a given class.
  * \param [in] ts       The virtual table for this element class.
