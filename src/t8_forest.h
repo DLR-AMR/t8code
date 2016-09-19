46cadc81e6ab358f75fe7fbd183a38548ec062af
Johannes Holke <holke@ins.uni-bonn.de>
2016-09-19 14:28:46 +0200

    indent

--- src/t8_forest.h
+++ src/t8_forest.h
@@ -220,7 +220,6 @@ void                t8_forest_comm_global_num_elements (t8_forest_t forest);
  */
 void                t8_forest_commit (t8_forest_t forest);
 
-
 t8_locidx_t         t8_forest_get_num_element (t8_forest_t forest);
 
 /** Change the cmesh associated to a forest to a partitioned cmesh that
