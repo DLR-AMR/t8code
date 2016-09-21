170bf62b5271138f6165b718f62242f9039770dc
Johannes Holke <holke@ins.uni-bonn.de>
2016-09-21 19:21:10 +0200

    cmesh save example can save a gmsh mesh and load a mesh

--- example/cmesh/cmesh_load_save.c
+++ example/cmesh/cmesh_load_save.c
@@ -21,32 +21,68 @@
 */
 
 #include <t8_cmesh.h>
+#include <t8_cmesh_vtk.h>
+#include <t8_cmesh_readmshfile.h>
 
 /* TODO: rename this file to t8_something */
 
 static void
-t8_cmesh_save_cmesh ()
+t8_cmesh_save_cmesh (char *mshfile)
 {
   t8_cmesh_t          cmesh;
   char                filename[BUFSIZ];
   int                 ret, mpirank, mpiret;
 
-  cmesh = t8_cmesh_new_hypercube (T8_ECLASS_TRIANGLE, sc_MPI_COMM_WORLD,
-                                  0, 0, 1);
+  if (mshfile == NULL) {
+    cmesh = t8_cmesh_new_hypercube (T8_ECLASS_TET, sc_MPI_COMM_WORLD,
+                                    0, 0, 1);
+  }
+  else {
+    t8_cmesh_t          cmesh_partition;
+    cmesh = t8_cmesh_from_msh_file (mshfile, 1, sc_MPI_COMM_WORLD, 3, 0);
+    t8_cmesh_init (&cmesh_partition);
+    t8_cmesh_set_derive (cmesh_partition, cmesh);
+    t8_cmesh_set_partition_uniform (cmesh_partition, 1);
+    t8_cmesh_commit (cmesh_partition, sc_MPI_COMM_WORLD);
+    t8_cmesh_destroy (&cmesh);
+    cmesh = cmesh_partition;
+  }
   mpiret = sc_MPI_Comm_rank (sc_MPI_COMM_WORLD, &mpirank);
   SC_CHECK_MPI (mpiret);
-  snprintf (filename, BUFSIZ, "cmesh_hypercube_triangle_%04d.cmsh", mpirank);
+  snprintf (filename, BUFSIZ, "cmesh_saved_%04d.cmsh", mpirank);
   ret = t8_cmesh_save (cmesh, filename);
   if (ret == 0) {
     t8_errorf ("Error when writing to file\n");
   }
+  else {
+    t8_debugf ("Saved cmesh to %s\n", filename);
+  }
   t8_cmesh_destroy (&cmesh);
+}
+
+static void
+t8_cmesh_load_cmesh ()
+{
+  t8_cmesh_t          cmesh;
+  char                filename[BUFSIZ];
+  int                 mpirank, mpiret;
+
+  mpiret = sc_MPI_Comm_rank (sc_MPI_COMM_WORLD, &mpirank);
+  SC_CHECK_MPI (mpiret);
+  snprintf (filename, BUFSIZ, "cmesh_saved_%04d.cmsh", mpirank);
+  cmesh = t8_cmesh_load (filename, sc_MPI_COMM_WORLD);
+  if (cmesh != NULL) {
+    t8_debugf ("Successfully loaded cmesh from %s\n", filename);
+    t8_cmesh_vtk_write_file (cmesh, "cmesh_loaded", 1.0);
+    t8_cmesh_destroy (&cmesh);
+  }
 }
 
 int
 main (int argc, char **argv)
 {
   int                 mpiret;
+  char               *meshfile;
 
   mpiret = sc_MPI_Init (&argc, &argv);
   SC_CHECK_MPI (mpiret);
@@ -54,7 +90,14 @@ main (int argc, char **argv)
   sc_init (sc_MPI_COMM_WORLD, 1, 1, NULL, SC_LP_ESSENTIAL);
   t8_init (SC_LP_DEFAULT);
 
-  t8_cmesh_save_cmesh ();
+  if (argc == 2) {
+    meshfile = argv[1];
+  }
+  else {
+    meshfile = NULL;
+  }
+  t8_cmesh_save_cmesh (meshfile);
+  t8_cmesh_load_cmesh ();
 
   sc_finalize ();
 
