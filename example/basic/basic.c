0e885c300873f067b2d2d19dcb90494cb9bb6c0f
Johannes Holke <s6joholk@uni-bonn.de>
2017-03-07 09:55:59 +0100

    create example for cse talk

--- example/basic/basic.c
+++ example/basic/basic.c
@@ -53,7 +53,54 @@ t8_basic_adapt (t8_forest_t forest, t8_locidx_t which_tree,
   return 0;
 }
 
+static int
+t8_basic_adapt_cse (t8_forest_t forest, t8_locidx_t which_tree,
+                    t8_eclass_scheme_t * ts,
+                    int num_elements, t8_element_t * elements[])
+{
+  int                 level;
+  t8_gloidx_t         glo_tree;
+  t8_gloidx_t         refine_this_tree;
+
+  T8_ASSERT (num_elements == 1 || num_elements ==
+             t8_eclass_num_children[ts->eclass]);
+
+  /* refine a particular tree that was given as user data */
+  refine_this_tree = *(t8_gloidx_t *) t8_forest_get_user_data (forest);
+  /* The level of the element */
+  level = t8_element_level (ts, elements[0]);
+  /* Get the global id of the current tree */
+  glo_tree = t8_cmesh_get_global_id (t8_forest_get_cmesh (forest),
+                                     t8_forest_ltreeid_to_cmesh_ltreeid
+                                     (forest, which_tree));
+
+  if (num_elements > 1 && glo_tree != refine_this_tree) {
+    /* Do coarsen here */
+    if (level > 2)
+      return -1;
+  }
+  /* We refine one tree more than the others, since we want it to be shared by
+   * some procs */
+  if (glo_tree == refine_this_tree) {
+    t8_locidx_t         anchor[3];
+    int8_t              maxlevel = t8_element_maxlevel (ts);
+    t8_element_vertex_coords (ts, elements[0], 1, anchor);
+    if (anchor[0] > (1 << (maxlevel - level + 1))
+        && anchor[0] < t8_element_root_len (ts, elements[0])) {
+      return level < 4;
+    }
+    else
+      return level < 3;
+  }
+  else {
+    /* The rest has max level 2 */
+    if (level < 2)
+      return 1;
+  }
+  return 0;
+}
 #endif
+
 #if 1
 static void
 t8_basic_refine_test ()
@@ -79,6 +126,93 @@ t8_basic_refine_test ()
   t8_forest_unref (&forest_adapt);
 }
 #endif
+
+/* Generate a 1xn brick connectivity, create a forest with nontrivial
+ * refinement on it and print to vtk.
+ * We used this function to create plots for our talk at
+ * the SIAM CSE 2017 */
+static void
+t8_basic_for_cse_talk (int m_x)
+{
+  int                 level = 1;
+  t8_cmesh_t          cmesh, cmesh_partition;
+  t8_forest_t         forest, forest_adapt, forest_part;
+  p4est_connectivity_t *conn;
+  t8_gloidx_t         refine_this_tree;
+
+  /* Create m_x x 1 brick conn */
+  conn = p4est_connectivity_new_brick (m_x, 1, 0, 0);
+  /* build partitioned cmesh from connectivity */
+  cmesh = t8_cmesh_new_from_p4est (conn, sc_MPI_COMM_WORLD, 1);
+  p4est_connectivity_destroy (conn);
+  /* partition the cmesh according to initial uniform forest */
+  t8_cmesh_init (&cmesh_partition);
+  t8_cmesh_set_derive (cmesh_partition, cmesh);
+  t8_cmesh_set_partition_uniform (cmesh_partition, level);
+  t8_cmesh_commit (cmesh_partition, sc_MPI_COMM_WORLD);
+
+  /* Create a uniform forest */
+  t8_forest_init (&forest);
+  t8_forest_set_cmesh (forest, cmesh_partition, sc_MPI_COMM_WORLD);
+  t8_cmesh_unref (&cmesh_partition);
+  t8_cmesh_unref (&cmesh);
+  t8_forest_set_scheme (forest, t8_scheme_new_default ());
+  t8_forest_set_level (forest, level);
+  t8_forest_commit (forest);
+  t8_debugf ("[H] Created initial forest\n");
+
+  /* adapt the forest */
+  t8_forest_ref (forest);       /* We want to adapt it another time below and thus reference it here */
+  t8_forest_init (&forest_adapt);
+  refine_this_tree = 1;
+  t8_forest_set_user_data (forest_adapt, (void *) &refine_this_tree);
+  t8_forest_set_adapt (forest_adapt, forest, t8_basic_adapt_cse, NULL, 1);
+  t8_forest_commit (forest_adapt);
+
+  /* partition the coarse mesh according to the new forest partition */
+  t8_forest_partition_cmesh (forest_adapt, sc_MPI_COMM_WORLD, 0);
+
+  t8_debugf ("[H] Created adapted forest\n");
+  t8_forest_write_vtk (forest_adapt, "cse_forest_adapt");
+  /* partition forest */
+  t8_forest_init (&forest_part);
+  t8_forest_set_partition (forest_part, forest_adapt, 0);
+  t8_forest_commit (forest_part);
+
+  t8_debugf ("[H] Created partitioned forest\n");
+  /* partition the coarse mesh according to the new forest partition */
+  t8_forest_partition_cmesh (forest_part, sc_MPI_COMM_WORLD, 0);
+  t8_debugf ("[H] Partitioned coarse mesh\n");
+  t8_forest_write_vtk (forest_part, "cse_forest_part");
+  t8_cmesh_vtk_write_file (t8_forest_get_cmesh (forest_part),
+                           "cse_cmesh_part", 1.0);
+
+  /* Re-adapt the forest */
+  t8_forest_init (&forest_adapt);
+  refine_this_tree = 2;
+  t8_forest_set_user_data (forest_adapt, (void *) &refine_this_tree);
+  t8_forest_set_adapt (forest_adapt, forest, t8_basic_adapt_cse, NULL, 1);
+  t8_forest_commit (forest_adapt);
+
+  t8_debugf ("[H] Created adapted forest 2\n");
+  t8_forest_write_vtk (forest_adapt, "cse_forest_adapt2");
+  /* Re-partition the forest */
+  t8_forest_unref (&forest_part);
+
+  t8_forest_init (&forest_part);
+  t8_forest_set_partition (forest_part, forest_adapt, 0);
+  t8_forest_commit (forest_part);
+
+  t8_debugf ("[H] Created partitioned forest\n");
+  /* partition the coarse mesh according to the new forest partition */
+  t8_forest_partition_cmesh (forest_part, sc_MPI_COMM_WORLD, 0);
+  t8_debugf ("[H] Partitioned coarse mesh\n");
+  t8_forest_write_vtk (forest_part, "cse_forest_part2");
+  t8_cmesh_vtk_write_file (t8_forest_get_cmesh (forest_part),
+                           "cse_cmesh_part2", 1.0);
+
+  t8_forest_unref (&forest_part);
+}
 
 #if 0
 static void
@@ -356,11 +490,12 @@ main (int argc, char **argv)
   t8_basic_hypercube (T8_ECLASS_QUAD, 0, 1, 1);
   t8_basic ();
 #endif
+  t8_basic_for_cse_talk (3);
+
   t8_basic_hypercube (T8_ECLASS_QUAD, 2, 1, 0);
   t8_basic_refine_test ();
-#if 0
   t8_global_productionf ("Testing hypercube cmesh.\n");
-
+#if 0
   for (eclass = T8_ECLASS_QUAD; eclass < T8_ECLASS_COUNT; eclass++) {
     /* Construct the mesh on each process */
     t8_basic_hypercube ((t8_eclass_t) eclass, level, 0, 1);
