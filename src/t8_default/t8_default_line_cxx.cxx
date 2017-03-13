65b8c5c5e682e1981f65e49376bd9c3399cf3ffe
Johannes Holke <s6joholk@uni-bonn.de>
2017-03-13 20:26:49 +0100

    continue merge, adapt to c++ interface

--- src/t8_default/t8_default_line_cxx.cxx
+++ src/t8_default/t8_default_line_cxx.cxx
@@ -20,39 +20,43 @@
   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
-#include "t8_default_common.h"
-#include "t8_default_line.h"
+#include "t8_default_common_cxx.hxx"
+#include "t8_default_line_cxx.hxx"
 #include "t8_dline_bits.h"
 #include "t8_dline.h"
 
 typedef t8_dline_t  t8_default_line_t;
 
-static size_t
-t8_default_line_size (void)
+/* *INDENT-OFF* */
+size_t
+t8_default_scheme_line_c::t8_element_size (void)
+/* *INDENT-ON* */
 {
   return sizeof (t8_default_line_t);
 }
 
-static int
-t8_default_line_maxlevel (void)
+int
+t8_default_scheme_line_c::t8_element_maxlevel (void)
 {
   return T8_DLINE_MAXLEVEL;
 }
 
-static int
-t8_default_line_level (const t8_element_t * elem)
+int
+t8_default_scheme_line_c::t8_element_level (const t8_element_t * elem)
 {
   return t8_dline_get_level ((t8_dline_t *) elem);
 }
 
-static void
-t8_default_tline_copy (const t8_element_t * source, t8_element_t * dest)
+void
+t8_default_scheme_line_c::t8_element_copy (const t8_element_t * source,
+                                           t8_element_t * dest)
 {
   t8_dline_copy ((const t8_dline_t *) source, (t8_dline_t *) dest);
 }
 
-static void
-t8_default_line_parent (const t8_element_t * elem, t8_element_t * parent)
+void
+t8_default_scheme_line_c::t8_element_parent (const t8_element_t * elem,
+                                             t8_element_t * parent)
 {
   const t8_default_line_t *l = (const t8_default_line_t *) elem;
   t8_default_line_t  *p = (t8_default_line_t *) parent;
@@ -60,9 +64,9 @@ t8_default_line_parent (const t8_element_t * elem, t8_element_t * parent)
   t8_dline_parent (l, p);
 }
 
-static void
-t8_default_line_child (const t8_element_t * elem,
-                       int childid, t8_element_t * child)
+void
+t8_default_scheme_line_c::t8_element_child (const t8_element_t * elem,
+                                            int childid, t8_element_t * child)
 {
   const t8_default_line_t *l = (const t8_default_line_t *) elem;
   t8_default_line_t  *c = (t8_default_line_t *) child;
@@ -70,8 +74,9 @@ t8_default_line_child (const t8_element_t * elem,
   t8_dline_child (l, childid, c);
 }
 
-static void
-t8_default_line_set_linear_id (t8_element_t * elem, int level, uint64_t id)
+void
+t8_default_scheme_line_c::t8_element_set_linear_id (t8_element_t * elem,
+                                                    int level, uint64_t id)
 {
   T8_ASSERT (0 <= level && level <= T8_DLINE_MAXLEVEL);
   T8_ASSERT (0 <= id && id < ((u_int64_t) 1) << level);
@@ -79,9 +84,10 @@ t8_default_line_set_linear_id (t8_element_t * elem, int level, uint64_t id)
   t8_dline_init_linear_id ((t8_default_line_t *) elem, level, id);
 }
 
-static void
-t8_default_line_successor (const t8_element_t * elem1,
-                           t8_element_t * elem2, int level)
+void
+t8_default_scheme_line_c::t8_element_successor (const t8_element_t * elem1,
+                                                t8_element_t * elem2,
+                                                int level)
 {
   T8_ASSERT (0 <= level && level <= T8_DLINE_MAXLEVEL);
 
@@ -89,48 +95,28 @@ t8_default_line_successor (const t8_element_t * elem1,
                       (t8_default_line_t *) elem2, level);
 }
 
-static void
-t8_default_line_first_descendant (const t8_element_t * elem,
-                                  t8_element_t * desc)
+void
+t8_default_scheme_line_c::t8_element_first_descendant (const t8_element_t *
+                                                       elem,
+                                                       t8_element_t * desc)
 {
   t8_dline_first_descendant ((t8_dline_t *) elem, (t8_dline_t *) desc,
                              T8_DLINE_MAXLEVEL);
 }
 
-t8_eclass_scheme_t *
-t8_default_scheme_new_line (void)
+/* Constructor */
+t8_default_scheme_line_c::t8_default_scheme_line_c (void)
 {
-  t8_eclass_scheme_t *ts;
+  eclass = T8_ECLASS_LINE;
+  element_size = sizeof (t8_default_line_t);
+  ts_context = sc_mempool_new (sizeof (element_size));
+}
 
-  ts = T8_ALLOC_ZERO (t8_eclass_scheme_t, 1);
-  ts->eclass = T8_ECLASS_LINE;
-
-  ts->elem_size = t8_default_line_size;
-  ts->elem_maxlevel = t8_default_line_maxlevel;
-
-  ts->elem_level = t8_default_line_level;
-  ts->elem_copy = t8_default_tline_copy;
-  ts->elem_compare = NULL;
-  ts->elem_parent = t8_default_line_parent;
-  ts->elem_sibling = NULL;
-  ts->elem_child = t8_default_line_child;
-  ts->elem_children = NULL;
-  ts->elem_is_family = NULL;
-  ts->elem_child_id = NULL;
-  ts->elem_nca = NULL;
-  ts->elem_set_linear_id = t8_default_line_set_linear_id;
-  ts->elem_get_linear_id = NULL;
-  ts->elem_first_desc = t8_default_line_first_descendant;
-  ts->elem_last_desc = NULL;
-  ts->elem_successor = t8_default_line_successor;
-  ts->elem_anchor = NULL;
-  ts->elem_root_len = NULL;
-
-  ts->elem_new = t8_default_mempool_alloc;
-  ts->elem_destroy = t8_default_mempool_free;
-
-  ts->ts_destroy = t8_default_scheme_mempool_destroy;
-  ts->ts_context = sc_mempool_new (sizeof (t8_default_line_t));
-
-  return ts;
+t8_default_scheme_line_c::~t8_default_scheme_line_c ()
+{
+  /* This destructor is empty since the destructor of the
+   * default_common scheme is called automatically and it
+   * suffices to destroy the quad_scheme.
+   * However we need to provide an implementation of the destructor
+   * and hence this empty function. */
 }
---
+++ src/t8_default/t8_default_line_cxx.hxx
@@ -0,0 +1,141 @@
+/*
+  This file is part of t8code.
+  t8code is a C library to manage a collection (a forest) of multiple
+  connected adaptive space-trees of general element classes in parallel.
+
+  Copyright (C) 2015 the developers
+
+  t8code is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  t8code is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with t8code; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+/** \file t8_default_lines.h
+ * The default implementation for lines.
+ */
+
+#ifndef T8_DEFAULT_LINE_CXX_HXX
+#define T8_DEFAULT_LINE_CXX_HXX
+
+#include <t8_element_cxx.hxx>
+
+T8_EXTERN_C_BEGIN ();
+
+/** Provide an implementation for the line element class.
+ * It is written as a self-contained library in the t8_dline_* files.
+ */
+
+struct t8_default_scheme_line_c:public t8_default_scheme_common_c
+{
+public:
+  /** The virtual table for a particular implementation of an element class. */
+
+  /** Constructor. */
+  t8_default_scheme_line_c ();
+
+  ~t8_default_scheme_line_c ();
+
+/** Return the maximum level allowed for this element class. */
+  virtual int         t8_element_maxlevel (void);
+
+/** Return the type of each child in the ordering of the implementation. */
+  virtual t8_eclass_t t8_element_child_eclass (int childid);
+
+/** Return the refinement level of an element. */
+  virtual int         t8_element_level (const t8_element_t * elem);
+
+/** Copy one element to another */
+  virtual void        t8_element_copy (const t8_element_t * source,
+                                       t8_element_t * dest);
+
+/** Compare to elements. returns negativ if elem1 < elem2, zero if elem1 equals elem2
+ *  and positiv if elem1 > elem2.
+ *  If elem2 is a copy of elem1 then the elements are equal.
+ */
+  virtual int         t8_element_compare (const t8_element_t * elem1,
+                                          const t8_element_t * elem2);
+
+/** Construct the parent of a given element. */
+  virtual void        t8_element_parent (const t8_element_t * elem,
+                                         t8_element_t * parent);
+
+/** Construct a same-size sibling of a given element. */
+  virtual void        t8_element_sibling (const t8_element_t * elem,
+                                          int sibid, t8_element_t * sibling);
+
+/** Construct the child element of a given number. */
+  virtual void        t8_element_child (const t8_element_t * elem,
+                                        int childid, t8_element_t * child);
+
+/** Construct all children of a given element. */
+  virtual void        t8_element_children (const t8_element_t * elem,
+                                           int length, t8_element_t * c[]);
+
+/** Return the child id of an element */
+  virtual int         t8_element_child_id (const t8_element_t * elem);
+
+/** Return nonzero if collection of elements is a family */
+  virtual int         t8_element_is_family (t8_element_t ** fam);
+
+/** Construct the nearest common ancestor of two elements in the same tree. */
+  virtual void        t8_element_nca (const t8_element_t * elem1,
+                                      const t8_element_t * elem2,
+                                      t8_element_t * nca);
+
+/** Construct all codimension-one boundary elements of a given element. */
+  virtual void        t8_element_boundary (const t8_element_t * elem,
+                                           int min_dim, int length,
+                                           t8_element_t ** boundary);
+
+/** Initialize an element according to a given linear id */
+  virtual void        t8_element_set_linear_id (t8_element_t * elem,
+                                                int level, uint64_t id);
+
+/** Calculate the linear id of an element */
+  virtual u_int64_t   t8_element_get_linear_id (const
+                                                t8_element_t *
+                                                elem, int level);
+
+/** Calculate the first descendant of a given element e. That is, the
+ *  first element in a uniform refinement of e of the maximal possible level.
+ */
+  virtual void        t8_element_first_descendant (const t8_element_t *
+                                                   elem, t8_element_t * desc);
+
+/** Calculate the last descendant of a given element e. That is, the
+ *  last element in a uniform refinement of e of the maximal possible level.
+ */
+  virtual void        t8_element_last_descendant (const t8_element_t *
+                                                  elem, t8_element_t * desc);
+
+/** Compute s as a successor of t*/
+  virtual void        t8_element_successor (const t8_element_t * t,
+                                            t8_element_t * s, int level);
+
+/** Get the integer coordinates of the anchor node of an element */
+  virtual void        t8_element_anchor (const t8_element_t * elem,
+                                         int anchor[3]);
+
+/** Get the integer root length of an element, that is the length of
+ *  the level 0 ancestor.
+ */
+  virtual int         t8_element_root_len (const t8_element_t * elem);
+
+  /** Compute the integer coordinates of a given element vertex. */
+  virtual void        t8_element_vertex_coords (const t8_element_t * t,
+                                                int vertex, int coords[]);
+};
+
+T8_EXTERN_C_END ();
+
+#endif /* !T8_DEFAULT_LINE_CXX_HXX */
