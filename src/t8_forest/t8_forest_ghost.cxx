6f8586aa649a27561b019a226094c84389bfa81a
Johannes Holke <holke@ins.uni-bonn.de>
2017-03-28 18:40:03 +0200

    ghost_add_tree and ghost_fill_tree_array implemented

--- src/t8_forest/t8_forest_ghost.cxx
+++ src/t8_forest/t8_forest_ghost.cxx
@@ -24,6 +24,7 @@
 #include <t8_forest/t8_forest_types.h>
 #include <t8_forest/t8_forest_private.h>
 #include <t8_forest.h>
+#include <t8_cmesh/t8_cmesh_trees.h>
 #include <t8_element_cxx.hxx>
 
 /* We want to export the whole implementation to be callable from "C" */
@@ -59,6 +60,20 @@ typedef struct
   int                 mpirank;  /* a process rank */
   size_t              index;    /* index into the remote_ghosts array */
 } t8_ghost_remote_hash_t;
+
+/* Compare two ghost_tree entries. We need this function to sort the
+ * ghost_trees array by global_id. */
+static int
+t8_ghost_tree_compare (const void *tree_a, const void *tree_b)
+{
+  const t8_ghost_tree_t *A = (const t8_ghost_tree_t *) tree_a;
+  const t8_ghost_tree_t *B = (const t8_ghost_tree_t *) tree_b;
+
+  if (A->global_id < B->global_id) {
+    return -1;
+  }
+  return A->global_id != B->global_id;
+}
 
 /* The hash function for the global tree hash.
  * As hash value we just return the global tree id. */
@@ -191,6 +206,154 @@ t8_forest_ghost_init (t8_forest_ghost_t * pghost)
   ghost->remote_processes = sc_array_new (sizeof (int));
 }
 
+/* Given a local tree in a forest add all non-local face neighbor trees
+ * to a ghost structure. If the trees already exist in the ghost structure
+ * they are not added.
+ */
+static void
+t8_forest_ghost_add_tree (t8_forest_t forest, t8_forest_ghost_t ghost,
+                          t8_gloidx_t gtreeid)
+{
+  t8_cmesh_t          cmesh;
+  t8_eclass_t         eclass;
+  t8_locidx_t         lctree_id, num_cmesh_local_trees;
+  t8_locidx_t         lctreeid;
+  sc_mempool_t       *hash_mempool;
+  t8_ghost_gtree_hash_t *global_to_index_entry;
+  int                 is_inserted;
+
+  T8_ASSERT (t8_forest_is_committed (forest));
+  T8_ASSERT (ghost != NULL);
+
+  cmesh = t8_forest_get_cmesh (forest);
+  /* Compute the cmesh local id of the tree */
+  lctreeid = gtreeid - t8_cmesh_get_first_treeid (cmesh);
+  num_cmesh_local_trees = t8_cmesh_get_num_local_trees (cmesh);
+  /* The tree must be a local tree or ghost tree in the cmesh */
+  T8_ASSERT (0 <= lctree_id && lctree_id < num_cmesh_local_trees
+             + t8_cmesh_get_num_ghosts (cmesh));
+
+  /* Get the coarse tree and its face-neighbor information */
+  if (lctree_id < num_cmesh_local_trees) {
+    /* The tree is a local tree in the cmesh */
+    eclass = t8_cmesh_get_tree_class (cmesh, lctreeid);
+  }
+  else {
+    /* The tree is a ghost in the cmesh */
+    eclass = t8_cmesh_get_ghost_class (cmesh,
+                                       lctreeid - cmesh->num_local_trees);
+  }
+
+  /* Build a new entry for the global_tree_to_ghost_tree hashtable */
+  hash_mempool = ghost->global_tree_to_ghost_tree->allocator;
+  global_to_index_entry =
+    (t8_ghost_gtree_hash_t *) sc_mempool_alloc (hash_mempool);
+  global_to_index_entry->global_id = gtreeid;
+  /* Try to add the entry to the array */
+  is_inserted = sc_hash_insert_unique (ghost->global_tree_to_ghost_tree,
+                                       global_to_index_entry, NULL);
+  if (!is_inserted) {
+    /* The tree was already added.
+     * clean-up and exit */
+    sc_mempool_free (hash_mempool, global_to_index_entry);
+    return;
+  }
+  else {
+    t8_ghost_tree_t    *ghost_tree;
+    /* The tree was not already added. */
+    /* Create the entry in the ghost_trees array */
+    ghost_tree = (t8_ghost_tree_t *) sc_array_push (ghost->ghost_trees);
+    ghost_tree->eclass = eclass;
+    ghost_tree->global_id = gtreeid;
+    sc_array_init (&ghost_tree->elements, sizeof (t8_element_t *));
+    /* Store the array-index of ghost_tree in the hashtable */
+    global_to_index_entry->index = ghost->ghost_trees->elem_count - 1;
+  }
+}
+
+/* Fill the ghost_trees array of a ghost structure with an entry
+ * for each ghost tree of the forest.
+ * This function does not create the process_offset table yet. */
+/* TODO: For the first and last tree we may add more trees than
+ *       neccessary, since we add all non-local faceneighbors, and
+ *       for these trees not all face-neighbors must contain ghost elements.
+ */
+static void
+t8_forest_ghost_fill_ghost_tree_array (t8_forest_t forest,
+                                       t8_forest_ghost_t ghost)
+{
+  t8_locidx_t         itree, num_local_trees;
+  t8_cmesh_t          cmesh;
+  t8_ctree_t          ctree;
+  t8_locidx_t        *face_neighbors, lneighid, first_ctreeid;
+  int                 iface, num_faces;
+  t8_ghost_gtree_hash_t global_tree_tgt_search;
+  t8_ghost_gtree_hash_t **pglobal_tree_tgt_entry, *global_tree_tgt_entry;
+  t8_ghost_tree_t    *ghost_tree;
+  size_t              it;
+
+  T8_ASSERT (t8_forest_is_committed (forest));
+  T8_ASSERT (ghost != NULL);
+
+  /* If the first tree of the forest is shared with other
+   * processes, then it must contain ghost elements */
+  if (t8_forest_first_tree_shared (forest)) {
+    t8_forest_ghost_add_tree (forest, ghost,
+                              t8_forest_get_first_local_tree_id (forest));
+  }
+  /* If the last tree of the forest is shared with other
+   * processes, then it must contain ghost elements */
+  if (t8_forest_last_tree_shared (forest)) {
+    t8_forest_ghost_add_tree (forest, ghost,
+                              t8_forest_get_first_local_tree_id (forest)
+                              + num_local_trees - 1);
+  }
+
+  cmesh = t8_forest_get_cmesh (forest);
+  first_ctreeid = t8_cmesh_get_first_treeid (cmesh);
+  /* Iterate over all tree */
+  for (itree = 0; itree < num_local_trees; itree++) {
+    /* Get a pointer to the coarse mesh tree and its face_neighbors */
+    ctree = t8_forest_get_coarse_tree_ext (forest, itree, &face_neighbors,
+                                           NULL);
+    num_faces = t8_eclass_num_faces[ctree->eclass];
+    /* Iterate over all faces of this tree */
+    for (iface = 0; iface < num_faces; iface++) {
+      /* Compute the (theoretical) forest local id of the neighbor */
+      lneighid = t8_forest_cmesh_ltreeid_to_ltreeid (forest,
+                                                     face_neighbors[iface]);
+      if (lneighid == -1) {
+        /* This face neighbor is not a forest local tree.
+         * We thus add it to the ghost trees */
+        t8_forest_ghost_add_tree (forest, ghost,
+                                  ctree->treeid + first_ctreeid);
+      }
+    }
+  }
+  /* Now we have added all trees to the array, we have to sort them
+   * according to their global_id */
+  sc_array_sort (ghost->ghost_trees, t8_ghost_tree_compare);
+  /* After sorting, we have to reset the global_tree_to_ghost_tree entries
+   * since these store for a global tree id the index in ghost->ghost_trees,
+   * which has changed now. */
+  for (it = 0; it < ghost->ghost_trees->elem_count; it++) {
+    ghost_tree = (t8_ghost_tree_t *) sc_array_index (ghost->ghost_trees, it);
+    /* Find the entry belonging to this ghost_tree in the hash table */
+    global_tree_tgt_search.global_id = ghost_tree->global_id;
+    sc_hash_insert_unique (ghost->global_tree_to_ghost_tree,
+                           &global_tree_tgt_search,
+                           (void ***) &pglobal_tree_tgt_entry);
+    global_tree_tgt_entry = *pglobal_tree_tgt_entry;
+    /* Check if the entry that we found was already included and
+     * not added to the hash table */
+    T8_ASSERT (global_tree_tgt_entry != &global_tree_tgt_search);
+    /* Also check for obvious equality */
+    T8_ASSERT (global_tree_tgt_entry->global_id == ghost_tree->global_id);
+    /* Set the new array index */
+    global_tree_tgt_entry->index = it;
+  }
+}
+
 /* Create one layer of ghost algorithms, following the algorithm
  * in p4est: Scalable Algorithms For Parallel Adaptive
  *           Mesh Refinement On Forests of Octrees
@@ -199,7 +362,7 @@ t8_forest_ghost_init (t8_forest_ghost_t * pghost)
 void
 t8_forest_ghost_create (t8_forest_t forest)
 {
-  t8_element_t       *elem, *half_neighbors;
+  t8_element_t       *elem, *neigh, *half_neighbors;
   t8_locidx_t         num_local_trees, num_tree_elems;
   t8_locidx_t         itree, ielem;
   t8_tree_t           tree;
@@ -218,6 +381,7 @@ t8_forest_ghost_create (t8_forest_t forest)
     tree = t8_forest_get_tree (forest, itree);
     tree_class = t8_forest_get_tree_class (forest, itree);
     ts = t8_forest_get_eclass_scheme (forest, tree_class);
+
     /* Loop over the elements of this tree */
     num_tree_elems = t8_forest_get_tree_element_count (tree);
     for (ielem = 0; ielem < num_tree_elems; ielem++) {
@@ -225,6 +389,11 @@ t8_forest_ghost_create (t8_forest_t forest)
       elem = t8_forest_get_tree_element (tree, ielem);
       num_faces = ts->t8_element_num_faces (elem);
       for (iface = 0; iface < num_faces; iface++) {
+        /* TODO: Check whether the neighbor element is inside the forest,
+         *       if not then add the neighbor tree to the ghost trees
+         *       and find the owners of the half sized neighbors.
+         *       This will save computing time. Need an "element is in forest" function*/
+
         /* Get the element class of the neighbor tree */
         t8_forest_element_neighbor_eclass (forest, itree, elem, iface);
         neigh_scheme = t8_forest_get_eclass_scheme (forest, neigh_class);
@@ -240,8 +409,9 @@ t8_forest_ghost_create (t8_forest_t forest)
           t8_forest_element_half_face_neighbors (forest, itree, elem,
                                                  &half_neighbors, iface,
                                                  num_face_children);
-#if 0
         /* Find the owner process of each face_child */
+#if 0
+        /* TODO: implement */
         for (ichild = 0; ichild < num_face_children; ichild++) {
           t8_forest_element_find_owner (forest,);
         }
