5708e4d31874b4c406ffacaf88625e08f56ef603
David <david@Salsa.(none)>
2017-04-19 13:33:47 +0200

    Updated Prism

--- src/t8_default/t8_dprism_bits.c
+++ src/t8_default/t8_dprism_bits.c
@@ -24,6 +24,7 @@
 #include "t8_dline_bits.h"
 #include "t8_dtri_bits.h"
 
+typedef int8_t      t8_dtri_cube_id_t;
 
 int
 t8_dprism_get_level (const t8_dprism_t * p)
@@ -57,12 +58,12 @@ t8_dprism_successor (const t8_dprism_t * l, t8_dprism_t * succ, int level)
     const int tri_child_id = t8_dtri_child_id(&l->tri);
     const int line_child_id = t8_dline_child_id(&l->line);
 
-    /*printf("l->level: %i, level: %i\n", l->tri.level, level);*/
     T8_ASSERT (1 <= level && level <= l->tri.level);
-
+    /*Prism has local id 7*/
     if(tri_child_id == 3 && line_child_id == 1)
     {
         t8_dtri_parent(&l->tri, &succ->tri);
+        /*Parent has also id 7, maybe it's parent, too. Have to check via recursion*/
         if(t8_dtri_child_id(&succ->tri) == 3)
         {
             succ->tri.level = level-1;
@@ -76,20 +77,25 @@ t8_dprism_successor (const t8_dprism_t * l, t8_dprism_t * succ, int level)
             succ->tri.level = level;
             succ->line.level = level;*/
         }
+        /*Successor is next triangle on "basement level" */
         else
         {
+            /*We computed the parent above, got to move back to the child*/
             t8_dtri_child(&succ->tri, 3, &succ->tri);
             t8_dtri_successor(&l->tri,&succ->tri, level);
+            /*Go to the basement level*/
             t8_dline_parent(&l->line, &succ->line);
             t8_dline_child(&succ->line, 0, &succ->line);
         }
     }
+    /*last prism in basement level, next prism is "triangle 0 x next line" */
     else if(tri_child_id == 3 && line_child_id == 0)
     {
         t8_dline_successor(&l->line, &succ->line, level);
         t8_dtri_parent(&l->tri, &succ->tri);
         t8_dtri_child(&succ->tri, 0, &succ->tri);
     }
+    /*Successor is "next triangle x same line"*/
     else
     {
         t8_dtri_successor(&l->tri,&succ->tri, level);
@@ -102,6 +108,7 @@ t8_dprism_first_descendant (const t8_dprism_t * l,
                                                t8_dprism_t * s, int level)
 {
     T8_ASSERT (level >= l->line.level && level <= T8_DPRISM_MAXLEVEL);
+    /*First prism descendant = first triangle desc x first line desc*/
     t8_dtri_first_descendant(&l->tri, &s->tri);
     t8_dline_first_descendant(&l->line, &s->line, level);
 }
@@ -110,6 +117,7 @@ void
 t8_dprism_last_descendant(const t8_dprism_t * l, t8_dprism_t * s, int level)
 {
     T8_ASSERT (level >= l->line.level && level <= T8_DPRISM_MAXLEVEL);
+    /*Last prism descendant = last triangle desc x last line desc*/
     t8_dtri_last_descendant(&l->tri, &s->tri);
     t8_dline_last_descendant(&l->line, &s->line, level);
 }
@@ -117,21 +125,18 @@ t8_dprism_last_descendant(const t8_dprism_t * l, t8_dprism_t * s, int level)
 void
 t8_dprism_vertex_coords(const t8_dprism_t * t, int vertex,
                                            int coords[3]){
-    T8_ASSERT (vertex >= 0 || vertex <= 8);
-    /*The vertices starting at the base tirangle*/
-    if (0 <= vertex && vertex < 6){
-        t8_dtri_compute_coords(&t->tri, vertex % 3, coords);
-        t8_dline_vertex_coords(&t->line, 0, &coords[2]);
-    }
-    else if( 6 <= vertex && vertex < 9)
-    {
-        t8_dtri_compute_coords(&t->tri, vertex, coords);
-        t8_dline_vertex_coords(&t->line, 1, &coords[2]);
-    }
+    T8_ASSERT (vertex >= 0 || vertex <= 6);
+    t8_dtri_compute_coords(&t->tri, vertex % 3, coords);
+    t8_dline_vertex_coords(&t->line, vertex / 3, &coords[2]);
+
 }
 
 uint64_t
 t8_dprism_linear_id(const t8_dprism_t * elem, int level)
 {
+    uint64_t            id = 0;
+
     T8_ASSERT (0 <= level && level <= T8_DPRISM_MAXLEVEL);
+
+    return id;
 }
