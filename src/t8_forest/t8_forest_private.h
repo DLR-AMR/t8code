a69ba6b0976abddc4622e091b251453144d2266c
Johannes Holke <s6joholk@uni-bonn.de>
2017-05-12 13:24:26 +0200

    todos

--- src/t8_forest/t8_forest_private.h
+++ src/t8_forest/t8_forest_private.h
@@ -161,6 +161,10 @@ t8_element_t       *t8_forest_get_tree_element (t8_tree_t tree,
  *       We call this in owners_at_face where element is a descendant,
  *       add a flag that is true is element is a descendant, such that the
  *       first desc must not be created */
+/* TODO: ext  version with parameters: lower_bound, upper_bound, is_desc/is_leaf
+ *       is it really needed to construct the tree owners? Cant we just use the global
+ *       offset array?
+ */
 int                 t8_forest_element_find_owner (t8_forest_t forest,
                                                   t8_gloidx_t gtreeid,
                                                   t8_element_t * element,
