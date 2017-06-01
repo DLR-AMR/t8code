c46abfb67e1d2d07ea4e7090b240686482a9d5e2
Johannes Holke <s6joholk@uni-bonn.de>
2017-06-01 16:08:25 +0200

    disable initialized check for set/get_user_data

--- src/t8_forest/t8_forest.c
+++ src/t8_forest/t8_forest.c
@@ -251,15 +251,19 @@ t8_forest_set_adapt (t8_forest_t forest, const t8_forest_t set_from,
 void
 t8_forest_set_user_data (t8_forest_t forest, void *data)
 {
+  /* TODO: we need a is_initialized and may be committed check! */
+#if 0
   T8_ASSERT (t8_forest_is_initialized (forest));
-  T8_ASSERT (!t8_forest_is_committed (forest));
+#endif
   forest->user_data = data;
 }
 
-void *
+void               *
 t8_forest_get_user_data (t8_forest_t forest)
 {
+#if 0
   T8_ASSERT (t8_forest_is_initialized (forest));
+#endif
   return forest->user_data;
 }
 
