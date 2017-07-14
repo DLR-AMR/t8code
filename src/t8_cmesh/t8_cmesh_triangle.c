374c98ee392a76452a8237b7e2e494a9ab8a5af1
Johannes Holke <holke@ins.uni-bonn.de>
2017-07-14 17:26:54 +0200

    bugfix tetgen negative volume code

--- src/t8_cmesh/t8_cmesh_triangle.c
+++ src/t8_cmesh/t8_cmesh_triangle.c
@@ -273,13 +273,16 @@ t8_cmesh_triangle_read_eles (t8_cmesh_t cmesh, int corner_offset,
       tree_vertices[3 * i + 2] =
         dim == 2 ? 0 : vertices[dim * tcorners[i] + 2];
     }
-    if (t8_cmesh_tree_vertices_negative_volume (eclass, tree_vertices,
-                                                dim + 1)) {
+    if (dim == 3
+        && t8_cmesh_tree_vertices_negative_volume (T8_ECLASS_TET,
+                                                   tree_vertices, dim + 1)) {
       /* The volume described is negative. We need to switch two
        * vertices. */
       double              temp;
-      T8_ASSERT (t8_eclass_to_dimension[eclass] == 3);
-      t8_debugf ("Correcting negative volume of tree %li\n", tree_count);
+
+      T8_ASSERT (dim == 3);
+      t8_debugf ("Correcting negative volume of tree %li\n",
+                 (long) triangle - triangle_offset);
       /* We switch vertex 0 and vertex 1 */
       for (i = 0; i < 3; i++) {
         temp = tree_vertices[i];
@@ -287,7 +290,7 @@ t8_cmesh_triangle_read_eles (t8_cmesh_t cmesh, int corner_offset,
         tree_vertices[3 + i] = temp;
       }
       T8_ASSERT (!t8_cmesh_tree_vertices_negative_volume
-                 (eclass, tree_vertices, dim + 1));
+                 (T8_ECLASS_TET, tree_vertices, dim + 1));
     }
     t8_cmesh_set_tree_vertices (cmesh, triangle - triangle_offset,
                                 t8_get_package_id (), 0,
