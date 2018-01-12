adedda65219b52fd2dfff4478406840b63401299
Johannes Holke <s6joholk@uni-bonn.de>
2018-01-12 18:21:09 +0100

    fix tet half neighbors

--- src/t8_default/t8_dtet_connectivity.h
+++ src/t8_default/t8_dtet_connectivity.h
@@ -74,8 +74,7 @@ extern const int    t8_dtet_parenttype_Iloc_to_cid[6][8];
  * I.e. {2, 1} means the boundary triangle is of category 2 and type 1.
  * The category determines how the coordinates of the triangle are computed
  * from the parent. \see t8_default_scheme_tet_c::t8_element_boundary.
- * Not all pairs (type, face_index) are valid input. For the invalid input,
- * the values (-1,-1) are stored.
+ * Invalid inpute values are represented by -1.
  */
 extern const int    t8_dtet_type_face_to_boundary[6][4][2];
 
