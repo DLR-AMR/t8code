9962a4a6ade4411096994f394b993b7a65523bc0
Johannes Holke <holke@ins.uni-bonn.de>
2018-06-10 16:58:48 +0200

    change help output of advect

--- example/advect/t8_advection.cxx
+++ example/advect/t8_advection.cxx
@@ -1984,13 +1984,13 @@ main (int argc, char *argv[])
                          "\t\t\t\t     are performed per element. Decreases the "
                          "performance!");
   sc_options_add_double(opt, 'X', "Xcoord", &ls_data.M[0],0.6,"The X-Coordinate of the middlepoint"
-                           "of the sphere. Default is 0.");
+                           "of the sphere. Default is 0.6.");
   sc_options_add_double(opt, 'Y', "Xcoord", &ls_data.M[1],0.6,"The Y-Coordinate of the middlepoint"
-                           "of the sphere. Default is 0.");
+                           "of the sphere. Default is 0.6.");
   sc_options_add_double(opt, 'Z', "Xcoord", &ls_data.M[2],0.6,"The Z-Coordinate of the middlepoint"
-                           "of the sphere. Default is 0.");
+                           "of the sphere. Default is 0.6.");
   sc_options_add_double(opt, 'R', "Radius", &ls_data.radius, 0.25, "The radius of the Sphere."
-                        "Default is 0.");
+                        "Default is 0.25.");
 
   sc_options_add_int (opt, 'V', "volume-refine", &volume_refine, -1,
                       "Refine elements close to the 0 level-set only "
