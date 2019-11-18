deff5c02aae2571c2965624b71ae3f3b321f2389
Johannes Holke <johannes.holke@dlr.de>
2019-11-18 11:23:13 +0100

    Indent new ghost example

--- example/ghost/t8_test_ghost_large_level_diff.cxx
+++ example/ghost/t8_test_ghost_large_level_diff.cxx
@@ -33,7 +33,6 @@
  * (This property is not tested).
  */
 
-
 #include <sc_flops.h>
 #include <sc_statistics.h>
 #include <sc_options.h>
@@ -43,7 +42,6 @@
 #include <t8_schemes/t8_default/t8_dtet.h>
 #include <t8_forest/t8_forest_adapt.h>
 #include <t8_schemes/t8_default_cxx.hxx>
-
 
 /* The refinement criterion
  * returns 1 if we refine the element, -1 if we coarsen,
@@ -67,12 +65,12 @@
  */
 static int
 t8_ghost_fractal_adapt (t8_forest_t forest, t8_forest_t forest_from,
-                            t8_locidx_t which_tree, t8_locidx_t lelement_id,
-                            t8_eclass_scheme_c * ts, int num_elements,
-                            t8_element_t * elements[])
+                        t8_locidx_t which_tree, t8_locidx_t lelement_id,
+                        t8_eclass_scheme_c * ts, int num_elements,
+                        t8_element_t * elements[])
 {
-  int                   level;
-  int                   type, child_id;
+  int                 level;
+  int                 type, child_id;
   T8_ASSERT (num_elements == 1 || num_elements ==
              ts->t8_element_num_children (elements[0]));
   T8_ASSERT (t8_eclass_scheme_is_default (ts));
@@ -81,36 +79,36 @@ t8_ghost_fractal_adapt (t8_forest_t forest, t8_forest_t forest_from,
   if (level >= *(int *) t8_forest_get_user_data (forest)) {
     return 0;
   }
-  if(ts->eclass == T8_ECLASS_PRISM){
-      type = ((t8_dprism_t *) elements[0])->tri.type;
-      /* refine type 0 except those with child_id 3 or 4*/
-      if (type == 0) {
-        child_id = ts->t8_element_child_id(elements[0]);
-        /* Not refining */
-        if(child_id == 3 || child_id == 4){
-            return 0;
-        }
-        /* Refining */
-        else{
-            return 1;
-        }
+  if (ts->eclass == T8_ECLASS_PRISM) {
+    type = ((t8_dprism_t *) elements[0])->tri.type;
+    /* refine type 0 except those with child_id 3 or 4 */
+    if (type == 0) {
+      child_id = ts->t8_element_child_id (elements[0]);
+      /* Not refining */
+      if (child_id == 3 || child_id == 4) {
+        return 0;
       }
-      return 0;
-  }
-  else if(ts->eclass == T8_ECLASS_TET){
-      type = ((t8_dtet_t * ) elements[0])->type;
-      /* Refine tets of type 0, 3 or 5 */
-      if (type == 0 || type == 3 || type == 5){
-          return 1;
+      /* Refining */
+      else {
+        return 1;
       }
-      return 0;
+    }
+    return 0;
   }
-  else if(ts->eclass == T8_ECLASS_HEX){
-      child_id = ts->t8_element_child_id(elements[0]);
-      if(child_id == 0 || child_id == 3 || child_id == 5 || child_id == 6){
-          return 1;
-      };
-      return 0;
+  else if (ts->eclass == T8_ECLASS_TET) {
+    type = ((t8_dtet_t *) elements[0])->type;
+    /* Refine tets of type 0, 3 or 5 */
+    if (type == 0 || type == 3 || type == 5) {
+      return 1;
+    }
+    return 0;
+  }
+  else if (ts->eclass == T8_ECLASS_HEX) {
+    child_id = ts->t8_element_child_id (elements[0]);
+    if (child_id == 0 || child_id == 3 || child_id == 5 || child_id == 6) {
+      return 1;
+    };
+    return 0;
   }
   return 0;
 }
@@ -128,81 +126,81 @@ t8_ghost_fractal_adapt (t8_forest_t forest, t8_forest_t forest_from,
  * @param comm      The MPI-communicator
  */
 static void
-t8_ghost_large_level_diff(const char* prefix, int dim, int level, int refine,
-                          int no_vtk, sc_MPI_Comm comm){
-    t8_forest_t     forest, forest_adapt, forest_partition;
-    t8_cmesh_t      cmesh, cmesh_partition;
-    sc_flopinfo_t       fi, snapshot;
-    sc_statinfo_t       stats[1];
+t8_ghost_large_level_diff (const char *prefix, int dim, int level, int refine,
+                           int no_vtk, sc_MPI_Comm comm)
+{
+  t8_forest_t         forest, forest_adapt, forest_partition;
+  t8_cmesh_t          cmesh, cmesh_partition;
+  sc_flopinfo_t       fi, snapshot;
+  sc_statinfo_t       stats[1];
 
+  if (prefix != NULL) {
+    cmesh = t8_cmesh_from_msh_file (prefix, 1, comm, dim, 0);
+  }
+  /* If no prefix given, create hypercube */
+  else {
+    cmesh = t8_cmesh_new_hypercube (T8_ECLASS_PRISM, comm, 0, 0, 0);
+  }
+  t8_cmesh_init (&cmesh_partition);
+  t8_cmesh_set_derive (cmesh_partition, cmesh);
+  t8_cmesh_set_partition_uniform (cmesh_partition, level);
+  t8_cmesh_commit (cmesh_partition, comm);
+  if (!no_vtk) {
+    t8_cmesh_vtk_write_file (cmesh_partition, "partitioned_cmesh", 1.0);
+  }
 
-    if(prefix != NULL){
-        cmesh = t8_cmesh_from_msh_file(prefix, 1, comm, dim, 0);
-    }
-    /* If no prefix given, create hypercube */
-    else{
-        cmesh = t8_cmesh_new_hypercube(T8_ECLASS_PRISM, comm, 0, 0, 0);
-    }
-    t8_cmesh_init(&cmesh_partition);
-    t8_cmesh_set_derive(cmesh_partition, cmesh);
-    t8_cmesh_set_partition_uniform(cmesh_partition, level);
-    t8_cmesh_commit(cmesh_partition, comm);
-    if(!no_vtk){
-        t8_cmesh_vtk_write_file(cmesh_partition, "partitioned_cmesh", 1.0);
-    }
+  /* New */
+  t8_forest_init (&forest);
+  t8_forest_set_cmesh (forest, cmesh_partition, comm);
+  t8_forest_set_scheme (forest, t8_scheme_new_default_cxx ());
+  t8_forest_set_level (forest, level);
+  sc_flops_start (&fi);
+  sc_flops_snap (&fi, &snapshot);
+  t8_forest_commit (forest);
+  sc_flops_shot (&fi, &snapshot);
+  sc_stats_set1 (&stats[0], snapshot.iwtime, "New");
+  if (!no_vtk) {
+    t8_forest_write_vtk (forest, "Forest_new");
+  }
+  t8_global_productionf ("Successfully commited forest.\n");
 
-    /* New */
-    t8_forest_init(&forest);
-    t8_forest_set_cmesh(forest, cmesh_partition, comm);
-    t8_forest_set_scheme(forest, t8_scheme_new_default_cxx() );
-    t8_forest_set_level(forest, level);
-    sc_flops_start (&fi);
-    sc_flops_snap (&fi, &snapshot);
-    t8_forest_commit (forest);
-    sc_flops_shot (&fi, &snapshot);
-    sc_stats_set1 (&stats[0], snapshot.iwtime, "New");
-    if(!no_vtk){
-        t8_forest_write_vtk(forest, "Forest_new");
-    }
-    t8_global_productionf("Successfully commited forest.\n");
+  /* Adapt */
+  t8_forest_init (&forest_adapt);
+  refine += level;
+  t8_forest_set_user_data (forest_adapt, &refine);
+  t8_forest_set_profiling (forest_adapt, 1);
+  t8_forest_set_adapt (forest_adapt, forest, t8_ghost_fractal_adapt, 1);
+  t8_forest_commit (forest_adapt);
+  if (!no_vtk) {
+    t8_forest_write_vtk (forest_adapt, "Forest_adapt");
+  }
+  t8_global_productionf ("Successfully refined forest adaptivly.\n");
 
-    /* Adapt */
-    t8_forest_init(&forest_adapt);
-    refine += level;
-    t8_forest_set_user_data(forest_adapt, &refine);
-    t8_forest_set_profiling(forest_adapt, 1);
-    t8_forest_set_adapt(forest_adapt, forest, t8_ghost_fractal_adapt, 1);
-    t8_forest_commit(forest_adapt);
-    if(!no_vtk){
-        t8_forest_write_vtk(forest_adapt, "Forest_adapt");
-    }
-    t8_global_productionf("Successfully refined forest adaptivly.\n");
+  /* Partition */
+  t8_forest_init (&forest_partition);
+  t8_forest_set_partition (forest_partition, forest_adapt, 0);
+  t8_forest_set_ghost_ext (forest_partition, 1, T8_GHOST_FACES, 3);
+  t8_forest_set_profiling (forest_partition, 1);
+  t8_forest_commit (forest_partition);
+  if (!no_vtk) {
+    t8_forest_write_vtk (forest_partition, "Forest_partition");
+  }
+  t8_global_productionf ("Successfully partitioned forest.\n");
+  t8_forest_ghost_print (forest_partition);
 
-    /* Partition */
-    t8_forest_init(&forest_partition);
-    t8_forest_set_partition(forest_partition, forest_adapt, 0);
-    t8_forest_set_ghost_ext(forest_partition, 1, T8_GHOST_FACES, 3);
-    t8_forest_set_profiling(forest_partition, 1);
-    t8_forest_commit(forest_partition);
-    if(!no_vtk){
-        t8_forest_write_vtk(forest_partition, "Forest_partition");
-    }
-    t8_global_productionf("Successfully partitioned forest.\n");
-    t8_forest_ghost_print(forest_partition);
+  t8_forest_print_profile (forest_partition);
+  t8_forest_unref (&forest_partition);
 
-    t8_forest_print_profile (forest_partition);
-    t8_forest_unref (&forest_partition);
-
-    sc_stats_compute (comm, 1, stats);
-    sc_stats_print (t8_get_package_id (), SC_LP_STATISTICS, 1, stats, 1, 1);
-
+  sc_stats_compute (comm, 1, stats);
+  sc_stats_print (t8_get_package_id (), SC_LP_STATISTICS, 1, stats, 1, 1);
 
 }
 
 int
 main (int argc, char *argv[])
 {
-  int                 mpiret, parsed, dim, level, refine, mpisize, helpme, no_vtk;
+  int                 mpiret, parsed, dim, level, refine, mpisize, helpme,
+    no_vtk;
   sc_options_t       *opt;
   const char         *prefix = NULL;
   char                usage[BUFSIZ];
@@ -233,28 +231,34 @@ main (int argc, char *argv[])
   SC_CHECK_MPI (mpiret);
 
   opt = sc_options_new (argv[0]);
-  sc_options_add_switch(opt, 'h', "help", &helpme, "Display a short help message:");
-  sc_options_add_string (opt, 'f', "prefix", &prefix, NULL, "The prefix to the mesh-file. "
+  sc_options_add_switch (opt, 'h', "help", &helpme,
+                         "Display a short help message:");
+  sc_options_add_string (opt, 'f', "prefix", &prefix, NULL,
+                         "The prefix to the mesh-file. "
                          "The file must end in .msh and be created with gmsh. "
                          "If no file is given, a hypercube of prisms is created.");
   sc_options_add_int (opt, 'd', "dim", &dim, 3, "The dimension of the mesh.");
-  sc_options_add_int (opt, 'l', "level", &level, 0, "The intial refinement level of the mesh.");
-  sc_options_add_int (opt, 'r', "refine", &refine, 0, "The number of levels that the forest "
-                       "is refined from the initial level.");
-  sc_options_add_switch(opt, 'o', "no-vtk", &no_vtk, "Suppress vtk output.");
-  parsed =sc_options_parse (t8_get_package_id (), SC_LP_ERROR, opt, argc, argv);
-  if(helpme){
-      /* display help message and usage */
-      t8_global_productionf ("%s\n", help);
-      sc_options_print_usage (t8_get_package_id (), SC_LP_ERROR, opt, NULL);
+  sc_options_add_int (opt, 'l', "level", &level, 0,
+                      "The intial refinement level of the mesh.");
+  sc_options_add_int (opt, 'r', "refine", &refine, 0,
+                      "The number of levels that the forest "
+                      "is refined from the initial level.");
+  sc_options_add_switch (opt, 'o', "no-vtk", &no_vtk, "Suppress vtk output.");
+  parsed =
+    sc_options_parse (t8_get_package_id (), SC_LP_ERROR, opt, argc, argv);
+  if (helpme) {
+    /* display help message and usage */
+    t8_global_productionf ("%s\n", help);
+    sc_options_print_usage (t8_get_package_id (), SC_LP_ERROR, opt, NULL);
   }
   else if (parsed >= 0 && 0 <= level && 0 <= refine) {
-    t8_ghost_large_level_diff(prefix, dim, level, refine, no_vtk,sc_MPI_COMM_WORLD);
+    t8_ghost_large_level_diff (prefix, dim, level, refine, no_vtk,
+                               sc_MPI_COMM_WORLD);
   }
   else {
-    /*wrong usage*/
-      t8_global_productionf ("\n\t ERROR: Wrong usage.\n\n");
-      sc_options_print_usage (t8_get_package_id (), SC_LP_ERROR, opt, NULL);
+    /*wrong usage */
+    t8_global_productionf ("\n\t ERROR: Wrong usage.\n\n");
+    sc_options_print_usage (t8_get_package_id (), SC_LP_ERROR, opt, NULL);
   }
 
   sc_options_destroy (opt);
