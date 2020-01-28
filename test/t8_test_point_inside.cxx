c6631c710ceaad2e6c3333f1a612fe5bfc0c0189
Johannes Holke <johannes.holke@dlr.de>
2020-01-28 18:53:25 +0100

    Correct the point inside check for lines and triangles.
    Checking whether the point is inside each plane defined
    by every face does only work in 3D.
    For 2D triangles we solve a system of linear equations,
    testing whether we can write the point as linear combination
    of the triangle's vertices.
    For lines we solve a similar equation with onlt one vector.
    For points (T8_ECLASS_VERTEX) we just check whether
    the point to check equals the vertex (up to a tolerance).
    
    There is no implementation for quads yet.

--- test/t8_test_point_inside.cxx
+++ test/t8_test_point_inside.cxx
@@ -256,8 +256,10 @@ main (int argc, char **argv)
     ("Testing one specific point with one specific triangle.\n");
   t8_test_point_inside_specific_triangle ();
   for (ieclass = T8_ECLASS_LINE; ieclass < T8_ECLASS_COUNT; ieclass++) {
-    if (ieclass != T8_ECLASS_PYRAMID) {
-      /* TODO: does not work with pyramids yet */
+    if (ieclass != T8_ECLASS_PYRAMID && ieclass != T8_ECLASS_QUAD) {
+      /* TODO: - does not work with pyramids yet, since pyramid elements are not implemented.
+       *         the point check should work with pyramids, once they are implemented.
+       *       - point inside check does not work with quads yet. */
       t8_global_productionf
         ("Testing point finding with eclass %s\n",
          t8_eclass_to_string[ieclass]);
