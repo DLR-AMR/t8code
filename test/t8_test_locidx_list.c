9dc727344b9f26fd059fd5c03ca181c298a2b81e
Johannes Holke <johannes.holke@dlr.de>
2020-03-31 17:32:28 +0200

    Add a list test and tests for the list iterator

--- test/t8_test_locidx_list.c
+++ test/t8_test_locidx_list.c
@@ -116,12 +116,59 @@ test_locidx_list_reinit ()
                   "Wrong number of items in list.");
   /* Reset the list */
   t8_locidx_list_reset (&list);
-  SC_CHECK_ABORT (!t8_locidx_list_is_initialized (&list),
-                  "List is initialized but shoul not be.");
   /* Re init the list */
   t8_locidx_list_init (&list);
+  SC_CHECK_ABORT (t8_locidx_list_is_initialized (&list),
+                  "List is not initialized.");
   /* Check that it is empty */
   SC_CHECK_ABORT (t8_locidx_list_count (&list) == 0, "List is not empty.");
+  t8_locidx_list_reset (&list);
+}
+
+/* We initialize a list and fill it with entries.
+ * We then remove several entries. */
+void
+test_locidx_list_remove ()
+{
+  t8_locidx_list_t    list;
+  const t8_locidx_t   max = 10;
+  t8_locidx_t         i, removed_item;
+
+  /* Initialize the list */
+  t8_locidx_list_init (&list);
+  SC_CHECK_ABORT (t8_locidx_list_is_initialized (&list),
+                  "List is not initialized.");
+
+  /* Insert the numbers 0,1,2,...max - 1 into the list.
+   * Check the count each time. */
+  for (i = 0; i < max; ++i) {
+    t8_locidx_list_append (&list, i);
+    SC_CHECK_ABORT (t8_locidx_list_count (&list) == i + 1,
+                    "Wrong number of items in list.");
+  }
+
+  /* The list now contains 0, 1, 2, 3, 4, ...
+   * We want to remove the second entry (1) from it.
+   * In order to do so, we must get a pointer to the first entry.
+   */
+  removed_item = t8_locidx_list_remove (&list, list.list.first);
+  SC_CHECK_ABORT (removed_item == 1, "Removed wrong item from the list.");
+  /* Check that one item less is in the list */
+  SC_CHECK_ABORT (t8_locidx_list_count (&list) == max - 1,
+                  "Wrong number of items in list.");
+
+  /* We now remove the first item of the list until the list is empty */
+  for (i = 0; i < max - 1; ++i) {
+    const t8_locidx_t   expect = (i == 0 ? 0 : i + 1);  /* The item that we expect to get */
+    removed_item = t8_locidx_list_remove (&list, NULL);
+    /* Check that we remove the correct item */
+    SC_CHECK_ABORTF (removed_item == expect,
+                     "Removed wrong item from the list. Expected %i got %i\n",
+                     expect, removed_item);
+    /* Check that the list contains the correct number of elements */
+    SC_CHECK_ABORT (t8_locidx_list_count (&list) == max - 2 - i,
+                    "Wrong number of items in list.");
+  }
   t8_locidx_list_reset (&list);
 }
 
@@ -142,6 +189,8 @@ main (int argc, char **argv)
   test_locidx_list_new ();
   test_locidx_list_init ();
   test_locidx_list_append_pop_count ();
+  test_locidx_list_reinit ();
+  test_locidx_list_remove ();
 
   sc_finalize ();
 
