c8411aa2f09d28d40ba2669fc573ab0aedffcd9f
David Knapp <David.Knapp@dlr.de>
2020-05-11 12:14:37 +0200

    Added forest-test to linear-id. Have a bug regarding some references raised by sc_finalize()

--- test/t8_test_init_linear_id.cxx
+++ test/t8_test_init_linear_id.cxx
@@ -23,44 +23,26 @@
 
 #include <t8_eclass.h>
 #include <t8_schemes/t8_default_cxx.hxx>
+#include <t8_forest.h>
+#include <t8_forest/t8_forest_private.h>
 
-static void
-t8_recursive_linear_id (t8_element_t * element, t8_element_t * child,
-                        t8_element_t * test, t8_eclass_scheme_c * ts,
-                        int maxlvl, int *id)
-{
-  int                 level = ts->t8_element_level (element);
-  int                 num_children, i;
-  num_children = ts->t8_element_num_children (element);
-  if (level == maxlvl - 1) {
-    for (i = 0; i < num_children; i++) {
-    ts->t8_element_child (element, i, child);
-    ts->t8_element_set_linear_id (test, maxlvl, *id);
-      SC_CHECK_ABORT (!ts->t8_element_compare (child, test),
-                      "Wrong linear id\n");
-      (*id)++;
-    }
-  }
-  else {
-    for (i = 0; i < num_children; i++) {
-      ts->t8_element_child (element, i, child);
-      t8_recursive_linear_id (child, element, test, ts, maxlvl, id);
-      ts->t8_element_parent (child, element);
-    }
-  }
-}
 
 static void
 t8_serial_linear_id(t8_element_t * element, t8_eclass_scheme_c * ts,
                     int maxlvl)
 {
-    t8_linearidx_t num_leafs , id, check_id;
+    t8_gloidx_t     num_leafs;
+    t8_linearidx_t  id, check_id;
     int i;
+    /*Check every level up to maxlvl*/
     for(i = 0; i <= maxlvl; i++)
     {
+        /*Get the number of elements with level i*/
         num_leafs = ts->t8_element_count_leafs_from_root(i);
+        T8_ASSERT((t8_linearidx_t)(num_leafs) == num_leafs);
         for(id = 0; id < num_leafs; id++)
         {
+            /*Construct element #id and check, if the computed id is the same*/
             ts->t8_element_set_linear_id(element, i, id);
             check_id = ts->t8_element_get_linear_id(element, i);
             SC_CHECK_ABORT(check_id == id, "Wrong ID\n");
@@ -69,19 +51,64 @@ t8_serial_linear_id(t8_element_t * element, t8_eclass_scheme_c * ts,
 }
 
 static void
+t8_check_uniform_forest(t8_eclass_scheme_c * ts, t8_scheme_cxx_t * scheme,
+                        sc_MPI_Comm comm, int maxlvl)
+{
+    t8_forest_t         forest;
+    t8_cmesh_t          cmesh;
+    t8_locidx_t         first_id, last_id, j, id;
+    t8_locidx_t         first_tid, last_tid, tree_id;
+    t8_element_t        *element;
+    int                 i, min_level;
+
+    cmesh = t8_cmesh_new_from_class(ts->eclass, comm);
+    t8_cmesh_ref(cmesh);
+    for(i = min_level; i < maxlvl; i++)
+    {
+        forest = t8_forest_new_uniform(cmesh, scheme, i, 0, comm);
+        /*Reuse the forest*/
+        t8_forest_ref(forest);
+        /*Get the id of the first and last tree on this process*/
+        first_tid = t8_forest_get_first_local_tree_id(forest);
+        last_tid = first_tid + t8_forest_get_num_local_trees(forest);
+        /*Iterate over trees*/
+        for(tree_id = first_id; tree_id < last_tid; tree_id++)
+        {
+            /*Get id of the first and last element on this tree*/
+            first_id = t8_forest_get_first_local_element_id(forest);
+            last_id = first_id + t8_forest_get_num_element(forest);
+            /*Iterate over elements*/
+            for(j = first_id; j < last_id; j++){
+                /*Get the j-th element and check the computed linear id*/
+                element = t8_forest_get_element(forest, j, &tree_id);
+                id = ts->t8_element_get_linear_id(element, i);
+                SC_CHECK_ABORT(id == j, "Wrong ID\n");
+            }
+        }
+        t8_forest_unref(&forest);
+        t8_debugf("Done with eclass %s at level %i\n",
+                  t8_eclass_to_string[ts->eclass], i);
+    }
+    t8_cmesh_unref(&cmesh);
+    t8_cmesh_destroy(&cmesh);
+}
+
+static void
 t8_check_linear_id (const int maxlvl)
 {
   t8_element_t       *element, *child, *test;
-  t8_scheme_cxx      *scheme;
+  t8_scheme_cxx_t    *scheme;
   t8_eclass_scheme_c *ts;
-  int                 eclassi, id, level;
+  int                 eclassi;
   t8_eclass_t         eclass;
+
   scheme = t8_scheme_new_default_cxx ();
   for (eclassi = T8_ECLASS_ZERO; eclassi < T8_ECLASS_PYRAMID; eclassi++) {
     /* TODO: Include pyramids as soon as they are supported. */
     eclass = (t8_eclass_t) eclassi;
     /* Get scheme for eclass */
     ts = scheme->eclass_schemes[eclass];
+
     /* Get element and initialize it */
     ts->t8_element_new (1, &element);
     ts->t8_element_new (1, &child);
@@ -89,11 +116,9 @@ t8_check_linear_id (const int maxlvl)
 
     ts->t8_element_set_linear_id (element, 0, 0);
     /* Check for correct parent-child relation */
-    /*for (level = 1; level <= maxlvl; level++) {
-      id = 0;
-      t8_recursive_linear_id (element, child, test, ts, level, &id);
-    }*/
     t8_serial_linear_id(element, ts, maxlvl);
+    t8_check_uniform_forest(ts, scheme, sc_MPI_COMM_WORLD, maxlvl);
+
     /* Destroy element */
     ts->t8_element_destroy (1, &element);
     ts->t8_element_destroy (1, &child);
