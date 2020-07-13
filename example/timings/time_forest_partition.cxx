fe6280971cc8dced39fada0f5992a674264b998a
Johannes Holke <johannes.holke@dlr.de>
2020-07-13 11:46:02 +0200

    minor edit

--- example/timings/time_forest_partition.cxx
+++ example/timings/time_forest_partition.cxx
@@ -380,13 +380,13 @@ main (int argc, char *argv[])
   mpiret = sc_MPI_Init (&argc, &argv);
   SC_CHECK_MPI (mpiret);
 
-  /* get mpisize */
-  mpiret = sc_MPI_Comm_size (sc_MPI_COMM_WORLD, &mpisize);
-  SC_CHECK_MPI (mpiret);
-
   sc_init (sc_MPI_COMM_WORLD, 1, 1, NULL, SC_LP_ESSENTIAL);
   p4est_init (NULL, SC_LP_ESSENTIAL);
   t8_init (SC_LP_STATISTICS);
+
+  /* get mpisize */
+  mpiret = sc_MPI_Comm_size (sc_MPI_COMM_WORLD, &mpisize);
+  SC_CHECK_MPI (mpiret);
 
   /* Setup for command line options */
   opt = sc_options_new (argv[0]);
