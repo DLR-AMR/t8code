370f80c51a7dfc1791102440ad077b006a1727d5
Johannes Holke <johannes.holke@dlr.de>
2020-10-16 16:10:24 +0200

    Fix a bug in the the gmsh example
    When the msh file cannot not be read and the
    routine returns NULL, this NULL pointer was passed to cmesh_destroy
    causing an assertion to trigger.
    We guarded the cmesh_destroy call with an if.

--- example/gmsh/t8_read_msh_file.cxx
+++ example/gmsh/t8_read_msh_file.cxx
@@ -154,7 +154,9 @@ main (int argc, char *argv[])
   }
   else {
     cmesh = t8_read_msh_file_build_cmesh (prefix, partition, dim, master);
-    t8_cmesh_destroy (&cmesh);
+    if (cmesh != NULL) {
+      t8_cmesh_destroy (&cmesh);
+    }
     sc_options_print_summary (t8_get_package_id (), SC_LP_PRODUCTION, opt);
   }
 
