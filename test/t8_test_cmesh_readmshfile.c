04453ca6f894b1e05dafe4f1216fff9fbd9d9412
Johannes Holke <johannes.holke@dlr.de>
2021-01-21 10:57:09 +0100

    Add license text and test comment

--- test/t8_test_cmesh_readmshfile.c
+++ test/t8_test_cmesh_readmshfile.c
@@ -1,7 +1,39 @@
+/*
+  This file is part of t8code.
+  t8code is a C library to manage a collection (a forest) of multiple
+  connected adaptive space-trees of general element types in parallel.
+
+  Copyright (C) 2015 the developers
+
+  t8code is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  t8code is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with t8code; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#include <unistd.h> /* Needed to check for file access */
+#include <t8.h>
 #include <t8_eclass.h>
 #include <t8_cmesh.h>
-#include "t8_cmesh/t8_cmesh_trees.h"
 #include <t8_cmesh_readmshfile.h>
+#include "t8_cmesh/t8_cmesh_trees.h"
+
+/* In this file we test the msh file (gmsh) reader of the cmesh.
+ * Currently, we support version 2 ascii.
+ * We read a mesh file and check whether the constructed cmesh is correct.
+ * We also try to read version 2 binary, version 4 ascii and version 4 binary
+ * formats. All are not supported and we expect the reader to catch this.
+ */
+
 
 /* Check whether the input cmesh matches a given coarse mesh.
  * Returns 1 on success, -1 on failure, 0 if cmesh is NULL.
