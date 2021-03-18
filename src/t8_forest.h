142f608b4ec06eae55c05191cdbeb96730d5f9c5
Johannes Holke <johannes.holke@dlr.de>
2021-03-18 17:31:12 +0100

    Revert point inside and face normal changes

--- src/t8_forest.h
+++ src/t8_forest.h
@@ -951,30 +951,6 @@ void                t8_forest_element_face_normal (t8_forest_t forest,
                                                    *tree_vertices,
                                                    double normal[3]);
 
-/** Query whether a given point lies inside an element or not. For bilinearly interpolated elements.
- * \param [in]      forest     The forest.
- * \param [in]      ltree_id   The forest local id of the tree in which the element is.
- * \param [in]      element    The element.
- * \param [in]      vertices   An array storing the vertex coordinates of the tree.
- * \param [in]      point      3-dimensional coordinates of the point to check
- * \param [in]      tolerance  tolerance that we allow the point to not exactly match the element.
- *                             If this value is larger we detect more points.
- *                             If it is zero we probably do not detect points even if they are inside
- *                             due to rounding errors.
- * \return          True (non-zero) if \a point lies within \a element, false otherwise.
- *                  The return value is also true if the point lies on the element boundary.
- *                  Thus, this function may return true for different leaf elements, if they
- *                  are neighbors and the point lies on the common boundary.
- */
-int                 t8_forest_element_point_inside (t8_forest_t forest,
-                                                    t8_locidx_t ltreeid,
-                                                    const t8_element_t *
-                                                    element,
-                                                    const double
-                                                    *tree_vertices,
-                                                    const double point[3],
-                                                    const double tolerance);
-
 /* TODO: if set level and partition/adapt/balance all give NULL, then
  * refine uniformly and partition/adapt/balance the unfiform forest. */
 /** Build a uniformly refined forest on a coarse mesh.
