3dd35d09749c1c4b88acf88ba26f4dd693ee0083
Johannes Holke <johannes.holke@dlr.de>
2021-03-19 20:05:57 +0100

    Changed error message since error occurs on partition directly

--- src/t8_cmesh/t8_cmesh.c
+++ src/t8_cmesh/t8_cmesh.c
@@ -2107,9 +2107,8 @@ t8_cmesh_new_hypercube (t8_eclass_t eclass, sc_MPI_Comm comm, int do_bcast,
   SC_CHECK_ABORT (eclass != T8_ECLASS_PYRAMID || !periodic,
                   "The pyramid cube mesh cannot be periodic.\n");
 
-  SC_CHECK_ABORT (do_bcast
-                  && do_partition,
-                  "Broadcasting and partitioning the hypercube cmesh is currently not supported.\n");
+  SC_CHECK_ABORT (do_partition,
+                  "Partitioning the hypercube cmesh is currently not supported.\n");
 
   mpiret = sc_MPI_Comm_rank (comm, &mpirank);
   SC_CHECK_MPI (mpiret);
