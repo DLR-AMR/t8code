4ef191a94f2e94ccb72a814b67b8e0a9e76d0ebc
Johannes Holke <johannes.holke@dlr.de>
2021-03-22 18:24:59 +0100

    Fix copy paste error in output

--- example/tutorials/t8_step1_coarsemesh.c
+++ example/tutorials/t8_step1_coarsemesh.c
@@ -121,9 +121,9 @@ main (int argc, char **argv)
   local_num_trees = t8_cmesh_get_num_local_trees (cmesh);
   global_num_trees = t8_cmesh_get_num_trees (cmesh);
   t8_global_productionf (" [step1] Created coarse mesh.\n");
-  t8_global_productionf (" [step2] Local number of trees:\t%i\n",
+  t8_global_productionf (" [step1] Local number of trees:\t%i\n",
                          local_num_trees);
-  t8_global_productionf (" [step2] Global number of trees:\t%li\n",
+  t8_global_productionf (" [step1] Global number of trees:\t%li\n",
                          global_num_trees);
   t8_step1_write_cmesh_vtk (cmesh, prefix);
   t8_global_productionf (" [step1] Wrote coarse mesh to vtu files: %s*\n",
