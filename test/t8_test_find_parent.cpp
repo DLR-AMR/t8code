fef62cfc704bac7747fbc5a213bab6b8993d8e58
David <david.knapp@dlr.de>
2021-04-12 17:07:52 +0200

    Remove old test-comment

--- test/t8_test_find_parent.cpp
+++ test/t8_test_find_parent.cpp
@@ -29,7 +29,6 @@
  * reached.
  */
 
-/* This is a comment to test, if I can push to develop*/
 static void
 t8_recursive_child_find_parent (t8_element_t * element, t8_element_t * child,
                                 t8_element_t * test_parent, t8_eclass_scheme_c * ts,
