14b0a87b14301249ae3bac45ea51785b1b48684f
Sandro Elsweijer <Sandro.Elsweijer@dlr.de>
2021-04-25 13:35:34 +0200

    implemented t8_geometry_bspline

--- example/geometry/t8_analytic_geom.cxx
+++ example/geometry/t8_analytic_geom.cxx
@@ -25,7 +25,14 @@
 #include <t8_schemes/t8_default_cxx.hxx>
 #include <t8_forest.h>
 #include <t8_geometry/t8_geometry_implementations/t8_geometry_analytic.hxx>
+#include <t8_geometry/t8_geometry_implementations/t8_geometry_bspline.hxx>
 #include <t8_geometry/t8_geometry_helpers.h>
+
+#include <GeomAPI_PointsToBSplineSurface.hxx>
+#include <gp_Pnt.hxx>
+#include <NCollection_Array2.hxx>
+#include <TColgp_Array2OfPnt.hxx>
+#include <Geom_BSplineSurface.hxx>
 
 typedef enum
 {
@@ -36,6 +43,7 @@ typedef enum
   T8_GEOM_CIRCLE,
   T8_GEOM_3D,
   T8_GEOM_MOVING,
+  T8_GEOM_BSPLINE,
   T8_GEOM_COUNT
 } t8_analytic_geom_type;
 
@@ -301,6 +309,48 @@ t8_analytic_geom (int level, t8_analytic_geom_type geom_type)
     t8_cmesh_set_tree_class (cmesh, 0, T8_ECLASS_QUAD);
     snprintf (vtuname, BUFSIZ, "forest_moving_lvl_%i", level);
     break;
+  case T8_GEOM_BSPLINE:
+    {
+      Handle_Geom_BSplineSurface bspline_surface;
+      TColgp_Array2OfPnt point_array(1, 3, 1, 3);
+      
+      t8_global_productionf
+      ("Creating uniform level %i forest with a bspline geometry.\n",
+       level);
+    
+      //for (int x = 0; x < 5; ++x)
+      //{
+      //  for (int y = 0; y < 4; ++y)
+      //  {
+      //    point_array(x + 1, y + 1) = gp_Pnt(x * 0.25, y * 0.25, rand() % 10 / 100 - 0.05);
+      //  }
+      //}
+      //for (int x = 0; x < 4; ++x)
+      //{
+      //  point_array(x + 1, 5) = gp_Pnt(x * 0.25, 1 + rand() % 10 / 100 - 0.05, rand() % 10 / 100 - 0.05);
+      //}
+      
+      point_array(1, 1) = gp_Pnt(0, 0, 0);
+      point_array(2, 1) = gp_Pnt(0.5, 0, 0);
+      point_array(3, 1) = gp_Pnt(1, 0, 0);
+
+      point_array(1, 2) = gp_Pnt(0, 0.5, -0.1);
+      point_array(2, 2) = gp_Pnt(0.5, 0.5, 0);
+      point_array(3, 2) = gp_Pnt(1, 0.5, -0.1);
+
+      point_array(1, 3) = gp_Pnt(0, 1, 0);
+      point_array(2, 3) = gp_Pnt(0.5, 1.2, 0.1);
+      point_array(3, 3) = gp_Pnt(1, 1, 0);
+
+      bspline_surface = GeomAPI_PointsToBSplineSurface(point_array).Surface();
+      
+      geometry = new t8_geometry_bspline (2, "bspline dim=2", NULL, bspline_surface);
+      t8_cmesh_set_tree_class (cmesh, 0, T8_ECLASS_QUAD);
+      t8_cmesh_set_tree_class (cmesh, 1, T8_ECLASS_QUAD);
+      t8_cmesh_set_join (cmesh, 0, 1, 1, 0, 0);
+      snprintf (vtuname, BUFSIZ, "forest_bspline_lvl_%i", level);
+      break;
+    }
   default:
     SC_ABORT_NOT_REACHED ();
   }
---
+++ src/t8_geometry/t8_geometry_implementations/t8_geometry_bspline.cxx
@@ -0,0 +1,90 @@
+/*
+  This file is part of t8code.
+  t8code is a C library to manage a collection (a forest) of multiple
+  connected adaptive space-trees of general element classes in parallel.
+
+  Copyright (C) 2015 the developers
+
+  t8code is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  t8code is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with t8code; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#include <t8_geometry/t8_geometry_implementations/t8_geometry_bspline.hxx>
+
+t8_geometry_bspline::t8_geometry_bspline (int dim, const char *name_in,
+                                            t8_geom_load_tree_data_fn
+                                            load_tree_data_in,
+                                            const Handle_Geom_BSplineSurface bspline_in)
+{
+  T8_ASSERT (0 <= dim && dim <= 3);
+
+  name = name_in;
+  dimension = dim;
+
+  load_tree_data = load_tree_data_in;
+  const Handle_Geom_BSplineSurface bspline = bspline_in;
+}
+
+/**
+ * Map a point in the reference space $$[0,1]^dimension$$ to $$\mathbb R^3$$
+ * \param [in]  gtreeid     The global tree (of the cmesh) in which the reference point is.
+ * \param [in]  ref_coords  Array of \a dimension many entries, specifying a point in [0,1]^dimension.
+ * \param [out] out_coords  The mapped coordinates in physical space of \a ref_coords.
+ */
+void
+t8_geometry_bspline::t8_geom_evaluate (t8_cmesh_t cmesh,
+                                        t8_gloidx_t gtreeid,
+                                        const double *ref_coords,
+                                        double out_coords[3]) const
+{
+  gp_Pnt pnt;
+  bspline->D0(ref_coords[0], ref_coords[1], pnt);
+
+  out_coords[0] = pnt.X();
+  out_coords[2] = pnt.Z();
+  out_coords[1] = pnt.Y();
+}
+
+void
+t8_geometry_bspline::t8_geom_evalute_jacobian (t8_cmesh_t cmesh,
+                                                t8_gloidx_t gtreeid,
+                                                const double
+                                                *ref_coords,
+                                                double *jacobian_out) const
+{
+  
+}
+
+void
+t8_geometry_bspline::t8_geom_load_tree_data (t8_cmesh_t cmesh,
+                                              t8_gloidx_t gtreeid)
+{
+  if (load_tree_data != NULL) {
+    /* Load tree data if a loading function was provided. */
+    load_tree_data (cmesh, gtreeid, &tree_data);
+  }
+  else {
+    /* Otherwise it is NULL. */
+    tree_data = NULL;
+  }
+}
+
+void
+t8_geom_load_tree_data_vertices (t8_cmesh_t cmesh, t8_gloidx_t gtreeid,
+                                 const void **vertices_out)
+{
+  T8_ASSERT (t8_cmesh_is_committed (cmesh));
+  t8_locidx_t         ltreeid = t8_cmesh_get_local_id (cmesh, gtreeid);
+  *vertices_out = t8_cmesh_get_tree_vertices (cmesh, ltreeid);
+}
