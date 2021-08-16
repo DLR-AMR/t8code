ae075650ed9e6b7b573b387a673c6553605a649e
Becker <beck_f2@sc-030175l.intra.dlr.de>
2021-08-16 19:11:31 +0200

    Implement the possibility to coarse the mesh even with subelements

--- src/t8_forest/t8_forest_adapt.cxx
+++ src/t8_forest/t8_forest_adapt.cxx
@@ -286,7 +286,9 @@ t8_forest_adapt (t8_forest_t forest)
       t8_element_t       *current_element =
         t8_element_array_index_locidx (telements_from, el_considered);
 
+      int current_element_is_subelement = 0;
       if (tscheme->t8_element_test_if_subelement (current_element) == 1) {
+        current_element_is_subelement = 1;
         int                 subelement_type =
           tscheme->t8_element_get_subelement_type (current_element);
         num_siblings =
@@ -408,7 +410,12 @@ t8_forest_adapt (t8_forest_t forest)
                                         elements);
           el_inserted += num_children;
         }
-        el_considered++;
+        if (current_element_is_subelement == 1) {
+          el_considered += num_siblings;
+        }
+        else {
+          el_considered++;
+        }
       }
       else if (refine > 1) {    /* use subelements in this case */
         /* The subelement-callback function returns refine = subelement_type + 1 to avoid subelement_type = 1.
