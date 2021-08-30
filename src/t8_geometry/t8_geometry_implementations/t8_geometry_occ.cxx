23cef70f4205a85daf1fafdd339588290d37c4af
Sandro Elsweijer <Sandro.Elsweijer@dlr.de>
2021-08-30 11:29:57 +0200

    reworked interpolations and improved comments

--- src/t8_geometry/t8_geometry_implementations/t8_geometry_occ.cxx
+++ src/t8_geometry/t8_geometry_implementations/t8_geometry_occ.cxx
@@ -66,14 +66,6 @@ t8_geometry_occ::t8_geom_evaluate (t8_cmesh_t cmesh,
                                    active_tree_vertices, ref_coords,
                                    out_coords);
   
-  /* Sequence of normal vectors to do linear interpolations */
-  int normal_sequence[3][3] = 
-  {
-    {1, 2, 0},
-    {0, 2, 1},
-    {0, 1, 2}
-  };
-  
   const int
   t8_edge_vertex_to_tree_vertex[T8_ECLASS_MAX_EDGES][2] =
   {
@@ -88,14 +80,30 @@ t8_geometry_occ::t8_geom_evaluate (t8_cmesh_t cmesh,
   {
     if (edges[i] >= 0)
     {
-      /* Interpolate coordinates between edge vertices */
+      /* Interpolate coordinates between edge vertices. Due to the indices i of the edges, the edges point in
+      * direction of ref_coord i % 3. Therefore, we can use ref_coords[i % 3] for the interpolation.              
+      *          6 -------E3------- 7
+      *         /|                 /|
+      *       E5 |               E7 |
+      *       / E10              / E11
+      *      /   |              /   |          z y
+      *     4 -------E2------- 5    |          |/
+      *     |    |             |    |          x-- x
+      *     |    2 -------E1---|--- 3
+      *    E8   /             E9   /
+      *     |  E4              |  E6
+      *     | /                | /
+      *     |/                 |/
+      *     0 -------E0------- 1
+      *        
+      */
       for (int j = 0; j < 3; ++j)
       {
         interpolated_coords[j] = (1 - ref_coords[i % 3]) * active_tree_vertices[t8_edge_vertex_to_tree_vertex[i][0] * 3 + j]
                                 + ref_coords[i % 3] * active_tree_vertices[t8_edge_vertex_to_tree_vertex[i][1] * 3 + j];
       }
 
-      /* Interpolate parameters between edge vertices */
+      /* Interpolate parameters between edge vertices. Same procedure as above. */
       const double *parameters = (double *) t8_cmesh_get_attribute(cmesh, t8_get_package_id (),
                                                                   T8_CMESH_OCC_CURVE_PARAMETERS_ATTRIBUTE_KEY + i,
                                                                   gtreeid);
@@ -115,7 +123,10 @@ t8_geometry_occ::t8_geom_evaluate (t8_cmesh_t cmesh,
       cur_edge_delta[1] = pnt.Y() - interpolated_coords[1];
       cur_edge_delta[2] = pnt.Z() - interpolated_coords[2];
       
-      /* Multiply curve displacement with corresponding ref coords */
+      /* Multiply curve displacement with corresponding ref coords.
+      *  The edges are indexed so that all edges which satisfy i % 4 == 0 have to multiplied with the inversed (1 - ref_coord) 
+      *  coordinate. All edges which satisfy i % 4 == 1 have to multiplied with one inversed ref_coord and so forth...
+      */
       switch (i % 4)
       {
       case 0:
@@ -156,30 +167,50 @@ t8_geometry_occ::t8_geom_evaluate (t8_cmesh_t cmesh,
   for (int i = 0; i < 6; ++i)
   {
     if (faces[i] >= 0)
-    {
-      /* Interpolate coordinates without between face vertices */
+    {      
+      /* Interpolate coordinates between face vertices
+      *   
+      *               5 ---------------- 7
+      *              /|                 /|
+      *             / |      f5        / |
+      *            /  |     (top)     /  |  <-f3  
+      *           /   |              /   | (back)   z y
+      *          4 ---------------- 5    |          |/
+      *          | f0 |             | f1 |          x-- x
+      *          |    2 ------------|--- 3
+      *          |   /              |   /
+      *   f2->   |  /               |  /
+      *  (front) | /        f4      | /
+      *          |/      (bottom)   |/
+      *          0 ---------------- 1
+      *                 
+      *  ref_coords[(i / 2 + 1) % 3] and ref_coords[(i / 2 + 2) % 3] always returns a ref_coord perpendicular to face i
+      *  
+      *  The order in which the interpolations are computed is not consistent with the vertex indices.
+      *  Therefore, we need to switch the vertices 1 and 2 when calculating face 2 and 3. This is done with [i / 2 == 1 ? 2 : 1]
+      */
       for (int j = 0; j < 3; ++j)
       {
-        interpolated_coords[j] = ((1 - ref_coords[normal_sequence[i / 2][0]]) * active_tree_vertices[t8_face_vertex_to_tree_vertex[T8_ECLASS_HEX][i][0] * 3 + j]
-                                + ref_coords[normal_sequence[i / 2][0]] * active_tree_vertices[t8_face_vertex_to_tree_vertex[T8_ECLASS_HEX][i][1] * 3 + j])
-                                * (1 - ref_coords[normal_sequence[i / 2][1]]);
-        interpolated_coords[j] += ((1 - ref_coords[normal_sequence[i / 2][0]]) * active_tree_vertices[t8_face_vertex_to_tree_vertex[T8_ECLASS_HEX][i][2] * 3 + j]
-                                + ref_coords[normal_sequence[i / 2][0]] * active_tree_vertices[t8_face_vertex_to_tree_vertex[T8_ECLASS_HEX][i][3] * 3 + j])
-                                * ref_coords[normal_sequence[i / 2][1]];
+        interpolated_coords[j] = ((1 - ref_coords[(i / 2 + 1) % 3]) * active_tree_vertices[t8_face_vertex_to_tree_vertex[T8_ECLASS_HEX][i][0] * 3 + j]
+                                + ref_coords[(i / 2 + 1) % 3] * active_tree_vertices[t8_face_vertex_to_tree_vertex[T8_ECLASS_HEX][i][i / 2 == 1 ? 2 : 1] * 3 + j])
+                                * (1 - ref_coords[(i / 2 + 2) % 3]);
+        interpolated_coords[j] += ((1 - ref_coords[(i / 2 + 1) % 3]) * active_tree_vertices[t8_face_vertex_to_tree_vertex[T8_ECLASS_HEX][i][i / 2 == 1 ? 1 : 2] * 3 + j]
+                                + ref_coords[(i / 2 + 1) % 3] * active_tree_vertices[t8_face_vertex_to_tree_vertex[T8_ECLASS_HEX][i][3] * 3 + j])
+                                * ref_coords[(i / 2 + 2) % 3];
       }
-      
-      /* Interpolate parameters between face vertices */
+
+      /* Interpolate parameters between face vertices. Same procedure as above. */
       const double *parameters = (double *) t8_cmesh_get_attribute(cmesh, t8_get_package_id (),
                                                       T8_CMESH_OCC_SURFACE_PARAMETERS_ATTRIBUTE_KEY + i,
                                                       gtreeid);
       for (int j = 0; j < 2; ++j)
       {
-        param[j] = (parameters[0 + j] * (1 - ref_coords[normal_sequence[i / 2][0]])
-                  + parameters[2 + j] * (ref_coords[normal_sequence[i / 2][0]]))
-                  * (1 - ref_coords[normal_sequence[i / 2][1]]);
-        param[j] += (parameters[4 + j] * (1 - ref_coords[normal_sequence[i / 2][0]])
-                    +parameters[6 + j] * (ref_coords[normal_sequence[i / 2][0]]))
-                    *(ref_coords[normal_sequence[i / 2][1]]);
+        param[j] = (parameters[0 + j] * (1 - ref_coords[(i / 2 + 1) % 3])
+                  + parameters[(i / 2 == 1 ? 4 : 2) + j] * (ref_coords[(i / 2 + 1) % 3]))
+                  * (1 - ref_coords[(i / 2 + 2) % 3]);
+        param[j] += (parameters[(i / 2 == 1 ? 2 : 4) + j] * (1 - ref_coords[(i / 2 + 1) % 3])
+                    +parameters[6 + j] * (ref_coords[(i / 2 + 1) % 3]))
+                    *(ref_coords[(i / 2 + 2) % 3]);
       }
       
       /* Check if calculated parameters are valid */
