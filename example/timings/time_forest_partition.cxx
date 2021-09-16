b231183100f062e023ac7945fc50c1cab61daaf8
Sandro Elsweijer <49643115+sandro-elsweijer@users.noreply.github.com>
2021-09-16 14:37:12 +0200

    uncommented code
    Co-authored-by: Johannes Holke <johannes.holke@dlr.de>

--- example/timings/time_forest_partition.cxx
+++ example/timings/time_forest_partition.cxx
@@ -255,7 +255,7 @@ t8_time_forest_cmesh_mshfile (t8_cmesh_t cmesh, const char *vtu_prefix,
     t8_forest_compute_profile (forest_partition);
     balance_stats = t8_forest_profile_get_balance_stats (forest_partition);
     ghost_stats = t8_forest_profile_get_ghost_stats (forest_partition);
-    //t8_cmesh_print_profile (t8_forest_get_cmesh (forest_partition));
+    t8_cmesh_print_profile (t8_forest_get_cmesh (forest_partition));
     forest = forest_partition;
     //  }
 
