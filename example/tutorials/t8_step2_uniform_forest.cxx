20f6aa4f3a1e90ddcaf4e78f397379021c04e724
Johannes Holke <johannes.holke@dlr.de>
2021-12-14 15:14:02 +0100

    Update example/tutorials/t8_step2_uniform_forest.cxx

--- example/tutorials/t8_step2_uniform_forest.cxx
+++ example/tutorials/t8_step2_uniform_forest.cxx
@@ -77,7 +77,7 @@ t8_step2_build_prismcube_coarse_mesh (sc_MPI_Comm comm)
  * \return            A uniform forest with the given refinement level that is
  *                    partitioned across the processes in \a comm.
  */
-static              t8_forest_t
+static t8_forest_t
 t8_step2_build_uniform_forest (sc_MPI_Comm comm, t8_cmesh_t cmesh, int level)
 {
   t8_forest_t         forest;
