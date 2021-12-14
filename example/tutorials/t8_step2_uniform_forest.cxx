07d8267e87e3707ae8fb1a1003eb829628480b37
Johannes Holke <johannes.holke@dlr.de>
2021-12-14 15:13:21 +0100

    Update example/tutorials/t8_step2_uniform_forest.cxx

--- example/tutorials/t8_step2_uniform_forest.cxx
+++ example/tutorials/t8_step2_uniform_forest.cxx
@@ -56,7 +56,7 @@
  * \param [in] comm   MPI Communicator to use.
  * \return            The coarse mesh.
  */
-static              t8_cmesh_t
+static t8_cmesh_t
 t8_step2_build_prismcube_coarse_mesh (sc_MPI_Comm comm)
 {
   t8_cmesh_t          cmesh;
