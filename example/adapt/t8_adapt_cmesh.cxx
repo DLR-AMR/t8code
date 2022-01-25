8aadb432e46b7a17d6a0259af386942960522523
Johannes Holke <johannes.holke@dlr.de>
2022-01-25 17:48:38 +0100

    Define framework for adapt from forest and start with data types.
    Co-authored-by: David Knapp <david.knapp@dlr.de>

--- example/adapt/t8_adapt_cmesh.cxx
+++ example/adapt/t8_adapt_cmesh.cxx
@@ -25,15 +25,20 @@
 #include <t8_cmesh.h>
 #include <t8_forest.h>
 #include <t8_forest_vtk.h>
+#include <t8_forest_iterate.h>
 #include <t8_schemes/t8_default_cxx.hxx>
 
 /* Build a cmesh according to which a later forest shall be refined. */
-t8_cmesh_t
-t8_adapt_cmesh_init_adapt_geometry (sc_MPI_Comm comm)
+t8_forest_t
+t8_adapt_forest_init_adapt_geometry (sc_MPI_Comm comm)
 {
   t8_cmesh_t          cmesh = t8_cmesh_new_from_class (T8_ECLASS_TET, comm);
+  t8_scheme_cxx_t    *scheme = t8_scheme_new_default_cxx ();
+  const int           level = 0;
+  t8_forest_t         forest =
+    t8_forest_new_uniform (cmesh, scheme, level, 0, sc_MPI_COMM_SELF);
   T8_ASSERT (!t8_cmesh_is_partitioned (cmesh));
-  return cmesh;
+  return forest;
 }
 
 t8_forest_t
@@ -51,20 +56,92 @@ t8_adapt_cmesh_init_forest (sc_MPI_Comm comm, int level)
 /*Idee: cmesh als user_data mitgeben. */
 typedef struct
 {
-  t8_cmesh_t          cmesh_to_adapt_from;
+  t8_forest_t         forest_to_adapt_from;
 } t8_adapt_cmesh_user_data_t;
+typedef struct
+{
+  t8_locidx_t         tree_id;  /* Tree id of the element */
+  t8_locidx_t         element_id;       /* Id of the current element. */
+} t8_adapt_cmesh_search_query_t;
+
+static void
+t8_adapt_cmesh_search (t8_forest_t forest, t8_forest_t forest_to_adapt_from,
+                       sc_array_t * markers)
+{
+  sc_array_t          search_queries;
+  const t8_locidx_t   num_elements =
+    t8_forest_get_local_num_elements (forest_to_adapt_from);
+  const t8_locidx_t   num_trees =
+    t8_forest_get_num_local_trees (forest_to_adapt_from);
+  /* Initialize the queries array */
+  sc_array_init_count (&search_queries,
+                       sizeof (t8_adapt_cmesh_search_query_t), num_elements);
+  /* Fill queries with correct ids. */
+
+#if 0
+  /* TODO Fill tree ids and element ids */
+  for (t8_locidx_t itree; itree < num_trees; ++itree) {
+    *(t8_sc_array_index_locidx (&search_queries, itree)) = itree;
+  }
+#endif
+  /* Set the cmesh as forest user data */
+  t8_adapt_cmesh_user_data_t search_user_data;
+  search_user_data.cmesh_to_adapt_from = cmesh;
+  t8_forest_set_user_data (forest, &search_user_data);
+  /* Fill marker array.
+   * elements that should be refined are set to 1. 0 for no refinemnet. -1 for coarsening. */
+  t8_forest_search (forest, t8_adapt_cmesh_search_element_callback,
+                    t8_adapt_cmesh_search_query_callback, search_queries);
+}
+
+static              t8_forest_t
+t8_adapt_cmesh_adapt_forest (t8_forest_t forest, t8_cmesh_t cmesh)
+{
+  /* TODO ... */
+
+  const t8_locidx_t   num_local_elements =
+    t8_forest_get_local_num_elements (forest);
+  /* Create marker array  to mark elements for refinement */
+  sc_array_t          marker_array;
+
+  sc_array_init_count (&marker_array, sizeof (short), num_local_elements);
+
+  t8_adapt_cmesh_search (forest, cmesh, &marker_array);
+
+  /* Adapt the forest according to the markers */
+  t8_forest_t         forest_adapt =
+    t8_forest_new_adapt (forest, t8_forest_adapt_marker_array_callback,
+                         0, 0, marker_array);
+}
 
 static int
-t8_adapt_cmesh_search_callback (t8_forest_t forest,
-                                t8_locidx_t ltreeid,
-                                const t8_element_t *
-                                element,
-                                const int is_leaf,
-                                t8_element_array_t *
-                                leaf_elements,
-                                t8_locidx_t
-                                tree_leaf_index, void *query,
-                                size_t query_index)
+t8_adapt_cmesh_search_element_callback (t8_forest_t forest,
+                                        t8_locidx_t ltreeid,
+                                        const t8_element_t *
+                                        element,
+                                        const int is_leaf,
+                                        t8_element_array_t *
+                                        leaf_elements,
+                                        t8_locidx_t
+                                        tree_leaf_index, void *query,
+                                        size_t query_index)
+{
+  T8_ASSERT (query == NULL);
+  /* We want to continue the search as long as there are queries left for the element. */
+  return 1;
+}
+
+static int
+t8_adapt_cmesh_search_query_callback (t8_forest_t forest,
+                                      t8_locidx_t ltreeid,
+                                      const t8_element_t *
+                                      element,
+                                      const int is_leaf,
+                                      t8_element_array_t *
+                                      leaf_elements,
+                                      t8_locidx_t
+                                      tree_leaf_index, void *query,
+                                      size_t query_index)
 {
   /*Identifiziere Element in forest, die einen Mittelpunkt eines
      Elements in  cmesh_to_adapt_from enthalten. 
@@ -72,7 +149,20 @@ t8_adapt_cmesh_search_callback (t8_forest_t forest,
      Falls nein -> fertig */
   t8_adapt_cmesh_user_data_t *user_data =
     (t8_adapt_cmesh_user_data_t *) t8_forest_get_user_data (forest);
-  t8_locidx_t         current_tree_id, num_trees;
+  const t8_cmesh_t    cmesh = user_data->cmesh_to_adapt_from;
+  t8_adapt_cmesh_search_query_t *search_query =
+    (t8_adapt_cmesh_search_query_t *) query;
+  const t8_locidx_t   current_cmesh_tree_id = search_query->ctree_id;
+
+  /* TODO: Get tree id and element id from query */
+
+  /* TODO: Compute midpoint of query. Return true only if midpoint is in current element. */
+  /* Compute midpoint of tree */
+  double              midpoint[3];
+
+  t8_forest_element_centroid ();
+
+  /* Check if midpoint is inside element */
 }
 
 int
@@ -88,7 +178,7 @@ main (int argc, char **argv)
 
   /* long help message */
   snprintf (help, BUFSIZ,
-            "This program adapts a forest according to a provided coarse mesh.\n");
+            "This program adapts a forest according to a provided forest mesh.\n");
   mpiret = sc_MPI_Init (&argc, &argv);
   SC_CHECK_MPI (mpiret);
 
@@ -116,8 +206,8 @@ main (int argc, char **argv)
     sc_options_print_usage (t8_get_package_id (), SC_LP_ERROR, opt, NULL);
   }
   else if (parsed >= 0 /* TODO: Check correct arguments */ ) {
-    t8_cmesh_t          cmesh_to_adapt_from =
-      t8_adapt_cmesh_init_adapt_geometry (comm);
+    t8_forest_t         forest_to_adapt_from =
+      t8_adapt_forest_init_adapt_geometry (comm);
     t8_forest_t         forest = t8_adapt_cmesh_init_forest (comm, level);
 
     /* Identifiziere Element in forest, die einen Mittelpunkt eines
@@ -126,7 +216,7 @@ main (int argc, char **argv)
        deren Mittelpunkt in einem Element des cmesh_to_adapt_from liegen? Im Grobgitter 
        beschreibt, der Mittelpunkt das Element sehr schlecht. */
     /* Adaptiere forest, so dass alle identifizierten Elemente verfeienrt werden. */
-    t8_cmesh_destroy (&cmesh_to_adapt_from);
+    t8_forest_unref (&forest_to_adapt_from);
     t8_forest_unref (&forest);
   }
   else {
