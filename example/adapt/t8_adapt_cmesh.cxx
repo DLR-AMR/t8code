df8fa8900145794fd4cddd7efdc7be5ac4c47da0
Johannes Holke <johannes.holke@dlr.de>
2022-01-25 14:58:24 +0100

    Initialize cmesh and forest in cmesh adapt example
    Co-authored-by: David Knapp <david.knapp@dlr.de>

--- example/adapt/t8_adapt_cmesh.cxx
+++ example/adapt/t8_adapt_cmesh.cxx
@@ -25,6 +25,28 @@
 #include <t8_cmesh.h>
 #include <t8_forest.h>
 #include <t8_forest_vtk.h>
+#include <t8_schemes/t8_default_cxx.hxx>
+
+/* Build a cmesh according to which a later forest shall be refined. */
+t8_cmesh_t
+t8_adapt_cmesh_init_adapt_geometry (sc_MPI_Comm comm)
+{
+  t8_cmesh_t          cmesh = t8_cmesh_new_from_class (T8_ECLASS_TET, comm);
+  T8_ASSERT (!t8_cmesh_is_partitioned (cmesh));
+  return cmesh;
+}
+
+t8_forest_t
+t8_adapt_cmesh_init_forest (sc_MPI_Comm comm, int level)
+{
+  t8_cmesh_t          cmesh =
+    t8_cmesh_new_hypercube (T8_ECLASS_HEX, comm, 0, 0, 0);
+  t8_scheme_cxx_t    *scheme = t8_scheme_new_default_cxx ();
+  t8_forest_t         forest =
+    t8_forest_new_uniform (cmesh, scheme, level, 0, comm);
+
+  return forest;
+}
 
 int
 main (int argc, char **argv)
@@ -34,6 +56,8 @@ main (int argc, char **argv)
   char                help[BUFSIZ];
   int                 helpme;
   int                 parsed;
+  int                 level;
+  const sc_MPI_Comm   comm = sc_MPI_COMM_WORLD;
 
   /* long help message */
   snprintf (help, BUFSIZ,
@@ -41,7 +65,7 @@ main (int argc, char **argv)
   mpiret = sc_MPI_Init (&argc, &argv);
   SC_CHECK_MPI (mpiret);
 
-  sc_init (sc_MPI_COMM_WORLD, 1, 1, NULL, SC_LP_ESSENTIAL);
+  sc_init (comm, 1, 1, NULL, SC_LP_ESSENTIAL);
 #ifdef T8_ENABLE_DEBUG
   t8_init (SC_LP_DEBUG);
 #else
@@ -54,6 +78,9 @@ main (int argc, char **argv)
   sc_options_add_switch (opt, 'h', "help", &helpme,
                          "Display a short help message.");
 
+  sc_options_add_int (opt, 'l', "level", &level, 0,
+                      "The minimum refinement level of the forest.");
+
   parsed =
     sc_options_parse (t8_get_package_id (), SC_LP_ERROR, opt, argc, argv);
   if (helpme) {
@@ -62,8 +89,18 @@ main (int argc, char **argv)
     sc_options_print_usage (t8_get_package_id (), SC_LP_ERROR, opt, NULL);
   }
   else if (parsed >= 0 /* TODO: Check correct arguments */ ) {
+    t8_cmesh_t          cmesh_to_adapt_from =
+      t8_adapt_cmesh_init_adapt_geometry (comm);
 
-    /* Code goes here */
+    /* - cmesh initialisieren */
+    t8_forest_t         forest = t8_adapt_cmesh_init_forest (comm, level);
+    /* - cmesh und forest initialisieren. */
+
+    /* Identifiziere Element in forest, die einen Mittelpunkt eines
+       Elements in  cmesh_to_adapt_from enthalten. */
+    /* Adaptiere forest, so dass alle identifizierten Elemente verfeienrt werden. */
+    t8_cmesh_destroy (&cmesh_to_adapt_from);
+    t8_forest_unref (&forest);
   }
   else {
     /* wrong usage */
