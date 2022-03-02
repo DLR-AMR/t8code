cdba70bdc64928913829915c914d9fb45c698652
Sandro Elsweijer <sandro.elsweijer@gmail.com>
2022-03-02 16:32:52 +0100

    enabled vtk api output

--- example/advect/t8_advection.cxx
+++ example/advect/t8_advection.cxx
@@ -1401,8 +1401,8 @@ t8_advect_write_vtk (t8_advect_problem_t * problem)
   /* Write filename */
   snprintf (fileprefix, BUFSIZ, "advection_%03i", problem->vtk_count);
   /* Write vtk files */
-  if (t8_forest_vtk_write_file (problem->forest, fileprefix,
-                                1, 1, 1, 1, 0, 4, vtk_data)) {
+  if (t8_forest_write_vtk_via_API (problem->forest, fileprefix,
+                                1, 1, 1, 1, 1, 4, vtk_data)) {
     t8_debugf ("[Advect] Wrote pvtu to files %s\n", fileprefix);
   }
   else {
