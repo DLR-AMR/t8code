8058cd2d08f86da4d1ab0f87af0c7f23fc168632
Ioannis Lilikakis <73935606+ililikakis@users.noreply.github.com>
2022-03-21 09:02:32 +0100

    Update src/t8_forest.h: set is_family const
    Co-authored-by: Davknapp <david.knapp@dlr.de>

--- src/t8_forest.h
+++ src/t8_forest.h
@@ -121,7 +121,7 @@ typedef int         (*t8_forest_adapt_t) (t8_forest_t forest,
                                           t8_locidx_t which_tree,
                                           t8_locidx_t lelement_id,
                                           t8_eclass_scheme_c * ts,
-                                          int is_family,
+                                          const int is_family,
                                           int num_elements,
                                           t8_element_t * elements[]);
 
