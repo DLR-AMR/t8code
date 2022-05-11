016f3a2756eb6efe89723ff26b6b4e981f4524a6
Knapp <knap_da@sc-030182l.intra.dlr.de>
2022-05-11 15:59:17 +0200

    Remove obsolete indent-guards

--- src/t8_schemes/t8_default/t8_default_common/t8_default_common_cxx.cxx
+++ src/t8_schemes/t8_default/t8_default_common/t8_default_common_cxx.cxx
@@ -140,11 +140,8 @@ t8_default_scheme_common_c::t8_element_count_leafs (const t8_element_t *t,
 /* Count the number of siblings.
  * The number of children is 2^dim for each element, except for pyramids.
  * TODO: For pyramids we will have to implement a standalone version in the pyramid scheme. */
-/* *INDENT-OFF* */
-/* Indent bug: indent adds an additional const */
 int
-t8_default_scheme_common_c::t8_element_num_siblings (const t8_element_t * elem) const
-/* *INDENT-ON* */
+t8_default_scheme_common_c::t8_element_num_siblings (const t8_element_t *elem) const
 {
   const int           dim = t8_eclass_to_dimension[eclass];
   return sc_intpow (2, dim);
