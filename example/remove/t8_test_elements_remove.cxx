cfa69af32e008e2a8fa6830579c6b863cd4f55a9
Ioannis Lilikakis <info@lilikakis.de>
2022-05-11 16:43:54 +0200

    Added new include

--- example/remove/t8_test_elements_remove.cxx
+++ example/remove/t8_test_elements_remove.cxx
@@ -23,6 +23,7 @@
 #include <t8.h>
 #include <t8_eclass.h>
 #include <t8_cmesh.h>
+#include <t8_cmesh/t8_cmesh_examples.h>
 #include <t8_forest.h>
 #include <t8_vec.h>
 #include <t8_schemes/t8_default_cxx.hxx>
