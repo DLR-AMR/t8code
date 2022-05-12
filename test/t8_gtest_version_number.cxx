288beb382067fca3998d3778f4ef6c1730097f9d
Johannes Holke <johannes.holke@dlr.de>
2022-05-12 17:34:10 +0200

    Start with testing the version number

---
+++ test/t8_gtest_version_number.cxx
@@ -0,0 +1,35 @@
+/*
+  This file is part of t8code.
+  t8code is a C library to manage a collection (a forest) of multiple
+  connected adaptive space-trees of general element types in parallel.
+
+  Copyright (C) 2010 The University of Texas System
+  Written by Carsten Burstedde, Lucas C. Wilcox, and Tobin Isaac
+
+  t8code is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  t8code is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with t8code; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#include <gtest/gtest.h>
+#include <t8.h>
+
+/* Test whether the return value of t8_get_version_string
+ * matches T8_PACKAGE_STRING.
+ */
+TEST (t8_gtest_version_number, getter_function)
+{
+  const char         *version_number = t8_get_version_string ();
+
+  EXPECT_EQ (0, strcmp (version_number, T8_PACKAGE_STRING));
+}
