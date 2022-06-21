dbaa2e70271bc185a42872550d10aee4e143e9c5
Knapp <knap_da@sc-030182l.intra.dlr.de>
2022-06-21 08:58:55 +0200

    doxygen return for refines_irregular for tets

--- src/t8_schemes/t8_default/t8_default_tet/t8_default_tet_cxx.hxx
+++ src/t8_schemes/t8_default/t8_default_tet/t8_default_tet_cxx.hxx
@@ -287,6 +287,7 @@ public:
 
   /** Returns true, if there is one element in the tree, that does not refine into 2^dim children.
    * Returns false otherwise.
+   * * \return           0, because tets refine regularly
    */
   virtual int         t8_element_refines_irregular (void);
 
