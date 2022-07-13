9c34fbeb2eb092499c727034155a676c59a9588d
Florian Becker <f.becker@dlr.de>
2022-07-13 17:14:02 +0200

    Add a MACRO to switch between default quad and quad with subelements. There is a bug here, when calling LFN.

--- example/transition/t8_advection_transition.cxx
+++ example/transition/t8_advection_transition.cxx
@@ -34,10 +34,17 @@
  *  4) Additionally, one can freely set the CFL value, end time step T, init and adapt level, coarse mesh configuration etc..
  */
 
+#define USE_QUAD_TRANSITION 1
+
 #include <sc_options.h>
 #include <sc_statistics.h>
-#include <t8_schemes/t8_quads_transition/t8_transition/t8_transition_quad_cxx.hxx>
-#include <t8_schemes/t8_quads_transition/t8_transition_cxx.hxx>
+#if USE_QUAD_TRANSITION
+  #include <t8_schemes/t8_quads_transition/t8_transition/t8_transition_quad_cxx.hxx>
+  #include <t8_schemes/t8_quads_transition/t8_transition_cxx.hxx>
+#else 
+  #include <t8_schemes/t8_default_cxx.hxx>
+  #include <t8_schemes/t8_default/t8_default_quad_cxx.hxx>
+#endif 
 #include <t8_forest.h>
 #include <t8_forest/t8_forest_iterate.h>
 #include <t8_forest/t8_forest_partition.h>
@@ -2007,8 +2014,13 @@ t8_advect_create_cmesh (sc_MPI_Comm comm, t8_eclass_t eclass,
     cmesh = t8_cmesh_from_msh_file (mshfile, 1, comm, dim, 0);
     /* partition this cmesh according to the initial refinement level */
     t8_cmesh_init (&cmesh_partition);
+#if USE_QUAD_TRANSITION
     t8_cmesh_set_partition_uniform (cmesh_partition, level,
                                     t8_scheme_new_subelement_cxx ());
+#else
+    t8_cmesh_set_partition_uniform (cmesh_partition, level,
+                                      t8_scheme_new_default_cxx ());
+#endif                                    
     t8_cmesh_set_derive (cmesh_partition, cmesh);
     t8_cmesh_commit (cmesh_partition, comm);
     return cmesh_partition;
@@ -2109,7 +2121,11 @@ t8_advect_problem_init (t8_cmesh_t cmesh,
   }
 
   /* Contruct uniform forest with ghosts */
+#if USE_QUAD_TRANSITION
   default_scheme = t8_scheme_new_subelement_cxx ();
+#else
+  default_scheme = t8_scheme_new_default_cxx ();
+#endif
 
   problem->forest =
     t8_forest_new_uniform (cmesh, default_scheme, level, 1, comm);
@@ -2962,7 +2978,7 @@ main (int argc, char *argv[])
                       "if their volume is smaller than the l+V-times refined\n"
                       " smallest element int the mesh.");
 
-  sc_options_add_int (opt, 't', "transition", &do_transition, 1,
+  sc_options_add_int (opt, 't', "transition", &do_transition, 0,
                       "Transition the forest such that it is conformal.");
 
   parsed =
