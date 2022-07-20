7399a05fa95608840d2d50095c3462e6b7907cf3
Dreyer, Lukas <Lukas.Dreyer@dlr.de>
2022-07-20 09:47:36 +0200

    Adjust element documentation for sibling

--- src/t8_element_cxx.hxx
+++ src/t8_element_cxx.hxx
@@ -143,15 +143,13 @@ public:
   /** Compute a specific sibling of a given element \b elem and store it in \b sibling.
    *  \b sibling needs to be an existing element. No memory is allocated by this function.
    *  \b elem and \b sibling can point to the same element, then the entries of
-   *  \b elem are overwritten by the ones of its i-th sibling.
-   * \param [in] elem   The element whose parent will be computed.
+   *  \b elem are overwritten by the ones of its sibid-th sibling.
+   * \param [in] elem   The element whose sibling will be computed.
    * \param [in] sibid  The id of the sibling computed.
    * \param [in,out] sibling This element's entries will be overwritten by those
    *                    of \b elem's sibid-th sibling.
    *                    The storage for this element must exist
    *                    and match the element class of the sibling.
-   *                    For a pyramid, for example, it may be either a
-   *                    tetrahedron or a pyramid depending on \b sibid.
    */
   virtual void        t8_element_sibling (const t8_element_t *elem,
                                           int sibid,
