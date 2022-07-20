23790dc8daef2b80ee4188c9cf8ea82a1167f04d
Dreyer, Lukas <Lukas.Dreyer@dlr.de>
2022-07-20 09:49:09 +0200

    Fix documentation for specific element sibling functions

--- src/t8_schemes/t8_default/t8_default_tet/t8_default_tet_cxx.hxx
+++ src/t8_schemes/t8_default/t8_default_tet/t8_default_tet_cxx.hxx
@@ -126,11 +126,16 @@ public:
   virtual void        t8_element_parent (const t8_element_t *elem,
                                          t8_element_t *parent);
 
-  /** Compute the number of siblings of an element. That is the number of 
-   * Children of its parent.
-   * \param [in] elem The element.
-   * \return          The number of siblings of \a element.
-   * Note that this number is >= 1, since we count the element itself as a sibling.
+  /** Compute a specific sibling of a given tet element \b elem and store it in \b sibling.
+   *  \b sibling needs to be an existing element. No memory is allocated by this function.
+   *  \b elem and \b sibling can point to the same element, then the entries of
+   *  \b elem are overwritten by the ones of its \b sibid -th sibling.
+   * \param [in] elem   The element whose sibling will be computed.
+   * \param [in] sibid  The id of the sibling computed.
+   * \param [in,out] sibling This element's entries will be overwritten by those
+   *                    of \b elem's sibid-th sibling.
+   *                    The storage for this element must exist
+   *                    and match the element class of the sibling.
    */
   virtual void        t8_element_sibling (const t8_element_t *elem,
                                           int sibid, t8_element_t *sibling);
