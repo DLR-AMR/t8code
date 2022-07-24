cf150f67f6a3afb760effb64c5a2dc900ea803e4
Jakob Hördt <jakoblhoerdt@gmail.com>
2022-07-24 12:28:27 +0200

    document new parameters for t8_forest_write_netcdf_ext

--- src/t8_forest_netcdf.h
+++ src/t8_forest_netcdf.h
@@ -33,7 +33,7 @@
 T8_EXTERN_C_BEGIN ();
 
 /** Creates a netCDF-4 file containing the (geometrical) information about the given forest mesh and additional elementwise data variables
- * This functions calls 't8_forest_write_netcdf_ext()' with default values for 'netcdf_var_storage_mode' (NC_CONTIGUOUS) and 'netcdf_var_mpi_access' (NC_INDEPENDENT).
+ * This functions calls 't8_forest_write_netcdf_ext()' with default values for 'netcdf_var_storage_mode' (NC_CONTIGUOUS), 'netcdf_var_mpi_access' (NC_INDEPENDENT), 'coordinate_chunksize' (nullptr), fill_mode (NC_NOFILL), cmode (NC_NETCDF4), and multifile_mode (false).
  * \param [in]  forest    A forest.
  * \param [in]  file_prefix    A string which holds the file's name (output file will be 'file_prefix.nc').
  * \param [in]  file_title    A string to caption the NetCDF-File.
@@ -58,8 +58,11 @@ void                t8_forest_write_netcdf (t8_forest_t forest,
  * \param [in]  num_extern_netcdf_vars    The number of extern user-defined variables which hold elementwise data (if none, set it to 0).
  * \param [in]  ext_variables An array of pointers of the herein before mentioned user-defined variables (if none, set it to NULL).
  * \param [in]  comm The sc_MPI_Communicator to use.
- * \param [in]  netcdf_var_storage_mode This defines whether the variables in the netCDF-4 File are stored contiguously (NC_CONTIGUOUS) or in chunks (NC_CHUNKED (in this case netCDF's default chunking scheme is used)).
+ * \param [in]  netcdf_var_storage_mode This defines whether the variables in the netCDF-4 File are stored contiguously (NC_CONTIGUOUS) or in chunks (NC_CHUNKED (in this case netCDF's defNC_NETCDF4 single size_t indicating the chunksize used only for the coordinate variables. May be nullptr indicating the netcdf default
  * \param [in]  netcdf_var_mpi_access This defines whether the mpi processes are writing the variables' data collectively or independently.
+ * \param [in]  fill_mode the fillmode used for the netcdf file. Must be either NC_NOFILL or NC_FILL
+ * \param [in]  cmode the given flags are potentially combined with other flags (eg. NC_CLOBBER) and given to nc_create[_par]. Supported modes are NC_NETCDF4 and NC_CLASSIC_MODEL | NC_64BIT_DATA.
+ * \param [in]  multifile_mode if true, every MPI rank writes its local data to its own file, which can have performance benefits but leaves you with many files. In this mode, netcdf_var_mpi_access is irrelevant and ignored.
  * \note Only use this extended version of \ref t8_forest_write_netcdf if you know exactly what you are doing. In all other cases, we recommend using \ref t8_forest_write_netcdf instead.
  */
 void                t8_forest_write_netcdf_ext (t8_forest_t forest,
@@ -73,7 +76,7 @@ void                t8_forest_write_netcdf_ext (t8_forest_t forest,
                                                 int netcdf_var_storage_mode,
                                                 const size_t
                                                 *coordinate_chunksize,
-                                                int netcdf_mpi_access,
+                                                int netcdf_var_mpi_access,
                                                 int fill_mode, int cmode,
                                                 bool multifile_mode);
 
