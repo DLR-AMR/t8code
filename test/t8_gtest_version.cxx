caab998eb35c52ac2da619aebf1763f1ff486c7e
Johannes Holke <johannes.holke@dlr.de>
2022-08-30 14:26:14 +0200

    Update version number in test

--- test/t8_gtest_version.cxx
+++ test/t8_gtest_version.cxx
@@ -27,7 +27,7 @@
 /* The following three tests check whether t8code computes the correct
  * version.
  * The current version of t8code is
- *    0.12.0
+ *    0.13.0
  * If you increase the major or minor version number, you need to adjust these tests and
  * this comment. */
 
@@ -42,7 +42,7 @@ TEST (t8_gtest_version, major_version)
 TEST (t8_gtest_version, minor_version)
 {
   /* Change this number when you increase the minor version. */
-  const int           minor_version = 12;
+  const int           minor_version = 13;
 
   EXPECT_EQ (t8_get_version_minor (), minor_version);
 }
