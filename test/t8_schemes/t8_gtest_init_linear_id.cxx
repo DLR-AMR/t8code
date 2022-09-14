dd2a4267b9fac579b8cc81f08529d40f9e3e0286
Johannes Holke <johannes.holke@dlr.de>
2022-09-14 16:35:42 +0200

    Update test/t8_schemes/t8_gtest_init_linear_id.cxx

--- test/t8_schemes/t8_gtest_init_linear_id.cxx
+++ test/t8_schemes/t8_gtest_init_linear_id.cxx
@@ -70,7 +70,6 @@ TEST_P (linear_id, uniform_forest) {
   t8_forest_t forest, forest_adapt;
   t8_cmesh_t cmesh;
   t8_locidx_t num_elements_in_tree, id;
-  t8_locidx_t num_local_trees; 
   t8_locidx_t shift;
   t8_element_t *element;
   t8_eclass_t   tree_class;
