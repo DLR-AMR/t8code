004d9bbdafae19f016b70007e003c7f0162c029a
Weber <webe_ji@sc-0081vml.intra.dlr.de>
2023-02-17 15:47:52 +0100

    change exception to assertion and add messages

--- test/t8_gtest_vtk_linkage.cxx
+++ test/t8_gtest_vtk_linkage.cxx
@@ -53,7 +53,13 @@ TEST (t8_gtest_vtk_linkage, t8_test_vtk_version_number)
   char                vtk_version[BUFSIZ];
   snprintf (vtk_version, BUFSIZ, "%i.%i", VTK_MAJOR_VERSION,
             VTK_MINOR_VERSION);
-  EXPECT_FALSE (strcmp (T8_VTK_VERSION_USED, vtk_version));
+  ASSERT_FALSE (strcmp (T8_VTK_VERSION_USED, vtk_version)) <<
+    "linked vtk version (" << vtk_version <<
+    ") does not equal the version t8code was configured with (" <<
+    T (T8_VTK_VERSION_USED << ").\n");
+  if (!strcmp (T8_VTK_VERSION_USED, vtk_version)) {
+    t8_debugf ("Using vtk version %s.\n", vtk_verion);
+  }
 #endif
 }
 
@@ -63,10 +69,8 @@ TEST (t8_gtest_vtk_linkage, t8_test_vtk_linkage)
 #if T8_WITH_VTK
   vtkNew < vtkUnstructuredGrid > unstructuredGrid;
 
-  t8_global_productionf
-    ("Successfully created VTK unstructuredGrid object.\n");
+  t8_debugf ("Successfully created VTK unstructuredGrid object.\n");
 #else
-  t8_global_productionf
-    ("This version of t8code is not compiled with vtk support.\n");
+  t8_debugf ("This version of t8code is not compiled with vtk support.\n");
 #endif
 }
