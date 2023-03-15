dee51f0c9a3803cccc59ad34101422c5640d31ca
Knapp <knap_da@sc-030182l.intra.dlr.de>
2023-03-15 09:12:23 +0100

    Replace all uint64_t by t8_linearidx_t

--- src/t8_schemes/t8_default/t8_default_prism/t8_default_prism_cxx.cxx
+++ src/t8_schemes/t8_default/t8_default_prism/t8_default_prism_cxx.cxx
@@ -383,7 +383,8 @@ t8_default_scheme_prism_c::t8_element_face_neighbor_inside (const t8_element_t
 
 void
 t8_default_scheme_prism_c::t8_element_set_linear_id (t8_element_t *elem,
-                                                     int level, uint64_t id)
+                                                     int level,
+                                                     t8_linearidx_t id)
 {
   T8_ASSERT (0 <= level && level <= T8_DPRISM_MAXLEVEL);
   T8_ASSERT (0 <= id && id < ((u_int64_t) 1) << 3 * level);
