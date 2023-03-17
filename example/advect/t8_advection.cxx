13abeaff6d9f770c65833a5247754c94182b7465
Sandro Elsweijer <sandro.elsweijer@gmail.com>
2023-03-17 08:58:33 +0100

    mention .geo files for flow in help message

--- example/advect/t8_advection.cxx
+++ example/advect/t8_advection.cxx
@@ -1711,7 +1711,9 @@ main (int argc, char *argv[])
                       "\t\t7 - Flow past a rotating cylinder of radius of 0.5"
                       " around the z-axis. This flow is defined for a specific"
                       " mesh, which can be generated with Gmsh and the .geo"
-                      " files. These meshes can also be used with the curved geometry.\n");
+                      " files 't8_advection_generate_channel.geo' and"
+                      " 't8_advection_generate_channel_2d.geo'. These meshes"
+                      " can also be used with the curved geometry.\n");
   sc_options_add_int (opt, 'l', "level", &level, 0,
                       "The minimum refinement level of the mesh.");
   sc_options_add_int (opt, 'r', "rlevel", &reflevel, 0,
