1c046aa304a5713b7be72889f0984de2f03e21b7
Sandro Elsweijer <sandro.elsweijer@gmail.com>
2023-04-13 11:02:25 +0200

    remove code fragment

--- src/t8_forest/t8_forest_cxx.cxx
+++ src/t8_forest/t8_forest_cxx.cxx
@@ -235,10 +235,6 @@ t8_forest_element_from_ref_coords (t8_forest_t forest, t8_locidx_t ltreeid,
     t8_forest_get_eclass_scheme (forest, tree_class);
   scheme->t8_element_reference_coords (element, ref_coords, NULL,
                                        tree_ref_coords);
-  if (stretch_factors != NULL) {
-    //get element_midpoint
-    //stretch ref_coords by factor stored in array
-  }
   const t8_cmesh_t    cmesh = t8_forest_get_cmesh (forest);
   const t8_gloidx_t   gtreeid = t8_forest_global_tree_id (forest, ltreeid);
   t8_geometry_evaluate (cmesh, gtreeid, tree_ref_coords, coords_out);
