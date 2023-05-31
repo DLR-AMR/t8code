c5483ce04bc701a18503ced4a62c4b9e9b1a4ef8
ililikakis <info@lilikakis.de>
2023-05-31 16:07:34 +0200

    add documentation

--- test/t8_forest_incomplete/t8_gtest_permute_hole.cxx
+++ test/t8_forest_incomplete/t8_gtest_permute_hole.cxx
@@ -32,27 +32,30 @@
 
 #define MAX_NUM_ELEMETS 32
 
-/**
+/* In this test, a uniform forest with x many elements is given. 
+ * There are 2^x many ways to remove these x elements. After removing the elements,
+ * the forest is coarsened and checked for overlapping elements.
+ * 
  * Example:
- * num_elements = 4
- * num_permutations = 16
- * permutation - bianry with num_elements many digits
- *      0      -   0 0 0 0
- *      1      -   0 0 0 1
- *      2      -   0 0 1 0
- *      3      -   0 0 1 1
- *      4      -   0 1 0 0
- *      5      -   0 1 0 1
- *      6      -   0 1 1 0
- *      7      -   0 1 1 1
- *      8      -   1 0 0 0
- *      9      -   1 0 0 1
- *      10     -   1 0 1 0
- *      11     -   1 0 1 1
- *      12     -   1 1 0 0
- *      13     -   1 1 0 1
- *      14     -   1 1 1 0
- *      15     -   1 1 1 1
+ * num of elements = 4
+ * instances - bianry with num elements many digits
+ *      0    -    0 0 0 0
+ *      1    -    0 0 0 1
+ *      2    -    0 0 1 0
+ *      3    -    0 0 1 1
+ *      4    -    0 1 0 0
+ *      5    -    0 1 0 1
+ *      6    -    0 1 1 0
+ *      7    -    0 1 1 1
+ *      8    -    1 0 0 0
+ *      9    -    1 0 0 1
+ *      10   -    1 0 1 0
+ *      11   -    1 0 1 1
+ *      12   -    1 1 0 0
+ *      13   -    1 1 0 1
+ *      14   -    1 1 1 0
+ *      15   -    1 1 1 1
+ * We remove every element with id i if the i`th bit in the current instances is 0.
  */
 
 /* *INDENT-OFF* */
