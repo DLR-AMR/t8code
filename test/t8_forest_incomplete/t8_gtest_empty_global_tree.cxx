7831efc55fb60db45c88568594768e983f2a62c3
ililikakis <info@lilikakis.de>
2023-06-21 17:27:00 +0200

    extended test

--- test/t8_forest_incomplete/t8_gtest_empty_global_tree.cxx
+++ test/t8_forest_incomplete/t8_gtest_empty_global_tree.cxx
@@ -30,7 +30,7 @@
 /** In this test, we are given a forest with 3 global trees. 
  * We adapt the forest so that all 6 compositions of empty 
  * global trees are the result of it. 
- * Therefore, \a instance runs from 0 to 7.
+ * Therefore, \a instance runs from 0 to 5.
  * We do this twice. Once we partition the forest in the same call. 
  * The second time, we do the adapting and partitioning separately.
  * The two resulting forests must be equal.
@@ -43,8 +43,9 @@ protected:
     eclass = std::get<0>(GetParam());
     instance = std::get<1>(GetParam());
     forest = 
-      t8_forest_new_uniform (t8_cmesh_new_bigmesh (eclass, 3, sc_MPI_COMM_WORLD), t8_scheme_new_default_cxx (), 0, 0, sc_MPI_COMM_WORLD);  
-    /* we skip this test until feature is fully supported */
+      t8_forest_new_uniform (t8_cmesh_new_bigmesh (eclass, 3, sc_MPI_COMM_WORLD),
+                             t8_scheme_new_default_cxx (), 0, 0, sc_MPI_COMM_WORLD);  
+    /* Remove if partitioning empty trees from processes with no elements works */
     GTEST_SKIP ();
   }
   void TearDown () override {
@@ -65,13 +66,12 @@ t8_adapt_remove (t8_forest_t forest,
                  t8_locidx_t lelement_id,
                  t8_eclass_scheme_c *ts,
                  const int is_family,
-                 const int num_elements,
-                 t8_element_t *elements[])
+                 const int num_elements, t8_element_t *elements[])
 {
-  const int *inst = (const int *) t8_forest_get_user_data (forest);
-  const t8_gloidx_t global_tree_id = t8_forest_global_tree_id (forest_from, which_tree);
-  switch (*inst)
-  {
+  const int          *inst = (const int *) t8_forest_get_user_data (forest);
+  const t8_gloidx_t   global_tree_id =
+    t8_forest_global_tree_id (forest_from, which_tree);
+  switch (*inst) {
   case 0:
     if (global_tree_id == 0) {
       return -2;
@@ -102,23 +102,17 @@ t8_adapt_remove (t8_forest_t forest,
       return -2;
     }
     break;
-  case 6:
-    return -2;
-    break;
   default:
-    return 0;
     break;
   }
-  
+
   return 0;
 }
 
 t8_forest_t
 t8_adapt_forest (t8_forest_t forest_from,
                  t8_forest_adapt_t adapt_fn,
-                 int do_adapt,
-                 int do_partition,
-                 void *user_data)
+                 int do_adapt, int do_partition, void *user_data)
 {
   t8_forest_t         forest_new;
 
@@ -146,22 +140,40 @@ TEST_P (global_tree, test_empty_global_tree)
 
   t8_forest_ref (forest);
   /* Do adapt and partition in one step */
-  t8_forest_t         forest_adapt_a = t8_adapt_forest (forest, t8_adapt_remove, 1, 1, &instance);
+  t8_forest_t         forest_adapt_a =
+    t8_adapt_forest (forest, t8_adapt_remove, 1, 1, &instance);
   ASSERT_TRUE (forest_adapt_a->incomplete_trees);
   ASSERT_TRUE (!forest->incomplete_trees);
 
   t8_forest_ref (forest);
   /* Do adapt and partition in seperate steps */
-  t8_forest_t         forest_adapt_b = t8_adapt_forest (forest, t8_adapt_remove, 1, 0, &instance);
+  t8_forest_t         forest_adapt_b =
+    t8_adapt_forest (forest, t8_adapt_remove, 1, 0, &instance);
   ASSERT_TRUE (forest_adapt_b->incomplete_trees);
   ASSERT_TRUE (!forest->incomplete_trees);
-  forest_adapt_b = t8_adapt_forest (forest, NULL, 0, 1, NULL);
+  forest_adapt_b = t8_adapt_forest (forest_adapt_b, NULL, 0, 1, NULL);
 
   /* The number of trees and elements between forest_adapt_a and forest_adapt_b have to match. */
   /* Global */
-  ASSERT_EQ (t8_forest_get_num_global_trees (forest), t8_forest_get_num_global_trees (forest_adapt_a));
-  ASSERT_EQ (t8_forest_get_num_global_trees (forest_adapt_b), t8_forest_get_num_global_trees (forest_adapt_a));
-  ASSERT_EQ (t8_forest_get_global_num_elements (forest_adapt_b), t8_forest_get_global_num_elements (forest_adapt_a));
+  ASSERT_EQ (t8_forest_get_num_global_trees (forest),
+             t8_forest_get_num_global_trees (forest_adapt_a));
+  ASSERT_EQ (t8_forest_get_num_global_trees (forest_adapt_b),
+             t8_forest_get_num_global_trees (forest_adapt_a));
+  ASSERT_EQ (t8_forest_get_global_num_elements (forest_adapt_b),
+             t8_forest_get_global_num_elements (forest_adapt_a));
+
+  /* Compare forest->global_num_trees with the sum of all local trees
+   * on all processes. Those numbers must be equal, since every tree
+   * contains only the root element and no tree is shared. */
+  t8_gloidx_t         local_num_trees =
+    (t8_gloidx_t) t8_forest_get_num_local_trees (forest_adapt_a);
+  t8_gloidx_t         global_num_trees;
+  int                 mpiret =
+    sc_MPI_Allreduce (&local_num_trees, &global_num_trees, 1,
+                      MPI_LONG_LONG_INT, sc_MPI_SUM, sc_MPI_COMM_WORLD);
+  SC_CHECK_MPI (mpiret);
+  ASSERT_EQ (global_num_trees, t8_forest_get_num_global_trees (forest));
+
   /* Local */
   ASSERT_TRUE (t8_forest_is_equal (forest_adapt_b, forest_adapt_a));
 
@@ -170,5 +182,5 @@ TEST_P (global_tree, test_empty_global_tree)
 }
 
 /* *INDENT-OFF* */
-INSTANTIATE_TEST_SUITE_P (t8_gtest_empty_global_tree, global_tree, testing::Combine(testing::Range(T8_ECLASS_ZERO, T8_ECLASS_COUNT), testing::Range(0,8)));
+INSTANTIATE_TEST_SUITE_P (t8_gtest_empty_global_tree, global_tree, testing::Combine(testing::Range(T8_ECLASS_ZERO, T8_ECLASS_COUNT), testing::Range(0,6)));
 /* *INDENT-ON* */
