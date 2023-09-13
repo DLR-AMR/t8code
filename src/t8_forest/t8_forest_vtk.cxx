876be4df6b6488e7bc01662053a1c8123892a025
David Knapp <david.knapp@dlr.de>
2023-09-13 16:13:45 +0200

    Use AddArray() instead of SetScalar/SetVector
    The old version only overwrites the latest array, the new version adds another array

--- src/t8_forest/t8_forest_vtk.cxx
+++ src/t8_forest/t8_forest_vtk.cxx
@@ -551,6 +551,7 @@ t8_forest_to_vtkUnstructuredGrid (t8_forest_t forest, vtkSmartPointer<vtkUnstruc
     dataArrays[idata]->SetNumberOfTuples (num_elements);       /* We want number of tuples=number of elements */
     dataArrays[idata]->SetNumberOfComponents (num_components); /* Each tuples has 3 values */
     dataArrays[idata]->SetVoidArray (data[idata].data, num_elements * num_components, 1);
+    unstructuredGrid->GetCellData ()->AddArray (dataArrays[idata]);
     if (num_components == 1) {
       unstructuredGrid->GetCellData ()->SetScalars (dataArrays[idata]);
     }
