e1247bc05b2876923ce94ec8ae58584ca4f871de
David Knapp <david.knapp@dlr.de>
2023-10-13 23:35:18 +0200

    Added documentation to test

--- test/t8_forest/t8_gtest_tree_face_neigh.cxx
+++ test/t8_forest/t8_gtest_tree_face_neigh.cxx
@@ -58,23 +58,20 @@ class tree_face_neigh: public testing::TestWithParam<t8_eclass_t> {
 
 TEST_P (tree_face_neigh, t8_forest_neigh_face_test)
 {
+  /*Iterate over all faces of the tree */
   for (int face = 0; face < t8_eclass_num_faces[eclass]; face++) {
     const int face_type = t8_eclass_face_types[eclass][face];
     const int num_vertices = t8_eclass_num_vertices[face_type];
+    /* Check all possible orientations */
     for (int orientation = 0; orientation < num_vertices; orientation++) {
       t8_debugf ("[D] ------------------Test: %s face %i orientation %i -----------------\n",
                  t8_eclass_to_string[eclass], face, orientation);
       char fileprefix[BUFSIZ];
       t8_cmesh_t cmesh = t8_cmesh_new_two_trees_face_orientation (eclass, face, orientation, sc_MPI_COMM_WORLD);
-      t8_forest_t forest;
       scheme = t8_scheme_new_default_cxx ();
-      t8_forest_init (&forest);
-      t8_forest_set_level (forest, 0);
-      t8_forest_set_cmesh (forest, cmesh, comm);
-      t8_forest_set_scheme (forest, scheme);
-      t8_forest_commit (forest);
+      t8_forest_t forest = t8_forest_new_uniform (cmesh, scheme, 0, 0, comm);
       t8_element_t *elem = t8_forest_get_element_in_tree (forest, 0, 0);
-
+      /**/
       const int face_class = t8_eclass_face_types[eclass][face];
       t8_eclass_scheme_c *ts = scheme->eclass_schemes[eclass];
       t8_eclass_scheme_c *face_scheme = scheme->eclass_schemes[face_class];
@@ -91,9 +88,12 @@ TEST_P (tree_face_neigh, t8_forest_neigh_face_test)
       const int num_face_vertices = t8_eclass_num_vertices[face_class];
 
       for (int ichild = 0; ichild < num_face_children; ichild++) {
-        t8_debugf ("[D] nfc: %i, ichild %i\n", num_face_children, ichild);
+        /*Get the child at the face. */
         const int child_face = ts->t8_element_face_child_face (elem, face, ichild);
+        /*Construct the face of the child touching the boundary*/
         ts->t8_element_boundary_face (children[ichild], child_face, t0_face, face_scheme);
+
+        /* Transfer the face onto tree 1*/
         t8_locidx_t *face_neighbor;
         int8_t *ttf;
         (void) t8_cmesh_trees_get_tree_ext (cmesh->trees, 0, &face_neighbor, &ttf);
@@ -111,16 +111,19 @@ TEST_P (tree_face_neigh, t8_forest_neigh_face_test)
         const int is_smaller = face <= tree_neigh_face;
         const int sign
           = t8_eclass_face_orientation[eclass][face] == t8_eclass_face_orientation[eclass][tree_neigh_face];
-
+        /* Actual face transfer */
         face_scheme->t8_element_transform_face (t0_face, t1_face, ttf[face] / F, sign, is_smaller);
+        /* Extrude face onto tree 1*/
         const int neigh_face = ts->t8_element_extrude_face (t1_face, face_scheme, face_elem, tree_neigh_face);
         for (int ivertex = 0; ivertex < num_face_vertices; ivertex++) {
+          /* Compute the coordinates of all vertices touching the face of both elements*/
           int t0_face_vertex = t8_face_vertex_to_tree_vertex[eclass][child_face][ivertex];
           int t1_face_vertex = t8_face_vertex_to_tree_vertex[eclass][neigh_face][ivertex];
           double t0_face_vertex_coords[3];
           double t1_face_vertex_coords[3];
           t8_forest_element_coordinate (forest, 0, children[ichild], t0_face_vertex, t0_face_vertex_coords);
           t8_forest_element_coordinate (forest, 1, face_elem, t1_face_vertex, t1_face_vertex_coords);
+          /* If everything is set correct the coordinates should be equal. */
           for (int icoord = 0; icoord < 3; icoord++) {
             EXPECT_NEAR (t0_face_vertex_coords[icoord], t1_face_vertex_coords[icoord], T8_PRECISION_EPS);
           }
