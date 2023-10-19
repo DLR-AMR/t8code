56c2fd97dc527132e4d60c9e6b9ed802a5ea371c
David Knapp <david.knapp@dlr.de>
2023-10-19 14:03:29 +0200

    Added a test for the face-orientation

--- test/t8_gtest_eclass.cxx
+++ test/t8_gtest_eclass.cxx
@@ -23,6 +23,9 @@
 
 #include <gtest/gtest.h>
 #include <t8_eclass.h>
+#include <t8_cmesh.h>
+#include <t8_cmesh/t8_cmesh_examples.h>
+#include <t8_vec.h>
 
 class gtest_eclass: public testing::TestWithParam<int> {
  protected:
@@ -74,6 +77,49 @@ TEST_P (gtest_eclass, t8_to_vtk_corner_numbers)
     const int vtk_corner_number = t8_eclass_t8_to_vtk_corner_number[ieclass][ivertex];
     const int t8_corner_number = t8_eclass_vtk_to_t8_corner_number[ieclass][vtk_corner_number];
     EXPECT_EQ (ivertex, t8_corner_number);
+  }
+}
+
+TEST_P (gtest_eclass, eclass_face_orientation)
+{
+  const int num_faces = t8_eclass_num_faces[ieclass];
+  /* For dim <= 2 all faces are 0 oriented. */
+  if (t8_eclass_to_dimension[ieclass] <= 2) {
+    for (int iface = 0; iface < num_faces; iface++) {
+      EXPECT_EQ (0, t8_eclass_face_orientation[ieclass][iface]);
+    }
+  }
+  else {
+    /* Compute the normal of each face and the signed distance between the normal and the centroid
+     * (corrected wrt to vertex 0. If the sign is positive the face is 0 oriented, 
+     * ow it is 1 oriented. */
+    t8_cmesh_t cmesh = t8_cmesh_new_from_class ((t8_eclass_t) ieclass, sc_MPI_COMM_WORLD);
+    const double *vertices = t8_cmesh_get_tree_vertices (cmesh, 0);
+    for (int iface = 0; iface < num_faces; iface++) {
+      /* clang-format off */
+      const int v[3] = {  t8_face_vertex_to_tree_vertex[ieclass][iface][0], 
+                          t8_face_vertex_to_tree_vertex[ieclass][iface][1],
+                          t8_face_vertex_to_tree_vertex[ieclass][iface][2] };
+      /* clang-format on */
+      double vec_0[3];
+      double vec_1[3];
+      t8_vec_axpyz (vertices + 3 * v[1], vertices + 3 * v[0], vec_0, -1.0);
+      t8_vec_axpyz (vertices + 3 * v[2], vertices + 3 * v[0], vec_1, -1.0);
+      double normal[3];
+      t8_vec_cross (vec_0, vec_1, normal);
+      double centroid[3] = { 0.0 };
+      for (int ivertex = 0; ivertex < t8_eclass_num_vertices[ieclass]; ivertex++) {
+        centroid[0] += vertices[3 * ivertex];
+        centroid[1] += vertices[3 * ivertex + 1];
+        centroid[2] += vertices[3 * ivertex + 2];
+      }
+      t8_vec_ax (centroid, 1.0 / t8_eclass_num_vertices[ieclass]);
+      t8_vec_axpy (vertices + 3 * v[0], centroid, -1.0);
+      const double c_n = t8_vec_dot (centroid, normal);
+      const int orientation = c_n > 0 ? 0 : 1;
+      EXPECT_EQ (orientation, t8_eclass_face_orientation[ieclass][iface]);
+    }
+    t8_cmesh_destroy (&cmesh);
   }
 }
 
