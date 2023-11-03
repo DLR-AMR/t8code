ead3c042e739f8fed0d20b4c556274a9e232b7b8
Dreyer, Lukas <Lukas.Dreyer@dlr.de>
2023-11-03 11:14:33 +0100

    spellcheck and indent

--- test/t8_forest/t8_gtest_ghost_delete.cxx
+++ test/t8_forest/t8_gtest_ghost_delete.cxx
@@ -33,15 +33,18 @@
 /* This test
  */
 
-static int test_adapt_holes (t8_forest_t forest, t8_forest_t forest_from, t8_locidx_t which_tree, t8_locidx_t lelement_id,
-                       t8_eclass_scheme_c *ts, const int is_family, const int num_elements, t8_element_t *elements[])
+static int
+test_adapt_holes (t8_forest_t forest, t8_forest_t forest_from, t8_locidx_t which_tree, t8_locidx_t lelement_id,
+                  t8_eclass_scheme_c *ts, const int is_family, const int num_elements, t8_element_t *elements[])
 {
-    double coordinates[3];
-    double eps = 0.0001;
-    t8_forest_element_coordinate (forest_from, which_tree, elements[0], 0, coordinates);
-    if (fabs(coordinates[1]) < eps) return 1; // refine the lowest row 
-    if (fabs(coordinates[1] - 0.25) < eps) return -2; // refine the lowest row 
-    return 0;
+  double coordinates[3];
+  double eps = 0.0001;
+  t8_forest_element_coordinate (forest_from, which_tree, elements[0], 0, coordinates);
+  if (fabs (coordinates[1]) < eps)
+    return 1;  // refine the lowest row
+  if (fabs (coordinates[1] - 0.25) < eps)
+    return -2;  // refine the lowest row
+  return 0;
 }
 
 class forest_ghost_exchange_holes: public testing::Test {
@@ -52,41 +55,42 @@ class forest_ghost_exchange_holes: public testing::Test {
     /* adjust communicator size */
     int size, rank, color, key;
 
-    sc_MPI_Comm_size(sc_MPI_COMM_WORLD, &size);
-    sc_MPI_Comm_rank(sc_MPI_COMM_WORLD, &rank);
-    t8_debugf("size is %i\n", size);
+    sc_MPI_Comm_size (sc_MPI_COMM_WORLD, &size);
+    sc_MPI_Comm_rank (sc_MPI_COMM_WORLD, &rank);
+    t8_debugf ("size is %i\n", size);
 
-    if (rank < 2){
-        color = 0;
-        key = rank;
-    }else{
-        color = sc_MPI_UNDEFINED;
-        key = -1;
+    if (rank < 2) {
+      color = 0;
+      key = rank;
+    }
+    else {
+      color = sc_MPI_UNDEFINED;
+      key = -1;
     }
 
-    t8_debugf("Color: %i, Key: %i\n", color, key);
+    t8_debugf ("Color: %i, Key: %i\n", color, key);
 
-    sc_MPI_Comm_split(sc_MPI_COMM_WORLD, color, key, &comm);
+    sc_MPI_Comm_split (sc_MPI_COMM_WORLD, color, key, &comm);
 
-    if(comm != sc_MPI_COMM_NULL){
-        sc_MPI_Comm_size(comm, &size);
-        t8_debugf("\nNew size is: %i\n\n", size);
-        scheme = t8_scheme_new_default_cxx ();
-        /* Construct a cmesh */
-        cmesh = t8_cmesh_new_hypercube (T8_ECLASS_QUAD, comm, 0, 0, 0);
-    }else{
-        t8_debugf("\nThis proc has comm NULL \n\n");
+    if (comm != sc_MPI_COMM_NULL) {
+      sc_MPI_Comm_size (comm, &size);
+      t8_debugf ("\nNew size is: %i\n\n", size);
+      scheme = t8_scheme_new_default_cxx ();
+      /* Construct a cmesh */
+      cmesh = t8_cmesh_new_hypercube (T8_ECLASS_QUAD, comm, 0, 0, 0);
     }
-
+    else {
+      t8_debugf ("\nThis proc has comm NULL \n\n");
+    }
   }
   void
   TearDown () override
   {
     //cmesh and scheme are freed taken and freed by forest
-    if(comm != sc_MPI_COMM_NULL){
-      sc_MPI_Comm_free(&comm);
+    if (comm != sc_MPI_COMM_NULL) {
+      sc_MPI_Comm_free (&comm);
     }
-    sc_MPI_Barrier(sc_MPI_COMM_WORLD);
+    sc_MPI_Barrier (sc_MPI_COMM_WORLD);
   }
   sc_MPI_Comm comm;
   t8_scheme_cxx_t *scheme;
@@ -95,11 +99,11 @@ class forest_ghost_exchange_holes: public testing::Test {
 
 TEST_F (forest_ghost_exchange_holes, errorTest)
 {
-  if(comm != sc_MPI_COMM_NULL){
+  if (comm != sc_MPI_COMM_NULL) {
     int level = 1;
     t8_forest_t forest = t8_forest_new_uniform (cmesh, scheme, level, 1, comm);
     forest = t8_forest_new_adapt (forest, test_adapt_holes, 0, 1, NULL);
-    forest = t8_forest_new_adapt (forest, test_adapt_holes, 0, 1, NULL);
-    t8_forest_unref(&forest);
+    forest = t8_forest_new_adapt (forest, test_adapt_holes, 0, 0, NULL);
+    t8_forest_unref (&forest);
   }
 }
