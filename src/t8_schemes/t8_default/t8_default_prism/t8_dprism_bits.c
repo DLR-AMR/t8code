6c33b379e9a8303a4bfb2a372911901448504ffd
Sandro Elsweijer <sandro.elsweijer@dlr.de>
2023-11-09 15:14:52 +0100

    refactor t8_dtri_vertex_coords to t8_dtri_vertex_integer_coords

--- src/t8_schemes/t8_default/t8_default_prism/t8_dprism_bits.c
+++ src/t8_schemes/t8_default/t8_default_prism/t8_dprism_bits.c
@@ -535,7 +535,7 @@ t8_dprism_vertex_integer_coords (const t8_dprism_t *elem, const int vertex, int 
   T8_ASSERT (vertex >= 0 && vertex < 6);
   T8_ASSERT (elem->line.level == elem->tri.level);
   /*Compute x and y coordinate */
-  t8_dtri_compute_coords (&elem->tri, vertex % 3, coords);
+  t8_dtri_compute_integer_coords (&elem->tri, vertex % 3, coords);
   /* Compute z coordinate coords[0] *= T8_DPRISM_ROOT_BY_DTRI_ROOT; */
   t8_dline_vertex_integer_coords (&elem->line, vertex / 3, &coords[2]);
   coords[0] /= T8_DPRISM_ROOT_BY_DTRI_ROOT;
