5729ac5656e96e9191fde80877e2541e7533b2fa
Sandro Elsweijer <sandro.elsweijer@dlr.de>
2023-11-28 16:47:38 +0100

    indent

--- example/geometry/t8_christmas_card.cxx
+++ example/geometry/t8_christmas_card.cxx
@@ -40,83 +40,58 @@
 #define LEVEL_SPARKLE 4
 #define NUM_ROUNDS 50
 
-const std::unordered_map<t8_gloidx_t, int> tree_map =
-{
-    {0, 2},
-    {1, 0},
-    {2, 0},
-    {3, 0},
-    {4, 2},
-    {5, 0},
-    {6, 3},
-    {7, 2},
-    {8, 0},
-    {9, 3},
-    {10, 3},
-    {11, 0},
-    {12, 1},
-    {13, 2},
-    {14, 0},
-    {15, 0},
-    {16, 1},
-    {17, 1},
-    {18, 0},
-    {19, 0},
-    {20, 0}
-};
+const std::unordered_map<t8_gloidx_t, int> tree_map
+  = { { 0, 2 },  { 1, 0 },  { 2, 0 },  { 3, 0 },  { 4, 2 },  { 5, 0 },  { 6, 3 },
+      { 7, 2 },  { 8, 0 },  { 9, 3 },  { 10, 3 }, { 11, 0 }, { 12, 1 }, { 13, 2 },
+      { 14, 0 }, { 15, 0 }, { 16, 1 }, { 17, 1 }, { 18, 0 }, { 19, 0 }, { 20, 0 } };
 
 std::vector<std::pair<double, double>> sparkles;
 
-int t8_adapt_callback(t8_forest_t forest, t8_forest_t forest_from, t8_locidx_t which_tree,
-                              t8_locidx_t lelement_id, t8_eclass_scheme_c *ts, const int is_family,
-                              const int num_elements, t8_element_t *elements[])
+int
+t8_adapt_callback (t8_forest_t forest, t8_forest_t forest_from, t8_locidx_t which_tree, t8_locidx_t lelement_id,
+                   t8_eclass_scheme_c *ts, const int is_family, const int num_elements, t8_element_t *elements[])
 {
   const int elem_level = ts->t8_element_level (elements[0]);
-  const t8_gloidx_t gtreeid = t8_cmesh_get_global_id (t8_forest_get_cmesh(forest_from), which_tree);
-  if (elem_level < tree_map.at(gtreeid))
-  {
+  const t8_gloidx_t gtreeid = t8_cmesh_get_global_id (t8_forest_get_cmesh (forest_from), which_tree);
+  if (elem_level < tree_map.at (gtreeid)) {
     return 1;
   }
-  else if (elem_level > tree_map.at(gtreeid) && is_family)
-  {
+  else if (elem_level > tree_map.at (gtreeid) && is_family) {
     return -1;
   }
   return 0;
 }
 
-int t8_adapt_sparcle_callback(t8_forest_t forest, t8_forest_t forest_from, t8_locidx_t which_tree,
-                              t8_locidx_t lelement_id, t8_eclass_scheme_c *ts, const int is_family,
-                              const int num_elements, t8_element_t *elements[])
+int
+t8_adapt_sparcle_callback (t8_forest_t forest, t8_forest_t forest_from, t8_locidx_t which_tree, t8_locidx_t lelement_id,
+                           t8_eclass_scheme_c *ts, const int is_family, const int num_elements,
+                           t8_element_t *elements[])
 {
   const int elem_level = ts->t8_element_level (elements[0]);
-  const t8_gloidx_t gtreeid = t8_cmesh_get_global_id (t8_forest_get_cmesh(forest_from), which_tree);
+  const t8_gloidx_t gtreeid = t8_cmesh_get_global_id (t8_forest_get_cmesh (forest_from), which_tree);
   int is_inside = 0;
-  for (auto sparkle: sparkles)
-  {
-    const double sparkle_array[3] = {sparkle.first, sparkle.second, 0};
-    if (t8_forest_element_point_inside(forest_from, which_tree, *elements, sparkle_array, 0.01))
-    {
+  for (auto sparkle : sparkles) {
+    const double sparkle_array[3] = { sparkle.first, sparkle.second, 0 };
+    if (t8_forest_element_point_inside (forest_from, which_tree, *elements, sparkle_array, 0.01)) {
       is_inside = 1;
       break;
     }
   }
-  if (elem_level < (is_inside ? LEVEL_SPARKLE + 1 : tree_map.at(gtreeid)))
-  {
+  if (elem_level < (is_inside ? LEVEL_SPARKLE + 1 : tree_map.at (gtreeid))) {
     return 1;
   }
   return 0;
 }
 
-
-int t8_generate_sparkles()
+int
+t8_generate_sparkles ()
 {
-  sparkles.clear();
-  for (int i_sparkle = 0; i_sparkle < NUM_SPARKLES; ++i_sparkle)
-  {
+  sparkles.clear ();
+  for (int i_sparkle = 0; i_sparkle < NUM_SPARKLES; ++i_sparkle) {
     std::pair<double, double> sparkle;
-    sparkle.first = rand() / (double)RAND_MAX * 730.0 - 730 / 2.0;
-    sparkle.second = rand() / (double)RAND_MAX  * 730.0;
-    sparkles.push_back(sparkle);
+    sparkle.first = rand () / (double) RAND_MAX * 730.0 - 730 / 2.0;
+    sparkle.second = rand () / (double) RAND_MAX * 730.0;
+    sparkles.push_back (sparkle);
   }
   return 0;
 }
@@ -131,32 +106,30 @@ main (int argc, char **argv)
   t8_init (SC_LP_DEBUG);
   t8_forest_t forest, forest_new;
   t8_cmesh_t cmesh = t8_cmesh_from_msh_file ("t8_christmas_card", 0, comm, 2, 0, 1);
-  forest = t8_forest_new_uniform(cmesh, t8_scheme_new_default_cxx (), 0, 0, comm);
+  forest = t8_forest_new_uniform (cmesh, t8_scheme_new_default_cxx (), 0, 0, comm);
   t8_forest_init (&forest_new);
-  t8_forest_set_adapt(forest_new, forest, t8_adapt_callback, 1);
-  t8_forest_commit(forest_new);
+  t8_forest_set_adapt (forest_new, forest, t8_adapt_callback, 1);
+  t8_forest_commit (forest_new);
   forest = forest_new;
-  t8_forest_write_vtk_ext(forest, "christmas_card_0", 1, 1, 1, 1, 0, 1, 0, 0, NULL);
-  for (int rounds = 1; rounds < NUM_ROUNDS + 1; ++rounds)
-  {
-    t8_generate_sparkles();
+  t8_forest_write_vtk_ext (forest, "christmas_card_0", 1, 1, 1, 1, 0, 1, 0, 0, NULL);
+  for (int rounds = 1; rounds < NUM_ROUNDS + 1; ++rounds) {
+    t8_generate_sparkles ();
     t8_forest_init (&forest_new);
-    t8_forest_set_adapt(forest_new, forest, t8_adapt_sparcle_callback, 1);
+    t8_forest_set_adapt (forest_new, forest, t8_adapt_sparcle_callback, 1);
     //t8_forest_set_balance(forest_new, forest, 1);
-    t8_forest_commit(forest_new);
+    t8_forest_commit (forest_new);
     forest = forest_new;
 
     t8_forest_init (&forest_new);
-    t8_forest_set_adapt(forest_new, forest, t8_adapt_callback, 0);
+    t8_forest_set_adapt (forest_new, forest, t8_adapt_callback, 0);
     //t8_forest_set_balance(forest_new, forest, 1);
-    t8_forest_commit(forest_new);
+    t8_forest_commit (forest_new);
     forest = forest_new;
-    std::string filename = "christmas_card_" + std::to_string(rounds);
-    t8_forest_write_vtk_ext(forest, filename.c_str(), 1, 1, 1, 1, 0, 1, 0, 0, NULL);
+    std::string filename = "christmas_card_" + std::to_string (rounds);
+    t8_forest_write_vtk_ext (forest, filename.c_str (), 1, 1, 1, 1, 0, 1, 0, 0, NULL);
   }
 
-  
-  t8_forest_unref(&forest);
+  t8_forest_unref (&forest);
   sc_finalize ();
   mpiret = sc_MPI_Finalize ();
   SC_CHECK_MPI (mpiret);
