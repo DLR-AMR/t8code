831f9fa77c165254047d8a83d4b4a3a48b0bc9bc
Dreyer, Lukas <Lukas.Dreyer@dlr.de>
2024-01-26 13:16:41 +0100

    Fix test to changed interface

--- test/t8_schemes/t8_gtest_pack_unpack.cxx
+++ test/t8_schemes/t8_gtest_pack_unpack.cxx
@@ -41,14 +41,12 @@ class class_test_pack: public TestDFS {
 
     /* Compute pack size and allocate send buffer */
     int pack_size;
-    mpiret = ts->t8_element_MPI_Pack_size (count, comm, &pack_size);
-    SC_CHECK_MPI (mpiret);
+    ts->t8_element_MPI_Pack_size (count, comm, &pack_size);
 
     char *sendbuf = T8_ALLOC (char, pack_size);
 
     /* pack data */
-    mpiret = ts->t8_element_MPI_Pack (element, count, sendbuf, pack_size, &position, comm);
-    SC_CHECK_MPI (mpiret);
+    ts->t8_element_MPI_Pack (element, count, sendbuf, pack_size, &position, comm);
 
     int recvBufferSize = pack_size;
     char *recvbuf = T8_ALLOC (char, recvBufferSize);
@@ -75,8 +73,7 @@ class class_test_pack: public TestDFS {
 #endif
     /* Unpack data */
     position = 0;
-    mpiret = ts->t8_element_MPI_Unpack (recvbuf, recvBufferSize, &position, element_compare, count, comm);
-    SC_CHECK_MPI (mpiret);
+    ts->t8_element_MPI_Unpack (recvbuf, recvBufferSize, &position, element_compare, count, comm);
 
     /* free buffers */
     T8_FREE (sendbuf);
