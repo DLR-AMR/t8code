7b841fcd469b13bb266f96423adc475e7b8a4032
Dreyer, Lukas <Lukas.Dreyer@dlr.de>
2024-01-26 12:55:46 +0100

    Add documentation for schemes

--- src/t8_schemes/t8_default/t8_default_prism/t8_default_prism_cxx.hxx
+++ src/t8_schemes/t8_default/t8_default_prism/t8_default_prism_cxx.hxx
@@ -582,11 +582,35 @@ struct t8_default_scheme_prism_c: public t8_default_scheme_common_c
   virtual void
   t8_element_to_string (const t8_element_t *elem, char *debug_string, const int string_size) const;
 #endif
+
+  /** Pack multiple elements into contiguous memory, so they can be sent via MPI.
+   * \param [in] elements Array of elements that are to be packed
+   * \param [in] count Number of elements to pack
+   * \param [in,out] send_buffer Buffer in which to pack the elements
+   * \param [in] buffer_size size of the buffer (in order to check that we don't access out of range)
+   * \param [in, out] position the position of the first byte that is not already packed
+   * \param [in] comm MPI Communicator
+  */
   virtual void
   t8_element_MPI_Pack (const t8_element_t *elements, const int count, void *send_buffer, int buffer_size, int *position,
                        sc_MPI_Comm comm) const;
+
+  /** Determine an upper bound for the size of the packed message of \b count elements
+   * \param [in] count Number of elements to pack
+   * \param [in] comm MPI Communicator
+   * \param [out] pack_size upper bound on the message size
+  */
   virtual void
   t8_element_MPI_Pack_size (const int count, sc_MPI_Comm comm, int *pack_size) const;
+
+  /** Unpack multiple elements from contiguous memory that was received via MPI.
+   * \param [in] recvbuf Buffer from which to unpack the elements
+   * \param [in] buffer_size size of the buffer (in order to check that we don't access out of range)
+   * \param [in, out] position the position of the first byte that is not already packed
+   * \param [in] elements Array of initialised elements that is to be filled from the message
+   * \param [in] count Number of elements to unpack
+   * \param [in] comm MPI Communicator
+  */
   virtual void
   t8_element_MPI_Unpack (void *recvbuf, const int buffer_size, int *position, t8_element_t *elements, int count,
                          sc_MPI_Comm comm) const;
