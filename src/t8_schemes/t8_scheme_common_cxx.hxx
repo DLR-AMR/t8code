08ad0c513e698024633af0e4a623ee44bcb8f28c
lukasdreyer <58432628+lukasdreyer@users.noreply.github.com>
2024-03-06 12:09:17 +0100

    Apply suggestions from code review
    const
    
    Co-authored-by: David Knapp <david.knapp@dlr.de>

--- src/t8_schemes/t8_scheme_common_cxx.hxx
+++ src/t8_schemes/t8_scheme_common_cxx.hxx
@@ -44,7 +44,7 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
     sc_mempool_destroy (mempool);
   }
   /* Constructor */
-  t8_scheme_common_c (t8_eclass_t eclass_in, int elem_size)
+  t8_scheme_common_c (const t8_eclass_t eclass_in, const int elem_size)
   {
     element_size = elem_size;
     mempool = sc_mempool_new (element_size);
@@ -53,7 +53,7 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
 
   /** Use a mempool to get allocate */
   virtual void
-  t8_element_new (int length, t8_element_t **elem) const
+  t8_element_new (const t8_locidx_t length, t8_element_t **elem) const
   {
     T8_ASSERT (mempool != NULL);
     T8_ASSERT (0 <= length);
@@ -64,13 +64,13 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
   }
 
   virtual void
-  t8_element_destroy (int length, t8_element_t **elem) const
+  t8_element_destroy (const t8_locidx_t length, t8_element_t **elem) const
   {
     T8_ASSERT (mempool != NULL);
     T8_ASSERT (0 <= length);
     T8_ASSERT (elem != NULL);
-    for (int i = 0; i < length; ++i) {
-      sc_mempool_free (mempool, elem[i]);
+    for (int ielem = 0; ielem < length; ++ielem) {
+      sc_mempool_free (mempool, elem[ielem]);
     }
   }
 #if T8_ENABLE_DEBUG
@@ -89,13 +89,13 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
   }
 
   virtual t8_eclass_t
-  t8_element_child_eclass (int childid) const
+  t8_element_child_eclass (const int childid) const
   {
     return eclass;
   }
 
   virtual int
-  t8_element_is_family (t8_element_t **fam) const override
+  t8_element_is_family (const t8_element_t **fam) const override
   {
     if (t8_element_level (fam[0]) == 0)
       return 0;
@@ -103,7 +103,7 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
     t8_element_new (1, &parent);
     t8_element_new (1, &parent_compare);
     t8_element_parent (fam[0], parent);
-    int num_children = t8_element_num_children (parent);
+    const int num_children = t8_element_num_children (parent);
     bool is_family = true;
     for (int ichild = 1; ichild < num_children; ichild++) {
       t8_element_parent (fam[ichild], parent_compare);
@@ -129,7 +129,7 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
     t8_linearidx_t parent_id = 0;
     for (int ichild = 0; ichild < childid; ichild++) {
       t8_element_child (elem, ichild, elem);
-      t8_linearidx_t num_child_descendants = t8_element_count_leaves (elem, level);
+     const t8_linearidx_t num_child_descendants = t8_element_count_leaves (elem, level);
       t8_element_parent (elem, elem);
       parent_id += num_child_descendants;
     }
@@ -175,14 +175,14 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
   }
 
   virtual void
-  t8_element_set_linear_id (t8_element_t *elem, int level, t8_linearidx_t id) const override
+  t8_element_set_linear_id (t8_element_t *elem, const int level, const t8_linearidx_t id) const override
   {
     t8_element_root (elem);
     t8_element_init_linear_id_recursive (elem, level, id);
   }
 
   virtual t8_linearidx_t
-  t8_element_get_linear_id (const t8_element_t *elem, int level) const override
+  t8_element_get_linear_id (const t8_element_t *elem, const int level) const override
   {
     t8_element_t *rec_start;
     t8_element_new (1, &rec_start);
@@ -205,7 +205,7 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
   }
 
   virtual void
-  t8_element_first_descendant (const t8_element_t *elem, t8_element_t *desc, int level) const override
+  t8_element_first_descendant (const t8_element_t *elem, t8_element_t *desc, const int level) const override
   {
     t8_element_copy (elem, desc);
     while (t8_element_level (desc) < level) {
@@ -214,7 +214,7 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
   }
 
   virtual void
-  t8_element_last_descendant (const t8_element_t *elem, t8_element_t *desc, int level) const override
+  t8_element_last_descendant (const t8_element_t *elem, t8_element_t *desc, const int level) const override
   {
     t8_element_copy (elem, desc);
     while (t8_element_level (desc) < level) {
@@ -223,11 +223,11 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
   }
 
   virtual void
-  t8_element_successor (const t8_element_t *elem1, t8_element_t *elem2, int level) const override
+  t8_element_successor (const t8_element_t *elem1, t8_element_t *elem2, const int level) const override
   {
     T8_ASSERT (level != 0);
     T8_ASSERT (level == t8_element_level (elem1)); /* TODO: if this is true, do we need level in interface? */
-    int child_id = t8_element_child_id (elem1);
+    const int child_id = t8_element_child_id (elem1);
     if (child_id + 1 == t8_element_num_siblings (elem1)) {
       t8_element_parent (elem1, elem2);
       t8_element_successor (elem2, elem2, level - 1);
@@ -239,7 +239,7 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
   }
   /*****/
   virtual int
-  t8_element_ancestor_id (const t8_element_t *elem, int level) const override
+  t8_element_ancestor_id (const t8_element_t *elem, const int level) const override
   {
     T8_ASSERT (0 < level);
     T8_ASSERT (level <= t8_element_level (elem));
@@ -249,7 +249,7 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
     while (t8_element_level (anc) > level) {
       t8_element_parent (anc, anc);
     }
-    int child_id = t8_element_child_id (anc);
+    const int child_id = t8_element_child_id (anc);
     t8_element_destroy (1, &anc);
     return child_id;
   }
@@ -279,7 +279,7 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
   }
 
   virtual void
-  t8_element_children (const t8_element_t *elem, int length, t8_element_t *children[]) const override
+  t8_element_children (const t8_element_t *elem, const int length, t8_element_t *children[]) const override
   {
     T8_ASSERT (length == t8_element_num_children (elem));
     for (int ichild = 0; ichild < length; ichild++) {
@@ -298,7 +298,7 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
   }
 
   virtual void
-  t8_element_sibling (const t8_element_t *elem, int sibid, t8_element_t *sibling) const override
+  t8_element_sibling (const t8_element_t *elem, const int sibid, t8_element_t *sibling) const override
   {
     T8_ASSERT (t8_element_is_valid (elem));
     T8_ASSERT (t8_element_is_valid (sibling));
@@ -312,21 +312,21 @@ class t8_scheme_common_c: public t8_eclass_scheme_c {
   {
     T8_ASSERT (t8_element_is_valid (elem1));
     T8_ASSERT (t8_element_is_valid (elem2));
-    int level1 = t8_element_level (elem1);
-    int level2 = t8_element_level (elem2);
-    int maxlevel = SC_MAX (level1, level2);
-    t8_linearidx_t id1 = t8_element_get_linear_id (elem1, maxlevel);
-    t8_linearidx_t id2 = t8_element_get_linear_id (elem2, maxlevel);
+    const int level1 = t8_element_level (elem1);
+    const int level2 = t8_element_level (elem2);
+    const int maxlevel = SC_MAX (level1, level2);
+    const t8_linearidx_t id1 = t8_element_get_linear_id (elem1, maxlevel);
+    const t8_linearidx_t id2 = t8_element_get_linear_id (elem2, maxlevel);
     return id1 < id2 ? -1 : (id1 > id2 ? 1 : (level1 < level2 ? -1 : (level1 > level2 ? 1 : 0)));
   }
 
   virtual t8_gloidx_t
-  t8_element_count_leaves_from_root (int level) const override
+  t8_element_count_leaves_from_root (const int level) const override
   {
     t8_element_t *root;
     t8_element_new (1, &root);
     t8_element_root (root);
-    t8_gloidx_t num_leaves = t8_element_count_leaves (root, level);
+    const t8_gloidx_t num_leaves = t8_element_count_leaves (root, level);
     t8_element_destroy (1, &root);
     return num_leaves;
   }
