fecb34196c56229e3730721952d8ee00088c593c
Niklas <niklas.boeing@gmx.de>
2024-04-24 08:49:34 +0200

    Added a class for a geo-spatial dimension

---
+++ src/t8_netcdf/t8_nc_dimension_interval.hxx
@@ -0,0 +1,18 @@
+#ifndef T8_NC_DIMENSION_INTERVAL_HXX
+#define T8_NC_DIMENSION_INTERVAL_HXX
+
+#include <t8.h>
+
+enum t8_nc_dimension_t { DIMENSION_UNDEFINED = -1, LON = 0, LAT = 1, LEV = 2, TIME = 3, NUM_COORDINATES };
+
+struct DimensionInterval
+{
+  constexpr DimensionInterval (t8_nc_dimension_t dimension, t8_gloidx_t start_idx, t8_gloidx_t end_idx)
+    : dim { dimension }, start_index { start_idx }, end_index { end_idx } {};
+
+  t8_nc_dimension_t dim { t8_nc_dimension_t::DIMENSION_UNDEFINED };
+  t8_gloidx_t start_index { 0 };
+  t8_gloidx_t end_index { 0 };
+};
+
+#endif /* !T8_NC_DIMENSION_INTERVAL_HXX */
