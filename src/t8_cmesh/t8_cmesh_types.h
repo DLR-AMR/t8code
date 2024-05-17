d44924b2e7b8423fd6029b0a0dd3a67f326e85aa
jfussbro <jakob.fussbroich@gmx.de>
2024-05-17 09:14:22 +0200

    minor changes

--- src/t8_cmesh/t8_cmesh_types.h
+++ src/t8_cmesh/t8_cmesh_types.h
@@ -139,7 +139,7 @@ typedef struct t8_cmesh
 
   t8_geometry_handler_c *geometry_handler; /**< Handles all geometries that are used by trees in this cmesh. */
 
-  struct t8_cmesh_vertex_connectivity *vertex_connectivity;
+  struct t8_cmesh_vertex_connectivity *vertex_connectivity; /**< Structure that manages tree_to_vertex and vertex_to_tree connectivity. */
 
 #ifdef T8_ENABLE_DEBUG
   t8_locidx_t inserted_trees;  /**< Count the number of inserted trees to
--- src/t8_cmesh/t8_cmesh_vertex_connectivity.h
+++ src/t8_cmesh/t8_cmesh_vertex_connectivity.h
@@ -30,4 +30,24 @@
 
 typedef struct t8_cmesh_vertex_connectivity *t8_cmesh_vertex_connectivity_c;
 
+void
+t8_cmesh_set_global_vertices_of_tree (const t8_cmesh_t cmesh, const t8_gloidx_t global_tree,
+                                      const t8_gloidx_t *global_tree_vertices, const int num_vertices);
+
+t8_gloidx_t
+t8_cmesh_get_num_global_vertices (const t8_cmesh_t cmesh);
+
+t8_locidx_t
+t8_cmesh_get_num_local_vertices (const t8_cmesh_t cmesh);
+
+const t8_gloidx_t *
+t8_cmesh_get_global_vertices_of_tree (const t8_cmesh_t cmesh, const t8_locidx_t local_tree, const int num_vertices);
+
+const t8_gloidx_t
+t8_cmesh_get_global_vertex_of_tree (const t8_cmesh_t cmesh, const t8_locidx_t local_tree, const int local_tree_vertex,
+                                    const int num_vertices);
+
+const int
+t8_cmesh_get_num_trees_at_vertex (const t8_cmesh_t cmesh, t8_gloidx_t global_vertex);
+
 #endif /* !T8_CMESH_VERTEX_CONNECTIVITY_H */
