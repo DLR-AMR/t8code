02e263eda4a1f195f80778732883ed13fb5e17b3
Johannes Markert <johannes.markert@dlr.de>
2024-06-04 14:23:39 +0200

    Extended negative volume check for cmesh elements to tri/quad.

--- src/t8_geometry/t8_geometry_with_vertices.cxx
+++ src/t8_geometry/t8_geometry_with_vertices.cxx
@@ -55,42 +55,16 @@ t8_geometry_with_vertices::t8_geom_load_tree_data (t8_cmesh_t cmesh, t8_gloidx_t
 bool
 t8_geometry_with_vertices::t8_geom_tree_negative_volume () const
 {
-  /* `active_tree_vertices` is not necessarily available. 
-   * TODO: Is this the behavior we want?
-   */
-  if (active_tree_vertices == NULL) {
+  if (t8_eclass_to_dimension[active_tree_class] <= 2) {
+    /* Only three dimensional eclass do have a volume */
     return false;
   }
-
-  /* Points and lines do not have a volume. */
-  if (t8_eclass_to_dimension[active_tree_class] < 2) {
-    return false;
-  }
-
-  T8_ASSERT (active_tree_class == T8_ECLASS_TRIANGLE || active_tree_class == T8_ECLASS_QUAD
-             || active_tree_class == T8_ECLASS_TET || active_tree_class == T8_ECLASS_HEX
+  T8_ASSERT (active_tree_class == T8_ECLASS_TET || active_tree_class == T8_ECLASS_HEX
              || active_tree_class == T8_ECLASS_PRISM || active_tree_class == T8_ECLASS_PYRAMID);
 
-  T8_ASSERT (t8_eclass_num_vertices[active_tree_class] >= 3);
-
-  /* Check for negative volume (orientation of face normal) of 2D elements only
-   * when z-coordinates are all zero. */
-  if (t8_eclass_to_dimension[active_tree_class] < 3) {
-    for (int ivert = 0; ivert < t8_eclass_num_vertices[active_tree_class]; ivert++) {
-      if (std::abs (active_tree_vertices[3 * ivert + 2]) > 10 * T8_PRECISION_EPS) {
-        return false;
-      };
-    }
-  }
-
+  T8_ASSERT (t8_eclass_num_vertices[active_tree_class] >= 4);
   /*
-   *      z             For triangles and quads we enforce the right-hand-rule in terms
-   *      |             of node ordering. The volume is defined by the parallelepiped
-   *      | 2- - -(3)   spanned by the vectors between nodes 0:1 and 0:2 as well as the
-   *      |/____ /      unit vector in z-direction. This works for both triangles and quads.
-   *      0     1
-   *
-   *      6 ______  7   For Hexes and pyramids, if the vertex 4 is below the 0-1-2-3 plane,
+   *      6 ______  7  For Hexes and pyramids, if the vertex 4 is below the 0-1-2-3 plane,
    *       /|     /     the volume is negative. This is the case if and only if
    *    4 /_____5/|     the scalar product of v_4 with the cross product of v_1 and v_2 is
    *      | | _ |_|     smaller 0:
@@ -108,30 +82,9 @@ t8_geometry_with_vertices::t8_geom_tree_negative_volume () const
    *
    */
 
-  /* Build the vectors v_i as vertices_i - vertices_0. */
+  /* build the vectors v_i as vertices_i - vertices_0 */
   double v_1[3], v_2[3], v_j[3], cross[3], sc_prod;
-
-  if (active_tree_class == T8_ECLASS_TRIANGLE || active_tree_class == T8_ECLASS_QUAD) {
-    for (int i = 0; i < 3; i++) {
-      v_1[i] = active_tree_vertices[3 + i] - active_tree_vertices[i];
-      v_2[i] = active_tree_vertices[6 + i] - active_tree_vertices[i];
-    }
-
-    /* Unit vector in z-direction. */
-    v_j[0] = 0.0;
-    v_j[1] = 0.0;
-    v_j[2] = 1.0;
-
-    /* Compute cross = v_1 x v_2. */
-    t8_vec_cross (v_1, v_2, cross);
-    /* Compute sc_prod = <v_j, cross>. */
-    sc_prod = t8_vec_dot (v_j, cross);
-
-    T8_ASSERT (sc_prod != 0);
-    return sc_prod < 0;
-  }
-
-  int j;
+  int i, j;
   if (active_tree_class == T8_ECLASS_TET || active_tree_class == T8_ECLASS_PRISM) {
     /* In the tet/prism case, the third vector is v_3 */
     j = 3;
@@ -140,7 +93,7 @@ t8_geometry_with_vertices::t8_geom_tree_negative_volume () const
     /* For pyramids and Hexes, the third vector is v_4 */
     j = 4;
   }
-  for (int i = 0; i < 3; i++) {
+  for (i = 0; i < 3; i++) {
     v_1[i] = active_tree_vertices[3 + i] - active_tree_vertices[i];
     v_2[i] = active_tree_vertices[6 + i] - active_tree_vertices[i];
     v_j[i] = active_tree_vertices[3 * j + i] - active_tree_vertices[i];
