b8f0a34540fa16cc82a06295cb2f7f8160dc1149
Johannes Markert <johannes.markert@dlr.de>
2024-07-09 16:58:56 +0200

    Simplified C interface for registering geometry handlers.

--- src/t8_cmesh/t8_cmesh_geometry.cxx
+++ src/t8_cmesh/t8_cmesh_geometry.cxx
@@ -33,13 +33,13 @@
 #include <t8_geometry/t8_geometry_handler.hxx>
 
 void
-t8_cmesh_register_geometry (t8_cmesh_t cmesh, t8_geometry_c **geometry)
+t8_cmesh_register_geometry (t8_cmesh_t cmesh, t8_geometry_c *geometry)
 {
   if (cmesh->geometry_handler == NULL) {
     /* The handler was not constructed, do it now. */
     cmesh->geometry_handler = new t8_geometry_handler ();
   }
-  cmesh->geometry_handler->register_geometry (geometry);
+  cmesh->geometry_handler->register_geometry (&geometry);
 }
 
 void
