6954ee743e368d7a9cbe7678ec1408cd4607e95e
Chiara Hergl <124142713+chiaraMaHe@users.noreply.github.com>
2024-07-16 13:37:12 +0200

    Update api/t8_fortran_interface/t8_fortran_interface.h
    Co-authored-by: Sandro Elsweijer <49643115+sandro-elsweijer@users.noreply.github.com>

--- api/t8_fortran_interface/t8_fortran_interface.h
+++ api/t8_fortran_interface/t8_fortran_interface.h
@@ -5,7 +5,6 @@
 
   Copyright (C) 2024 the developers
 
-
   t8code is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
