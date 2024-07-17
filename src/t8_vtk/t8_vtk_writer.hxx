9e76567ff6af0923e3881bea17a09b9e0763e191
David Knapp <david.knapp@dlr.de>
2024-07-17 13:14:42 +0200

    Introduce a c-interface for the vtk-writer

--- src/t8_vtk/t8_vtk_writer.hxx
+++ src/t8_vtk/t8_vtk_writer.hxx
@@ -79,25 +79,36 @@ class vtk_writer {
    * Construct a new vtk writer object. All parameters are set to false by default. By default no data is used and
    * \a num_data is set to zero. A default \a fileprefix is NOT given. 
    * 
-   * \param write_treeid True, if we want to write the tree id of every element
-   * \param write_mpirank True, if we want to write the mpirankof every element
-   * \param write_level True, if we want to write the level of every element. Uses level 0 if used for a cmesh.
-   * \param write_element_id True, if we want to write the element id of every element. Ignored if used for a cmesh.
-   * \param write_ghosts True, if we want to write the ghost elements, too. 
-   * \param curved_flag True, if we want to use quadratic vtk cells. Uses the geometry of the grid to evaluate points between corners. 
-   * \param fileprefix The prefix of the output-file
-   * \param num_data The number of data-fields to print
-   * \param data The data to use
-   * \param comm The communicator for parallel output
+   * \param[in] write_treeid True, if we want to write the tree id of every element
+   * \param[in] write_mpirank True, if we want to write the mpirankof every element
+   * \param[in] write_level True, if we want to write the level of every element. Uses level 0 if used for a cmesh.
+   * \param[in] write_element_id True, if we want to write the element id of every element. Ignored if used for a cmesh.
+   * \param[in] write_ghosts True, if we want to write the ghost elements, too. 
+   * \param[in] curved_flag True, if we want to use quadratic vtk cells. Uses the geometry of the grid to evaluate points between corners. 
+   * \param[in] fileprefix The prefix of the output-file
+   * \param[in] num_data The number of data-fields to print
+   * \param[in] data The data to use
+   * \param[in] comm The communicator for parallel output
    */
   vtk_writer (const bool write_treeid, const bool write_mpirank, const bool write_level, const bool write_element_id,
               const bool write_ghosts, const bool curved_flag, std::string fileprefix, const int num_data,
               t8_vtk_data_field_t *data, sc_MPI_Comm comm)
     : write_treeid (write_treeid), write_mpirank (write_mpirank), write_level (write_level),
       write_ghosts (write_ghosts), curved_flag (curved_flag), fileprefix (fileprefix), num_data (num_data), data (data),
-      comm (comm) {};
+      comm (comm)
+  {
+  }
 
-#if T8_WITH_VTK
+  /**
+   * Construct a new vtk writer object. All parameters are set to false.
+   * 
+   * \param[in] fileprefix 
+   * \param[in] comm 
+   */
+  vtk_writer (std::string fileprefix, sc_MPI_Comm comm): fileprefix (fileprefix), comm (comm)
+  {
+  }
+
   /**
    * A vtk-writer function that uses the vtk API
    * 
@@ -110,7 +121,6 @@ class vtk_writer {
   {
     return write_vtk (grid);
   }
-#endif
 
   /**
    * A vtk-writer function that uses the vtk API
---
+++ src/t8_vtk/t8_vtk_writer_c_interface.cxx
@@ -0,0 +1,65 @@
+/*
+This file is part of t8code.
+t8code is a C library to manage a collection (a forest) of multiple
+connected adaptive space-trees of general element classes in parallel.
+
+Copyright (C) 2024 the developers
+
+t8code is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+t8code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with t8code; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#include <t8_vtk/t8_vtk_writer_c_interface.h>
+#include <t8_vtk/t8_vtk_writer.hxx>
+#include <string>
+
+T8_EXTERN_C_BEGIN ();
+
+int
+t8_forest_vtk_write_file_via_API (t8_forest_t forest, const char *fileprefix, const int write_treeid,
+                                  const int write_mpirank, const int write_level, const int write_element_id,
+                                  const int curved_flag, const int write_ghosts, const int num_data,
+                                  t8_vtk_data_field_t *data)
+{
+  vtk_writer<t8_forest_t> writer (write_treeid, write_mpirank, write_element_id, write_ghosts, curved_flag,
+                                  std::string (fileprefix), num_data, data, t8_forest_get_mpicomm (forest));
+  return writer.write_with_API (forest);
+}
+
+int
+t8_forest_vtk_write_file (t8_forest_t forest, const char *fileprefix, const int write_treeid, const int write_mpirank,
+                          const int write_level, const int write_element_id, int write_ghosts, const int num_data,
+                          t8_vtk_data_field_t *data)
+{
+  vtk_writer<t8_forest_t> writer (write_treeid, write_mpirank, write_element_id, write_ghosts, curved_flag,
+                                  std::string (fileprefix), num_data, data, t8_forest_get_mpicomm (forest));
+  return writer.write_ASCII (forest);
+}
+
+int
+t8_cmesh_vtk_write_file_via_API (t8_cmesh_t cmesh, const char *fileprefix, sc_MPI_Comm comm)
+{
+  vtk_writer<t8_cmesh_t> writer (std::string (fileprefix), comm);
+  return writer.write_with_API (cmesh);
+}
+
+int
+t8_cmesh_vtk_write_file (t8_cmesh_t cmesh, const char *fileprefix)
+{
+  /* No mpi Communicator is needed for ASCII output*/
+  vtk_writer<t8_cmesh_t> writer (std::string (fileprefix), sc_MPI_COMM_NULL);
+  return writer.write_ASCII (cmesh);
+}
+
+T8_EXTERN_C_END ();
