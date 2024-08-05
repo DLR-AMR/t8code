a46f0d41da6cd73f00e6964ce969877e460e8f7f
Johannes Holke <johannes.holke@dlr.de>
2024-08-05 16:56:30 +0200

    Try to fix element_is_leaf for subelements

--- src/t8_forest/t8_forest.cxx
+++ src/t8_forest/t8_forest.cxx
@@ -2590,9 +2590,24 @@ t8_forest_element_is_leaf (const t8_forest_t forest, const t8_element_t *element
     /* The element was not found. */
     return 0;
   }
-  /* An element was found but it may not be the candidate element. 
-   * To identify whether the element was found, we compare these two. */
-  const t8_element_t *check_element = t8_element_array_index_locidx (elements, search_result);
+  /* An element was found but it may not be the candidate element.
+   * If our original element is not a subelement, then we compare with the found element.
+   * Otherwise, if our element is a subelement, then all subelements from the same element match
+   * the search and we need to query whether our element is contained in that set. */
+  t8_locidx_t check_index = search_result;
+
+  if (scheme->t8_element_is_subelement (element)) {
+    /* From now we consider the case that our element is a subelement. */
+    const int subelement_id = scheme->t8_element_get_subelement_id (element);
+    check_index += subelement_id;
+  }
+  if (t8_element_array_get_count (elements) <= (size_t) check_index) {
+    /* The element cannot be contained in the array. */
+    return 0;
+  }
+
+  /* Get the respective element from the array and compare with our query element. */
+  const t8_element_t *check_element = t8_element_array_index_locidx (elements, check_index);
   T8_ASSERT (check_element != NULL);
   return (scheme->t8_element_equal (element, check_element));
 }
