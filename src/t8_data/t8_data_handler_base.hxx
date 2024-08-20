c985b9e0386fcc476381607c75c5e955dd9a5726
David Knapp <david.knapp@dlr.de>
2024-08-20 09:36:46 +0200

    Move single-pack implementation into seperate files.

---
+++ src/t8_data/t8_data_handler_base.hxx
@@ -0,0 +1,54 @@
+/*
+This file is part of t8code.
+t8code is a C library to manage a collection (a forest) of multiple
+connected adaptive space-trees of general element classes in parallel.
+
+Copyright (C) 2024 the developers
+
+t8code is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+t8code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with t8code; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#ifndef T8_DATA_HANDLER_BASE
+#define T8_DATA_HANDLER_BASE
+
+#include <t8.h>
+#include <test/t8_data/t8_data_handler_specs.hxx>
+
+template <typename T>
+class t8_single_data_handler {
+ public:
+  int
+  data_size (sc_MPI_Comm comm);
+
+  /**
+     * Overwrite this routine to describe how data of type T should be packed
+     * 
+     * \param[in] data Data to be packed via MPI_Pack
+     * \return the size of the packed data in number of bytes. 
+     */
+  void
+  data_pack (T &data, int &pos, std::vector<char> &buffer, sc_MPI_Comm comm);
+
+  /**
+     * Overwrite this routine to describe how data of type T should be unpacked 
+     * 
+     * \param packed_data A void-pointer to the packed Data
+     * \return T* the unpacked data. 
+     */
+  void
+  data_unpack (std::vector<char> &buffer, int &pos, T &data, sc_MPI_Comm comm);
+};
+
+#endif /* T8_DATA_HANDLER_BASE */
---
+++ src/t8_data/t8_data_packs/t8_enlarged_stdtypes.hxx
@@ -0,0 +1,98 @@
+/*
+This file is part of t8code.
+t8code is a C library to manage a collection (a forest) of multiple
+connected adaptive space-trees of general element classes in parallel.
+
+Copyright (C) 2024 the developers
+
+t8code is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+t8code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with t8code; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+*/
+
+#ifndef T8_ENLARGED_INT
+#define T8_ENLARGED_INT
+
+#include <test/t8_data/t8_data_handler_specs.hxx>
+#include <t8_data/t8_data_handler_base.hxx>
+
+template <>
+class t8_single_data_handler<enlarged_data<int>> {
+ public:
+  int
+  data_size (sc_MPI_Comm comm)
+  {
+    int size;
+    const int mpiret = sc_MPI_Pack_size (2, sc_MPI_INT, comm, &size);
+    SC_CHECK_MPI (mpiret);
+    return size;
+  }
+
+  void
+  data_pack (enlarged_data<int> data, int &pos, std::vector<char> &buffer, sc_MPI_Comm comm)
+  {
+    int mpiret = sc_MPI_Pack (&data.data, 1, sc_MPI_INT, buffer.data (), buffer.size (), &pos, comm);
+    SC_CHECK_MPI (mpiret);
+
+    mpiret = sc_MPI_Pack (&data.data, 1, sc_MPI_INT, buffer.data (), buffer.size (), &pos, comm);
+    SC_CHECK_MPI (mpiret);
+  }
+
+  void
+  data_unpack (std::vector<char> &buffer, int &pos, enlarged_data<int> &data, sc_MPI_Comm comm)
+  {
+    int mpiret = sc_MPI_Unpack (buffer.data (), buffer.size (), &pos, &data.data, 1, sc_MPI_INT, comm);
+    SC_CHECK_MPI (mpiret);
+
+    mpiret = sc_MPI_Unpack (buffer.data (), buffer.size (), &pos, &data.check, 1, sc_MPI_INT, comm);
+    SC_CHECK_MPI (mpiret);
+  }
+};
+
+template <>
+class t8_single_data_handler<enlarged_data<double>> {
+ public:
+  int
+  data_size (sc_MPI_Comm comm)
+  {
+    int int_size;
+    int mpiret = sc_MPI_Pack_size (1, sc_MPI_INT, comm, &int_size);
+    SC_CHECK_MPI (mpiret);
+    int double_size;
+    mpiret = sc_MPI_Pack_size (1, sc_MPI_DOUBLE, comm, &double_size);
+    SC_CHECK_MPI (mpiret);
+    return int_size + double_size;
+  }
+
+  void
+  data_pack (enlarged_data<double> data, int &pos, std::vector<char> &buffer, sc_MPI_Comm comm)
+  {
+    int mpiret = sc_MPI_Pack (&data.data, 1, sc_MPI_DOUBLE, buffer.data (), buffer.size (), &pos, comm);
+    SC_CHECK_MPI (mpiret);
+
+    mpiret = sc_MPI_Pack (&data.data, 1, sc_MPI_INT, buffer.data (), buffer.size (), &pos, comm);
+    SC_CHECK_MPI (mpiret);
+  }
+
+  void
+  data_unpack (std::vector<char> &buffer, int &pos, enlarged_data<double> &data, sc_MPI_Comm comm)
+  {
+    int mpiret = sc_MPI_Unpack (buffer.data (), buffer.size (), &pos, &data.data, 1, sc_MPI_DOUBLE, comm);
+    SC_CHECK_MPI (mpiret);
+
+    mpiret = sc_MPI_Unpack (buffer.data (), buffer.size (), &pos, &data.check, 1, sc_MPI_INT, comm);
+    SC_CHECK_MPI (mpiret);
+  }
+};
+
+#endif /* T8_ENLARGED_INT */
