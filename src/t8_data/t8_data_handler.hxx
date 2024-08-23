e640c3ec8fb7f96b7c071150aa96dd4659fc7c63
David Knapp <david.knapp@dlr.de>
2024-08-23 14:03:07 +0200

    remove Enable-Mpi-Makros around allgather

--- src/t8_data/t8_data_handler.hxx
+++ src/t8_data/t8_data_handler.hxx
@@ -205,7 +205,6 @@ class t8_data_handler: public t8_single_data_handler<T> {
   int
   allgather (const std::vector<T> &send, std::vector<T> &recv, sc_MPI_Comm comm)
   {
-#if T8_ENABLE_MPI
     const int bsize = buffer_size (send.size (), comm);
     std::vector<char> buffer (bsize);
     pack_vector_no_prefix (send, buffer, comm);
@@ -225,10 +224,6 @@ class t8_data_handler: public t8_single_data_handler<T> {
     unpack_vector_no_prefix (recv_buffer, recv, comm);
 
     return mpiret;
-#else
-    t8_infof ("Allgather only available when configured with --enable-mpi\n");
-    return sc_MPI_ERR_OTHER;
-#endif
   }
 };
 
