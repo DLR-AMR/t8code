4291fe4d42934d6a6b6e2c1d9df09a80b82f1a97
David Knapp <david.knapp@dlr.de>
2024-09-25 13:47:34 +0200

    write version-file
    WIP: reading from tar currently failing

--- CPackConfig.cmake
+++ CPackConfig.cmake
@@ -7,12 +7,27 @@ set(CPACK_PACKAGE_VERSION_PATCH ${T8CODE_VERSION_PATCH})
 set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/COPYING")
 set(CPACK_PACKAGE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/package)
-set( CPACK_PACKAGE_ICON  ${CMAKE_CURRENT_LIST_DIR}/t8code_logo.png)
+set(CPACK_PACKAGE_ICON  ${CMAKE_CURRENT_SOURCE_DIR}/t8code_logo.png)
 
+# Define a variable for the version file
+set(VERSION_FILE "${CMAKE_CURRENT_LIST_DIR}/version.txt")
+
+
+# Custom command to generate the version file
+add_custom_command(
+    OUTPUT ${VERSION_FILE}
+    COMMAND ${CMAKE_COMMAND} -E echo "Version ${T8CODE_VERSION_MAJOR}.${T8CODE_VERSION_MINOR}.${T8CODE_VERSION_PATCH}" > ${VERSION_FILE}
+    DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/CMakeLists.txt  # Change as needed
+    )
+    
+# Create a custom target to ensure the version file is generated
+add_custom_target(GenerateVersionFile ALL DEPENDS ${VERSION_FILE})
 
 set(CPACK_SOURCE_GENERATOR "TGZ;ZIP")
+set(CPACK_SOURCE_INCLUDE_FILES ${VERSION_FILE})
 set(CPACK_SOURCE_IGNORE_FILES .git/ .github/ .vscode/ _CPack_Packages/
-${CMAKE_BINARY_DIR}/ ${PROJECT_BINARY_DIR}/
+.gitmodules .gitignore
+${PROJECT_BINARY_DIR}/
 Makefile.in
 aclocal.m4
 autom4te.cache/
@@ -25,6 +40,7 @@ build/
 build-aux/
 configure
 config.log
+compile_commands.json
 )
 
 set(CPACK_PACKAGE_NAME "T8CODE")
@@ -32,3 +48,4 @@ set(CPACK_VERBATIM_VARIABLES TRUE)
 
 
 include(CPack)
+
--- cmake/GitProjectVersion.cmake
+++ cmake/GitProjectVersion.cmake
@@ -1,32 +1,53 @@
 find_package( Git REQUIRED )
 
-# See `scr/t8_version.h` for the documentation of following definitions.
+if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git")
+    # See `scr/t8_version.h` for the documentation of following definitions.
 
-execute_process( COMMAND ${GIT_EXECUTABLE} describe --tags --dirty
-                 COMMAND cut -c 2-
-                 WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-                 OUTPUT_VARIABLE T8CODE_VERSION_RAW
-                 OUTPUT_STRIP_TRAILING_WHITESPACE )
-execute_process( COMMAND echo ${T8CODE_VERSION_RAW}
-                 COMMAND cut -d- -f1
-                 OUTPUT_VARIABLE T8CODE_VERSION_NUMBERS
-                 OUTPUT_STRIP_TRAILING_WHITESPACE )
-execute_process( COMMAND echo ${T8CODE_VERSION_RAW}
-                 COMMAND cut -d- -f2-
-                 OUTPUT_VARIABLE T8CODE_VERSION_POINT
-                 OUTPUT_STRIP_TRAILING_WHITESPACE )
-execute_process( COMMAND echo ${T8CODE_VERSION_NUMBERS}
-                 COMMAND cut -d. -f1
-                 OUTPUT_VARIABLE T8CODE_VERSION_MAJOR
-                 OUTPUT_STRIP_TRAILING_WHITESPACE )
-execute_process( COMMAND echo ${T8CODE_VERSION_NUMBERS}
-                 COMMAND cut -d. -f2
-                 OUTPUT_VARIABLE T8CODE_VERSION_MINOR
-                 OUTPUT_STRIP_TRAILING_WHITESPACE )
-execute_process( COMMAND echo ${T8CODE_VERSION_NUMBERS}
-                 COMMAND cut -d. -f3
-                 OUTPUT_VARIABLE T8CODE_VERSION_PATCH
-                 OUTPUT_STRIP_TRAILING_WHITESPACE )
+    execute_process( COMMAND ${GIT_EXECUTABLE} describe --tags --dirty
+                    COMMAND cut -c 2-
+                    WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+                    OUTPUT_VARIABLE T8CODE_VERSION_RAW
+                    OUTPUT_STRIP_TRAILING_WHITESPACE )
 
-# To reuse the version in other CMakeLists.
-set(T8_VERSION ${T8CODE_VERSION_NUMBERS} CACHE INTERNAL "")
+    # To reuse the version in other CMakeLists.
+    set(T8_VERSION ${T8CODE_VERSION_NUMBERS} CACHE INTERNAL "")
+else()
+    if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/version.txt")
+        file(READ "${CMAKE_CURRENT_SOURCE_DIR}/version.txt" VERSION_CONTENT)
+        
+        # Extract the version number
+        string(REGEX MATCH "Version: ([0-9]+\\.[0-9]+\\.[0-9]+)" MATCH_RESULT "${VERSION_CONTENT}")
+        if (MATCH_RESULT)
+            # The version number will be in ${CMAKE_MATCH_1}
+            set(T8CODE_VERSION_NUMBERS "${CMAKE_MATCH_1}")
+            message(STATUS "Extracted Version: ${EXTRACTED_VERSION}")
+        else()
+            message(WARNING "Version number not found in version.txt")
+        endif()
+    else()
+        message(WARNING "Version information not found")
+    endif()
+endif()
+
+execute_process( COMMAND echo ${T8CODE_VERSION_RAW}
+                    COMMAND cut -d- -f1
+                    OUTPUT_VARIABLE T8CODE_VERSION_NUMBERS
+                    OUTPUT_STRIP_TRAILING_WHITESPACE )
+    execute_process( COMMAND echo ${T8CODE_VERSION_RAW}
+                    COMMAND cut -d- -f2-
+                    OUTPUT_VARIABLE T8CODE_VERSION_POINT
+                    OUTPUT_STRIP_TRAILING_WHITESPACE )
+    execute_process( COMMAND echo ${T8CODE_VERSION_NUMBERS}
+                    COMMAND cut -d. -f1
+                    OUTPUT_VARIABLE T8CODE_VERSION_MAJOR
+                    OUTPUT_STRIP_TRAILING_WHITESPACE )
+    execute_process( COMMAND echo ${T8CODE_VERSION_NUMBERS}
+                    COMMAND cut -d. -f2
+                    OUTPUT_VARIABLE T8CODE_VERSION_MINOR
+                    OUTPUT_STRIP_TRAILING_WHITESPACE )
+    execute_process( COMMAND echo ${T8CODE_VERSION_NUMBERS}
+                    COMMAND cut -d. -f3
+                    OUTPUT_VARIABLE T8CODE_VERSION_PATCH
+                    OUTPUT_STRIP_TRAILING_WHITESPACE )
+
+message(STATUS "Read Version: ${T8CODE_VERSION_NUMBERS}")
