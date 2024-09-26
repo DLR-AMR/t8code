3b99849ca77e57243f583f70f12e73c7a854405a
David Knapp <david.knapp@dlr.de>
2024-09-26 10:08:49 +0200

    Read version from file when building from tar

--- cmake/GitProjectVersion.cmake
+++ cmake/GitProjectVersion.cmake
@@ -8,19 +8,26 @@ if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git")
                     WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
                     OUTPUT_VARIABLE T8CODE_VERSION_RAW
                     OUTPUT_STRIP_TRAILING_WHITESPACE )
+    execute_process( COMMAND echo ${T8CODE_VERSION_RAW}
+                    COMMAND cut -d- -f1
+                    OUTPUT_VARIABLE T8CODE_VERSION_NUMBERS
+                    OUTPUT_STRIP_TRAILING_WHITESPACE )
+    execute_process( COMMAND echo ${T8CODE_VERSION_RAW}
+                COMMAND cut -d- -f2-
+                OUTPUT_VARIABLE T8CODE_VERSION_POINT
+                OUTPUT_STRIP_TRAILING_WHITESPACE )
 
     # To reuse the version in other CMakeLists.
-    set(T8_VERSION ${T8CODE_VERSION_NUMBERS} CACHE INTERNAL "")
-else()
+    else()
     if(EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/version.txt")
         file(READ "${CMAKE_CURRENT_SOURCE_DIR}/version.txt" VERSION_CONTENT)
-        
-        # Extract the version number
-        string(REGEX MATCH "Version: ([0-9]+\\.[0-9]+\\.[0-9]+)" MATCH_RESULT "${VERSION_CONTENT}")
-        if (MATCH_RESULT)
+        # Extract the version number 
+        string(REGEX MATCH "Version ([0-9])\.([0-9]+)\.([0-9]+)" VERSION_MATCH "${VERSION_CONTENT}" )
+        if (VERSION_MATCH)
             # The version number will be in ${CMAKE_MATCH_1}
-            set(T8CODE_VERSION_NUMBERS "${CMAKE_MATCH_1}")
-            message(STATUS "Extracted Version: ${EXTRACTED_VERSION}")
+            set(T8CODE_VERSION_NUMBERS "${CMAKE_MATCH_1}.${CMAKE_MATCH_2}.${CMAKE_MATCH_3}")
+            set(T8CODE_VERSION_RAW "${CMAKE_MATCH_1}.${CMAKE_MATCH_2}.${CMAKE_MATCH_3}")
+            message(STATUS "Extracted Version: ${T8CODE_VERSION_NUMBERS}")
         else()
             message(WARNING "Version number not found in version.txt")
         endif()
@@ -29,25 +36,19 @@ else()
     endif()
 endif()
 
-execute_process( COMMAND echo ${T8CODE_VERSION_RAW}
-                    COMMAND cut -d- -f1
-                    OUTPUT_VARIABLE T8CODE_VERSION_NUMBERS
-                    OUTPUT_STRIP_TRAILING_WHITESPACE )
-    execute_process( COMMAND echo ${T8CODE_VERSION_RAW}
-                    COMMAND cut -d- -f2-
-                    OUTPUT_VARIABLE T8CODE_VERSION_POINT
-                    OUTPUT_STRIP_TRAILING_WHITESPACE )
-    execute_process( COMMAND echo ${T8CODE_VERSION_NUMBERS}
-                    COMMAND cut -d. -f1
-                    OUTPUT_VARIABLE T8CODE_VERSION_MAJOR
-                    OUTPUT_STRIP_TRAILING_WHITESPACE )
-    execute_process( COMMAND echo ${T8CODE_VERSION_NUMBERS}
-                    COMMAND cut -d. -f2
-                    OUTPUT_VARIABLE T8CODE_VERSION_MINOR
-                    OUTPUT_STRIP_TRAILING_WHITESPACE )
-    execute_process( COMMAND echo ${T8CODE_VERSION_NUMBERS}
-                    COMMAND cut -d. -f3
-                    OUTPUT_VARIABLE T8CODE_VERSION_PATCH
-                    OUTPUT_STRIP_TRAILING_WHITESPACE )
 
-message(STATUS "Read Version: ${T8CODE_VERSION_NUMBERS}")
+execute_process( COMMAND echo ${T8CODE_VERSION_NUMBERS}
+                COMMAND cut -d. -f1
+                OUTPUT_VARIABLE T8CODE_VERSION_MAJOR
+                OUTPUT_STRIP_TRAILING_WHITESPACE )
+execute_process( COMMAND echo ${T8CODE_VERSION_NUMBERS}
+                COMMAND cut -d. -f2
+                OUTPUT_VARIABLE T8CODE_VERSION_MINOR
+                OUTPUT_STRIP_TRAILING_WHITESPACE )
+execute_process( COMMAND echo ${T8CODE_VERSION_NUMBERS}
+                COMMAND cut -d. -f3
+                OUTPUT_VARIABLE T8CODE_VERSION_PATCH
+                OUTPUT_STRIP_TRAILING_WHITESPACE )
+set(T8_VERSION ${T8CODE_VERSION_NUMBERS} CACHE INTERNAL "")
+
+
