38637292f02d7ff63579d107df3062a317d63677
Johannes Holke <johannes.holke@dlr.de>
2024-11-15 11:28:29 +0100

    indent

--- src/t8_forest/t8_forest_iterate.h
+++ src/t8_forest/t8_forest_iterate.h
@@ -108,8 +108,8 @@ t8_forest_split_array (const t8_element_t *element, const t8_element_array_t *le
  * If it returns false, the current element is not traversed further */
 void
 t8_forest_iterate_faces (t8_forest_t forest, t8_locidx_t ltreeid, const t8_element_t *element, int face,
-                         const t8_element_array_t *leaf_elements, void *user_data, t8_locidx_t tree_lindex_of_first_leaf,
-                         t8_forest_iterate_face_fn callback);
+                         const t8_element_array_t *leaf_elements, void *user_data,
+                         t8_locidx_t tree_lindex_of_first_leaf, t8_forest_iterate_face_fn callback);
 
 /* Perform a top-down search of the forest, executing a callback on each
  * intermediate element. The search will enter each tree at least once.
