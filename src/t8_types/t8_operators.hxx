35ca6f96cc12e4063b4900876ffdf203d255b897
Ole Albers <ole.albers@dlr.de>
2024-11-28 13:49:56 +0100

    add strong types for points and vectors

--- src/t8_types/t8_operators.hxx
+++ src/t8_types/t8_operators.hxx
@@ -211,6 +211,57 @@ struct EqualityComparable: t8_crtp_operator<TUnderlying, EqualityComparable>
   {
     return lhs.get () == rhs.get ();
   }
+
+  bool
+  operator!= (T const& other) const
+  {
+    return !(*this == other);
+  }
+};
+
+/**
+ * \brief A template for random accessible types. Provides the [] operator.
+ * 
+ * \tparam T 
+ */
+template <typename T>
+struct RandomAccessible: crtp<T, RandomAccessible>
+{
+  auto
+  operator[] (std::size_t index) -> decltype (auto)
+  {
+    return this->underlying ().get ()[index];
+  }
+
+  auto
+  operator[] (std::size_t index) const -> decltype (auto)
+  {
+    return this->underlying ().get ()[index];
+  }
+
+  auto
+  begin () -> decltype (auto)
+  {
+    return this->underlying ().get ().begin ();
+  }
+
+  auto
+  begin () const -> decltype (auto)
+  {
+    return this->underlying ().get ().begin ();
+  }
+
+  auto
+  end () -> decltype (auto)
+  {
+    return this->underlying ().get ().end ();
+  }
+
+  auto
+  end () const -> decltype (auto)
+  {
+    return this->underlying ().get ().end ();
+  }
 };
 
 /**
