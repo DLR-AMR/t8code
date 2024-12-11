c7654ea346e0e383fd5e82ed7dda80f26a4583ce
Johannes Holke <johannes.holke@dlr.de>
2024-12-11 15:02:50 +0100

    extend lfn test

--- test/t8_forest/t8_gtest_face_neighbors.cxx
+++ test/t8_forest/t8_gtest_face_neighbors.cxx
@@ -86,17 +86,23 @@ class forest_face_neighbors: public testing::TestWithParam<cmesh_example_base *>
 TEST_P (forest_face_neighbors, test_face_neighbors)
 {
   /* iterate over all elements */
-  const t8_locidx_t num_local_trees = t8_forest_get_num_local_trees (forest);
-  const t8_locidx_t num_ghost_trees = t8_forest_get_num_ghost_trees (forest);
+  const t8_forest_t forest_uniform = forest;
+  const t8_cmesh_t cmesh = t8_forest_get_cmesh (forest_uniform);
+  const t8_locidx_t num_local_trees = t8_forest_get_num_local_trees (forest_uniform);
+  const t8_locidx_t num_ghost_trees = t8_forest_get_num_ghost_trees (forest_uniform);
+  const t8_locidx_t num_local_elements = t8_forest_get_local_num_elements (forest_uniform);
   for (t8_locidx_t itree = 0; itree < num_local_trees + num_ghost_trees; itree++) {
+    const t8_gloidx_t gtree_id = t8_forest_global_tree_id (forest_uniform, itree);
     const bool is_ghost = itree >= num_local_trees;
     const t8_locidx_t ghost_tree_id = itree - num_local_trees;
     /* Get the leaf element array */
-    const t8_element_array_t *leaf_elements = !is_ghost ? t8_forest_get_tree_element_array (forest, itree)
-                                                        : t8_forest_ghost_get_tree_elements (forest, ghost_tree_id);
-    const t8_eclass_t tree_class = t8_forest_get_tree_class (forest, itree);
-    const t8_eclass_scheme_c *scheme = t8_forest_get_eclass_scheme (forest, tree_class);
+    const t8_element_array_t *leaf_elements = !is_ghost
+                                                ? t8_forest_get_tree_element_array (forest_uniform, itree)
+                                                : t8_forest_ghost_get_tree_elements (forest_uniform, ghost_tree_id);
+    const t8_eclass_t tree_class = t8_forest_get_tree_class (forest_uniform, itree);
+    const t8_eclass_scheme_c *scheme = t8_forest_get_eclass_scheme (forest_uniform, tree_class);
     const t8_locidx_t num_leafs = t8_element_array_get_count (leaf_elements);
+    const t8_locidx_t cmesh_tree = t8_forest_ltreeid_to_cmesh_ltreeid (forest_uniform, itree);
     for (t8_locidx_t ileaf = 0; ileaf < num_leafs; ++ileaf) {
       // Iterate over each leaf element
       const t8_element_t *element = t8_element_array_index_locidx (leaf_elements, ileaf);
@@ -107,18 +113,106 @@ TEST_P (forest_face_neighbors, test_face_neighbors)
         // preparation
         t8_element_t **neighbor_leaves;
         int *dual_faces;
-        int num_neighbors;
+        int num_neighbors = 0;
         t8_locidx_t *element_indices;
         t8_eclass_scheme_c *neigh_scheme;
         t8_gloidx_t gneigh_tree;
         int orientation;
-        // Actual computation
-        t8_forest_leaf_face_neighbors_ext (forest, itree, element, &neighbor_leaves, iface, &dual_faces, &num_neighbors,
-                                           &element_indices, &neigh_scheme, &gneigh_tree, &orientation);
+        // Actual computation of the face neighbors
+        t8_forest_leaf_face_neighbors_ext (forest_uniform, itree, element, &neighbor_leaves, iface, &dual_faces,
+                                           &num_neighbors, &element_indices, &neigh_scheme, &gneigh_tree, &orientation);
 
-        // clean-up
+        t8_debugf ("Tree %i element %i at face %i has %i face neighbors.\n", itree, ileaf, iface, num_neighbors);
+
+        if (gneigh_tree < 0) {
+          // No neighbors are found, check for correctly set return values
+          ASSERT_EQ (num_neighbors, 0);
+          ASSERT_EQ (neighbor_leaves, nullptr);
+          ASSERT_EQ (element_indices, nullptr);
+          ASSERT_EQ (dual_faces, nullptr);
+        }
+        else {
+          ASSERT_GE (num_neighbors, 0);
+          ASSERT_NE (neighbor_leaves, nullptr);
+          ASSERT_NE (element_indices, nullptr);
+          ASSERT_NE (dual_faces, nullptr);
+        }
+        // Checking for:
+        //      uniform forest:
+        //        - inner element has 1 face neighbors
+        //        - boundary element has 0 face neighbors // TODO: Add this test for all forests
+        //        - If E face f has neighbor E' face f', then
+        //             E' face f' must have neighbor E face f.
+
+        // Now checking for inner and boundary elements.
+
+        // Compute whether this element is a boundary element or not.
+        // An element is a boundary element if it lies on the tree boundary
+        // and if the corresponding tree face is at the domain boundary.
+        const bool is_root_boundary = scheme->t8_element_is_root_boundary (element, iface);
+        const int tree_face = scheme->t8_element_tree_face (element, iface);
+        const bool is_boundary_element
+          = is_root_boundary && t8_cmesh_tree_face_is_boundary (cmesh, cmesh_tree, tree_face);
+
+        if (!is_boundary_element) {
+          EXPECT_EQ (num_neighbors, 1) << "Inner element should have exactly 1 neighbor, has " << num_neighbors << ".";
+        }
+        else {
+          EXPECT_EQ (num_neighbors, 0) << "Boundary element should have exactly 0 neighbors, has " << num_neighbors
+                                       << ".";
+          ;
+        }
+
+        // Check that the neighbor of the neighbor is the original element.
+        for (int ineigh = 0; ineigh < num_neighbors; ++ineigh) {
+          const t8_element_t *neighbor = neighbor_leaves[ineigh];
+          const int dual_face = dual_faces[ineigh];
+          const t8_locidx_t neigh_index = element_indices[ineigh];
+
+          t8_debugf ("Checking neighbor element %p.\n", neighbor);
+          t8_debugf ("dual face is %i, index is %i\n", dual_face, neigh_index);
+          ASSERT_TRUE (neigh_scheme->t8_element_is_valid (neighbor))
+            << "Neighbor element " << ineigh << " is not valid";
+          // Compute the local tree id of the neighbors tree depending on whether
+          // it is a local tree or a ghost tree.
+          const t8_locidx_t neigh_ltreeid
+            = neigh_index < num_local_elements
+                ? gneigh_tree - t8_forest_get_first_local_tree_id (forest_uniform)
+                : t8_forest_ghost_get_ghost_treeid (forest_uniform, gneigh_tree) + num_local_trees;
+          // preparation
+          t8_element_t **neigh_neighbor_leaves;
+          int *neigh_dual_faces;
+          int neigh_num_neighbors = 0;
+          t8_locidx_t *neigh_element_indices;
+          t8_eclass_scheme_c *neigh_neigh_scheme;
+          t8_gloidx_t neigh_gneigh_tree;
+          int neigh_orientation;
+          // Actual computation of the neighbor's face neighbors
+          t8_forest_leaf_face_neighbors_ext (forest_uniform, neigh_ltreeid, neighbor, &neigh_neighbor_leaves, dual_face,
+                                             &neigh_dual_faces, &neigh_num_neighbors, &neigh_element_indices,
+                                             &neigh_neigh_scheme, &neigh_gneigh_tree, &neigh_orientation);
+
+          // We must have found at least one face neighbor, namely the original element.
+          EXPECT_GE (neigh_num_neighbors, 1);
+          // The neighbor's neighbor tree must be the current tree
+          EXPECT_EQ (gtree_id, neigh_gneigh_tree);
+          // The neighbor's scheme must be the current scheme
+          EXPECT_EQ (scheme, neigh_neigh_scheme);
+          // The neighbor's orientation must be the orientation
+          EXPECT_EQ (orientation, neigh_orientation);
+
+          // TODO Check for the specific face neighbor that would match the original element
+
+          // clean-up neighbor's neighbors
+          if (neigh_num_neighbors > 0) {
+            T8_FREE (neigh_neighbor_leaves);
+            T8_FREE (neigh_element_indices);
+            T8_FREE (neigh_dual_faces);
+          }
+        }
+
+        // clean-up original element neighbors
         if (num_neighbors > 0) {
-          scheme->t8_element_destroy (num_neighbors, neighbor_leaves);
           T8_FREE (neighbor_leaves);
           T8_FREE (element_indices);
           T8_FREE (dual_faces);
