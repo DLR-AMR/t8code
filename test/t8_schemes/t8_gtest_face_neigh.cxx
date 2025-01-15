b28d5199d5907958327e58d6e2cf01dc3b62099c
Ole Albers <ole.albers@dlr.de>
2025-01-15 10:33:15 +0100

    only test face functionality for the default scheme

--- test/t8_schemes/t8_gtest_face_neigh.cxx
+++ test/t8_schemes/t8_gtest_face_neigh.cxx
@@ -55,7 +55,6 @@ class face_neigh: public testing::TestWithParam<std::tuple<int, t8_eclass_t>> {
   t8_element_t *neigh;
   t8_scheme *scheme;
   t8_eclass_t eclass;
-  
 
 #ifdef T8_ENABLE_LESS_TESTS
   const int maxlvl = 3;
@@ -83,7 +82,8 @@ t8_test_face_neighbor_inside (int num_faces, t8_element_t *element, t8_element_t
 }
 
 int
-t8_test_get_middle_child (const t8_eclass_t eclass, int ilevel, t8_element_t *element, t8_element_t *child, t8_scheme *scheme)
+t8_test_get_middle_child (const t8_eclass_t eclass, int ilevel, t8_element_t *element, t8_element_t *child,
+                          t8_scheme *scheme)
 {
   /* Get the child number of the child in the middle of the element, depending of the shape of the element. */
   switch (eclass) {
@@ -200,4 +200,4 @@ TEST_P (face_neigh, recursive_check_diff)
   t8_recursive_check_diff (child, element, neigh, scheme, eclass, maxlvl, level);
 }
 
-INSTANTIATE_TEST_SUITE_P (t8_gtest_face_neigh, face_neigh, AllSchemes);
+INSTANTIATE_TEST_SUITE_P (t8_gtest_face_neigh, face_neigh, DefaultSchemes);
