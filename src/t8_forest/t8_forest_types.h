30c6f6b5d0c27dc51f93be88aed3a52b1aad13e4
antjeHP <s6aehenr@uni-bonn.de>
2025-01-21 14:19:33 +0100

    doc

--- src/t8_forest/t8_forest_types.h
+++ src/t8_forest/t8_forest_types.h
@@ -100,11 +100,8 @@ typedef struct t8_forest
                                              If 0, no balance. If 1 balance with repartitioning, if 2 balance without
                                              repartitioning, \see t8_forest_balance */
   int do_ghost;                   /**< If True, a ghost layer will be created when the forest is committed. */
-  // t8_ghost_type_t ghost_type;     /**< If a ghost layer will be created, the type of neighbors that count as ghost. */
-  // int ghost_algorithm;            /**< Controls the algorithm used for ghost. 1 = balanced only. 2 = also unbalanced
-  //  3 = top-down search and unbalanced. */
-  t8_forest_ghost_definition_c *ghost_definition;
-  void *user_data;          /**< Pointer for arbitrary user data. \see t8_forest_set_user_data. */
+  t8_forest_ghost_definition_c *ghost_definition; /**< The definition of the ghost as class, with a ghost_type > */
+  void *user_data;                                /**< Pointer for arbitrary user data. \see t8_forest_set_user_data. */
   void (*user_function) (); /**< Pointer for arbitrary user function. \see t8_forest_set_user_function. */
   void *t8code_data;        /**< Pointer for arbitrary data that is used internally. */
   int committed;            /**< \ref t8_forest_commit called? */
