89cf66a9a23fe705156e7461ada1a37008a8f30b
David Knapp <david.knapp@dlr.de>
2025-01-27 09:21:52 +0100

    fix comment

--- src/t8_data/t8_data_handler_base.hxx
+++ src/t8_data/t8_data_handler_base.hxx
@@ -1,5 +1,4 @@
 /*
-/*
   This file is part of t8code.
   t8code is a C library to manage a collection (a forest) of multiple
   connected adaptive space-trees of general element classes in parallel.
@@ -21,7 +20,6 @@
   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
-
 #ifndef T8_DATA_HANDLER_BASE
 #define T8_DATA_HANDLER_BASE
 
@@ -33,7 +31,6 @@
  */
 
 #include <t8.h>
-
 
 /**
  * \class t8_single_data_handler
