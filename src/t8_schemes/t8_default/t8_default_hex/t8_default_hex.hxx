d002060c0f9ac497535af85068488cc107af0a11
Ole Albers <122293607+ole-alb@users.noreply.github.com>
2025-02-07 09:29:24 +0100

    Apply suggestions from code review
    Co-authored-by: David Knapp <david.knapp@dlr.de>

--- src/t8_schemes/t8_default/t8_default_hex/t8_default_hex.hxx
+++ src/t8_schemes/t8_default/t8_default_hex/t8_default_hex.hxx
@@ -212,7 +212,7 @@ class t8_default_scheme_hex: public t8_default_scheme_common<t8_default_scheme_h
    * \return              The face number of the \a face-th face at \a corner.
    */
   int
-  element_get_corner_face (const t8_element_t *element, int corner, int face) const;
+  element_get_corner_face (const t8_element_t *element, const int corner, const int face) const;
 
   /** Construct the child element of a given number.
    * \param [in] elem     This must be a valid element, bigger than maxlevel.
