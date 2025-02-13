c3eab834cbe13966905abfd03d65f21b00285d36
lenaploetzke <70579874+lenaploetzke@users.noreply.github.com>
2025-02-13 16:01:49 +0100

    added out to parameter and small corrections

--- src/t8_forest/t8_forest_general.h
+++ src/t8_forest/t8_forest_general.h
@@ -63,10 +63,10 @@ T8_EXTERN_C_BEGIN ();
  * of the new forest that are either refined, coarsened or the same as elements in the old forest.
  *
  * \param [in] forest_old      The forest that is adapted
- * \param [in] forest_new      The forest that is newly constructed from \a forest_old
+ * \param [in, out] forest_new The forest that is newly constructed from \a forest_old
  * \param [in] which_tree      The local tree containing \a first_outgoing and \a first_incoming
  * \param [in] tree_class      The eclass of the local tree containing \a first_outgoing and \a first_incoming
- * \param [in] scheme              The scheme of the forest
+ * \param [in] scheme          The scheme of the forest
  * \param [in] refine          -1 if family in \a forest_old got coarsened, 0 if element
  *                             has not been touched, 1 if element got refined and -2 if
  *                             element got removed. See return of t8_forest_adapt_t.
@@ -103,7 +103,7 @@ typedef void (*t8_forest_replace_t) (t8_forest_t forest_old, t8_forest_t forest_
  * \param [in] which_tree   The local tree containing \a elements.
  * \param [in] tree_class   The eclass of \a which_tree.
  * \param [in] lelement_id  The local element id in \a forest_old in the tree of the current element.
- * \param [in] scheme           The scheme of the forest.
+ * \param [in] scheme       The scheme of the forest.
  * \param [in] is_family    If 1, the first \a num_elements entries in \a elements form a family. If 0, they do not.
  * \param [in] num_elements The number of entries in \a elements that are defined
  * \param [in] elements     Pointers to a family or, if \a is_family is zero,
@@ -648,8 +648,8 @@ t8_forest_same_level_leaf_face_neighbor_index (t8_forest_t forest, const t8_loci
                                                const int face_index, const t8_gloidx_t global_treeid, int *dual_face);
 
 /** Exchange ghost information of user defined element data.
- * \param[in] forest       The forest. Must be committed.
- * \param[in] element_data An array of length num_local_elements + num_ghosts
+ * \param [in] forest       The forest. Must be committed.
+ * \param [in] element_data An array of length num_local_elements + num_ghosts
  *                         storing one value for each local element and ghost in \a forest.
  *                         After calling this function the entries for the ghost elements
  *                         are update with the entries in the \a element_data array of
