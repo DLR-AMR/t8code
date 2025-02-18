48caafb95cae2b1564d295f6a00350a3e9ba9389
Alex Dutka <97711898+dutkalex@users.noreply.github.com>
2025-02-18 14:21:41 +0100

    fix -Wunused-parameter warnings in tests

--- example/common/t8_example_common.cxx
+++ example/common/t8_example_common.cxx
@@ -39,8 +39,8 @@ T8_EXTERN_C_BEGIN ();
  */
 int
 t8_common_adapt_balance (t8_forest_t forest, t8_forest_t forest_from, t8_locidx_t which_tree,
-                         const t8_eclass_t tree_class, t8_locidx_t lelement_id, const t8_scheme *scheme,
-                         const int is_family, const int num_elements, t8_element_t *elements[])
+                         const t8_eclass_t tree_class, [[maybe_unused]] t8_locidx_t lelement_id, const t8_scheme *scheme,
+                         [[maybe_unused]] const int is_family, [[maybe_unused]] const int num_elements, t8_element_t *elements[])
 {
   int level;
   int maxlevel, child_id;
@@ -119,8 +119,8 @@ t8_common_within_levelset (t8_forest_t forest, const t8_locidx_t ltreeid, const 
 /* TODO: Currently the band_width control is not working yet. */
 int
 t8_common_adapt_level_set (t8_forest_t forest, t8_forest_t forest_from, t8_locidx_t which_tree,
-                           const t8_eclass_t tree_class, t8_locidx_t lelement_id, const t8_scheme *scheme,
-                           const int is_family, const int num_elements, t8_element_t *elements[])
+                           const t8_eclass_t tree_class, [[maybe_unused]] t8_locidx_t lelement_id, const t8_scheme *scheme,
+                           const int is_family, [[maybe_unused]] const int num_elements, t8_element_t *elements[])
 {
   t8_example_level_set_struct_t *data;
   int within_band;
--- tutorials/general/t8_step3_adapt_forest.cxx
+++ tutorials/general/t8_step3_adapt_forest.cxx
@@ -83,8 +83,8 @@ T8_EXTERN_C_BEGIN ();
  * \param [in] elements     The element or family of elements to consider for refinement/coarsening.
  */
 int
-t8_step3_adapt_callback (t8_forest_t forest, t8_forest_t forest_from, t8_locidx_t which_tree, t8_eclass_t tree_class,
-                         t8_locidx_t lelement_id, const t8_scheme *scheme, const int is_family, const int num_elements,
+t8_step3_adapt_callback (t8_forest_t forest, t8_forest_t forest_from, t8_locidx_t which_tree, [[maybe_unused]] t8_eclass_t tree_class,
+    [[maybe_unused]] t8_locidx_t lelement_id, [[maybe_unused]] const t8_scheme *scheme, const int is_family, [[maybe_unused]] const int num_elements,
                          t8_element_t *elements[])
 {
   /* Our adaptation criterion is to look at the midpoint coordinates of the current element and if
