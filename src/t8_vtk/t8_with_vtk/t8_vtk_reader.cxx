d37b999df753811fab7781a305e2d62939d386c8
faouziH21 <2004homsani@gmail.com>
2025-03-28 16:09:10 +0100

    other: adds and updates linces

--- src/t8_vtk/t8_with_vtk/t8_vtk_reader.cxx
+++ src/t8_vtk/t8_with_vtk/t8_vtk_reader.cxx
@@ -3,7 +3,7 @@
   t8code is a C library to manage a collection (a forest) of multiple
   connected adaptive space-trees of general element classes in parallel.
 
-  Copyright (C) 2023 the developers
+  Copyright (C) 2025 the developers
 
   t8code is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
