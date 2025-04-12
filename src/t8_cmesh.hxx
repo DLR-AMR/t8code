cd22e31012197c93163676dce8e5bdde4c6cc3b1
David Knapp <david.knapp@dlr.de>
2025-04-12 19:39:35 +0200

    Add comments

--- src/t8_cmesh.hxx
+++ src/t8_cmesh.hxx
@@ -31,23 +31,52 @@
 #include <t8_cmesh/t8_cmesh_types.h>
 #include <t8_geometry/t8_geometry_handler.hxx>
 
+/**
+ * Compute the first element of a process in a partitioned mesh, via floor(process * mpisize / global_num_elements).
+ * Prevents overflow by splitting global_num_elements into two 32 bit integers.
+ * global_num_elements = a_0 * tau + a_1, tau = 2^32.
+ * 
+ * floor(a_0 * tau + a_1) * process / mpisize
+ * = floor (a_0 * tau * process / mpisize + a_1 * process / mpisize)
+ * (add the fractional part of the summands and take the floor) to split the computation into separate parts.
+ * = floor (a_0 * tau * process / mpisize) + floor (a_1 * process / mpisize) + floor ((a_o*tau*process) % mpisize + a_1*process % mpisize) / mpisize)
+ * the first term can computed as:
+ * floor (a_0 * tau * process / mpisize)
+ * = floor (floor (a_0 * tau / mpisize * process ) + (a_0 * tau % mpisize) * process / mpisize)
+ * We divide a_0 * tau by mpisize first to prevent an overflow. The second term corrects for the rounding error.
+ *
+ * Adding all summands gives the result.
+ *
+ * This enables us to partition 2^64-1 elements over 2^32-1 processes.
+ * Update this function if we have supercomputers with more than 2^32-1 processes, or need larger meshes.
+ *
+ * \param[in] process   The number of processes
+ * \param[in] mpisize   The size of the MPI communicator
+ * \param[in] global_num_elements   The number of elements in the global mesh
+ * \return The first element of the process in the partitioned mesh
+ */
 constexpr uint64_t
 t8_cmesh_get_first_element_of_process (const uint32_t process, const uint32_t mpisize,
                                        const uint64_t global_num_elements)
 {
+  /* Split the uint64_t */
   const uint64_t a_0 = (global_num_elements >> 32);
-
   const uint64_t a_1 = (global_num_elements << 32) >> 32;
 
+  /* Cast everything into uint64_t */
   const uint64_t process_64 = static_cast<uint64_t> (process);
   const uint64_t mpisize_64 = static_cast<uint64_t> (mpisize);
 
+  /* sum_0 = floor(a_0 * tau / mpisize * process) */
   const uint64_t sum_0 = (a_0 << 32) / mpisize_64 * process_64;
 
+  /* sum_1 = floor(a_0 * tau % mpisize * process / mpisize) */
   const uint64_t sum_1 = (((a_0 << 32) % mpisize_64) * process_64) / mpisize_64;
 
+  /* sum_2 = floor(a_1 * process / mpisize) */
   const uint64_t sum_2 = (a_1 * process_64) / mpisize_64;
 
+  /* sum_3 is the correction term of splitting the floor operation of the sum into two parts.  */
   const uint64_t sum_3_1 = ((a_0 << 32) % mpisize_64 * (process_64 % mpisize_64)) % mpisize_64;
   const uint64_t sum_3_2 = (a_1 * process_64) % mpisize_64;
   const uint64_t sum_3 = (sum_3_1 + sum_3_2) / mpisize_64;
