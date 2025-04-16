ee08119a8a9c6bf09b621fe071581c0ab25f2463
faouziH21 <2004homsani@gmail.com>
2025-04-16 14:19:02 +0200

    feat: adds new writer geometry class

---
+++ src/t8_geometry/t8_geometry_implementations/t8_geometry_writer_base.cxx
@@ -0,0 +1,8 @@
+#include<t8_geometry/t8_geometry.h>
+#include<t8_geometry/t8_geometry_implementations/t8_geometry_writer_base.hxx>
+#include<t8_geometry/t8_geometry_helpers.h>
+
+t8_geometry_writer_base::t8_geometry_writer_base()
+  : t8_geometry_with_vertices ("t8_geom_linear_axis_aligned")
+{
+}
