ac804d49b509b3a89f5b9cc465cfcb19fa4f13cd
Ole Albers <ole.albers@dlr.de>
2025-05-20 15:58:41 +0200

    rewrite are_family

--- src/t8_schemes/t8_standalone/t8_standalone_implementation.hxx
+++ src/t8_schemes/t8_standalone/t8_standalone_implementation.hxx
@@ -577,33 +577,46 @@ struct t8_standalone_scheme
   static constexpr int
   elements_are_family (t8_element_t *const *fam) noexcept
   {
-#if T8_ENABLE_DEBUG
-    const int num_siblings = element_get_num_siblings (fam[0]);
-    for (int isib = 0; isib < num_siblings; isib++) {
-      T8_ASSERT (element_is_valid (fam[isib]));
-    }
-#endif
+    // #if T8_ENABLE_DEBUG
+    //     const int num_siblings = element_get_num_siblings (fam[0]);
+    //     for (int isib = 0; isib < num_siblings; isib++) {
+    //       T8_ASSERT (element_is_valid (fam[isib]));
+    //     }
+    // #endif
 
-    t8_standalone_element<TEclass> parent, compare;
-    /* Take the parent of the first element as baseline to compare against */
-    element_get_parent ((const t8_element_t *) fam[0], (t8_element_t *) &parent);
-    const int num_children = element_get_num_children ((const t8_element_t *) &parent);
-    for (int childid = 0; childid < num_children; childid++) {
-      /* check whether each element has the same parent */
-      element_get_parent ((const t8_element_t *) fam[childid], (t8_element_t *) &compare);
-      if (element_compare ((const t8_element_t *) &parent, (const t8_element_t *) &compare)) {
-        return 0;
+    //     t8_standalone_element<TEclass> parent, compare;
+    //     /* Take the parent of the first element as baseline to compare against */
+    //     element_get_parent ((const t8_element_t *) fam[0], (t8_element_t *) &parent);
+    //     const int num_children = element_get_num_children ((const t8_element_t *) &parent);
+    //     for (int childid = 0; childid < num_children; childid++) {
+    //       /* check whether each element has the same parent */
+    //       element_get_parent ((const t8_element_t *) fam[childid], (t8_element_t *) &compare);
+    //       if (element_compare ((const t8_element_t *) &parent, (const t8_element_t *) &compare)) {
+    //         return 0;
+    //       }
+
+    //       /* check whether each element is the correct child of the collective parent */
+    //       /* Could be replaced by type comparison as level is already checked in parent comparison */
+    //       element_get_child ((const t8_element_t *) &parent, childid, (t8_element_t *) &compare);
+
+    //       if (element_compare ((const t8_element_t *) fam[childid], (const t8_element_t *) &compare)) {
+    //         return 0;
+    //       }
+    //     }
+    //     return 1;
+    if (element_get_child_id (fam[0]) == 0) {
+      int num_siblings = element_get_num_siblings (fam[0]);
+      for (int ielement = 1; ielement < num_siblings; ielement++) {
+        if (element_get_level (fam[ielement]) == element_get_level (fam[ielement - 1])) {
+          return 1;
+        }
+        else {
+          return 0;
+        }
       }
-
-      /* check whether each element is the correct child of the collective parent */
-      /* Could be replaced by type comparison as level is already checked in parent comparison */
-      element_get_child ((const t8_element_t *) &parent, childid, (t8_element_t *) &compare);
-
-      if (element_compare ((const t8_element_t *) fam[childid], (const t8_element_t *) &compare)) {
-        return 0;
-      }
     }
-    return 1;
+
+    return 0;
   }
 
   /** Compute the nearest common ancestor of two elements. That is,
