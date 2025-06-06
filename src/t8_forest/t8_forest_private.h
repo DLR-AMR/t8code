67d62654e97ac5ae0eb855837fdf4aab21e1c03b
Johannes Holke <johannes.holke@dlr.de>
2025-06-06 16:16:05 +0200

    Add declaration of +t8_forest_bin_search_first_descendant_ancenstor. docstring still missing

--- src/t8_forest/t8_forest_private.h
+++ src/t8_forest/t8_forest_private.h
@@ -222,6 +222,12 @@ t8_locidx_t
 t8_forest_bin_search_upper (const t8_element_array_t *elements, const t8_linearidx_t element_id,
                             const int element_level);
 
+/** \brief TODO: document
+ */
+t8_locidx_t
+t8_forest_bin_search_first_descendant_ancenstor (const t8_element_array_t *elements, const t8_element_t *element,
+                                                 const t8_element_t *element_found);
+
 /** Find the owner process of a given element, deprecated version.
  * Use t8_forest_element_find_owner instead.
  * \param [in]     forest  The forest.
