475b6f9cb701b423b44f10082a72693ad86b6b27
David Knapp <david.knapp@dlr.de>
2025-06-24 13:55:15 +0200

    call script

---
+++ scripts/configure_for_rtd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create the build directory
+mkdir build
+
+# Navigate into the build directory
+cd build
+
+# Run cmake with the specified options
+cmake .. -DT8CODE_BUILD_DOCUMENTATION=ON -DT8CODE_BUILD_DOCUMENTATION_SPHINX=ON
+
+# Build the project with make
+make -j V=0
+
+# Return to the parent directory
+cd ..
