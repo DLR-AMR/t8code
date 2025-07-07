4844c66b90f6c5d4b8c2c8517d1c832fdb59f142
Adrian Kolb <kolb@eddy.rwth-aachen.de>
2025-07-07 11:44:11 +0200

    formatting

--- src/t8_mra/basis_functions.hxx
+++ src/t8_mra/basis_functions.hxx
@@ -3,11 +3,15 @@
 
 #include <cmath>
 
-double skalierungsfunktion(int i, double tau1, double tau2);
+double
+skalierungsfunktion (int i, double tau1, double tau2);
 
-double muttermultiwavelets(int p, int i, double tau1, double tau2, int e);
+double
+muttermultiwavelets (int p, int i, double tau1, double tau2, int e);
 
-double muttermultiwavelet(int p, int i, double tau1, double tau2);
+double
+muttermultiwavelet (int p, int i, double tau1, double tau2);
 
-double skalierungsfunktion_nextlevel(int i, double tau1, double tau2);
+double
+skalierungsfunktion_nextlevel (int i, double tau1, double tau2);
 #endif
