2f77289d75370e6449818a13bffe48f83ba6164c
David Knapp <david.knapp@dlr.de>
2025-07-07 15:55:37 +0200

    fix t8_cmesh_commit

--- src/t8_cmesh/t8_cmesh_commit.cxx
+++ src/t8_cmesh/t8_cmesh_commit.cxx
@@ -46,9 +46,9 @@
  */
 typedef struct ghost_facejoins_struct
 {
-  t8_gloidx_t ghost_id; /* The id of the ghost */
-  t8_locidx_t local_id; /* The local id of the ghost */
-  t8_gloidx_t attr_id;  /* The current number of inserted ghost attributes */
+  t8_gloidx_t ghost_id; /**< The id of the ghost */
+  t8_locidx_t local_id; /**< The local id of the ghost */
+  t8_gloidx_t attr_id;  /**< The current number of inserted ghost attributes */
 } t8_ghost_facejoin_t;
 
 static int
