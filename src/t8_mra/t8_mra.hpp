94ef58cf8e42b416b9c4019ff966dfbf97634b1c
Adrian Kolb <kolb@eddy.rwth-aachen.de>
2025-07-15 14:08:13 +0200

    add mask coeffs to mst class

--- src/t8_mra/t8_mra.hpp
+++ src/t8_mra/t8_mra.hpp
@@ -7,27 +7,48 @@
 #include "t8_mra/data/cell_data.hpp"
 #include "t8_mra/data/levelmultiindex.hpp"
 #include "t8_mra/data/levelindex_map.hpp"
+#include "t8_mra/num/mask_coefficients.hpp"
 
 namespace t8_mra
 {
 
 template <t8_eclass TShape>
-class multiscale_types {
+struct multiscale_data
+{
   static constexpr unsigned short DIM = 0;
+
+  std::vector<t8_mra::mat> mask_coefficients;
+  std::vector<t8_mra::mat> inverse_mask_coefficients;
 };
 
 template <>
-class multiscale_types<T8_ECLASS_TRIANGLE> {
+struct multiscale_data<T8_ECLASS_TRIANGLE>
+{
   static constexpr unsigned short DIM = 2;
+
+  std::vector<t8_mra::mat> mask_coefficients;
+  std::vector<t8_mra::mat> inverse_mask_coefficients;
 };
 
-template <t8_eclass TShape, int U, int ORDER>
-class multiscale: multiscale_types<TShape> {
-  using element_t = data_per_element<TShape, U, ORDER>;
+/// TODO naming P -> ORDER?
+template <t8_eclass TShape, int U, int P>
+class multiscale: public multiscale_data<TShape> {
+  using element_t = data_per_element<TShape, U, P>;
   using levelmultiindex = levelmultiindex<TShape>;
 
+  static constexpr unsigned int DIM = element_t::DIM;
   static constexpr unsigned int U_DIM = U;
-  static constexpr unsigned int P_DIM = element_t::P_DIM;
+  static constexpr unsigned int P_DIM = P;
+
+  static constexpr unsigned int DOF = element_t::DOF;
+  static constexpr unsigned int W_DOF = element_t::W_DOF;
+
+ public:
+  multiscale ()
+  {
+    t8_mra::initialize_mask_coefficients<TShape> (P_DIM, DOF, multiscale_data<TShape>::mask_coefficients,
+                                                  multiscale_data<TShape>::inverse_mask_coefficients);
+  }
 
   void
   multiscale_transformation (t8_mra::levelindex_map<element_t>& grid_hierarchy, unsigned int l_min, unsigned int l_max)
@@ -49,11 +70,11 @@ class multiscale: multiscale_types<TShape> {
         parent_data.order = child_data[0].order;
         triangle_order::get_parent_order (parent_data.order);
 
-        for (auto i = 0u; i < P_DIM; ++i) {
+        for (auto i = 0u; i < DOF; ++i) {
           auto u_sum = 0.0;
           auto d_sum = 0.0;
 
-          for (auto j = 0u; j < P_DIM; ++j) {
+          for (auto j = 0u; j < DOF; ++j) {
             for (auto k = 0u; k < levelmultiindex::NUM_CHILDREN; ++k) {
               /// TODO mask coeffs
             }
