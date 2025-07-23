d0051716a605451d2178e61185fea3aba646c9f3
Adrian Kolb <kolb@eddy.rwth-aachen.de>
2025-07-23 14:27:39 +0200

    extended example for multiple variables

--- tutorials/general/t8_new_mra_example.cxx
+++ tutorials/general/t8_new_mra_example.cxx
@@ -99,21 +99,22 @@ main (int argc, char** argv)
   comm = sc_MPI_COMM_WORLD;
 
   /// Velis debugging example
-  auto f4 = [] (double x, double y) -> std::array<double, 1> {
+  auto f4 = [] (double x, double y) -> std::array<double, 2> {
     //if ((x == -1.) && (y == -1.)) return 6.;
     if (x < 0.41)
-      return { 0. };
+      return { 0., 0.0 };
     double r4 = (x - 0.5) * (x - 0.5) + (y - 0.5) * (y - 0.5);
     double r = sqrt (r4);
     if (r > 1. / 3.)
-      return { 0. };
+      return { 0., 0.0 };
     r *= 3.;
     r4 *= 9.;
     r4 *= r4;
     double rm1 = r - 1.;
     double rm1h2 = rm1 * rm1;
     double rm1h3 = rm1 * rm1h2;
-    return { 1. - r4 + 4. * r4 * rm1 - 10. * r4 * rm1h2 + 20 * r4 * rm1h3 };
+    return { 1. - r4 + 4. * r4 * rm1 - 10. * r4 * rm1h2 + 20 * r4 * rm1h3,
+             (1. - r4 + 4. * r4 * rm1 - 10. * r4 * rm1h2 + 20 * r4 * rm1h3) * 0.1 };
   };
 
   auto f = [] (double x, double y) { return x + y; };
@@ -125,7 +126,7 @@ main (int argc, char** argv)
   auto dunavant_rule = 10;
 
   constexpr int P = 3;
-  constexpr int U = 1;
+  constexpr int U = 2;
   using element_data_type = t8_mra::data_per_element<T8_ECLASS_TRIANGLE, U, P>;
   using mra_type = t8_mra::multiscale<T8_ECLASS_TRIANGLE, U, P>;
 
