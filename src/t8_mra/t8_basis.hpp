7c4024be40e6669e018726f8bb2f41e82a951e76
Adrian Kolb <kolb@eddy.rwth-aachen.de>
2025-07-31 12:02:03 +0200

    add new class for dg_basis
    add some ref/deref functions + transformation matrices for physical ->
    ref

---
+++ src/t8_mra/t8_basis.hpp
@@ -0,0 +1,111 @@
+#pragma once
+
+#ifdef T8_ENABLE_MRA
+
+#include <vector>
+
+#include "t8_mra/data/cell_data.hpp"
+#include "t8_mra/num/mask_coefficients.hpp"
+#include "t8_mra/num/dunavant.hxx"
+#include "t8_mra/num/mat.hpp"
+
+namespace t8_mra
+{
+
+template <t8_eclass TShape>
+struct dg_basis_base
+{
+  static constexpr unsigned int DIM = 0;
+  static constexpr t8_eclass Shape = TShape;
+
+  size_t num_quad_points;
+  std::vector<double> ref_quad_points;
+  std::vector<double> quad_weights;
+  /// TODO error message
+};
+
+template <>
+struct dg_basis_base<T8_ECLASS_TRIANGLE>
+{
+  static constexpr unsigned int DIM = 2;
+  static constexpr t8_eclass Shape = T8_ECLASS_TRIANGLE;
+
+  size_t num_quad_points;
+  int dunavant_rule;
+
+  std::vector<double> ref_quad_points;
+  std::vector<double> quad_weights;
+
+  dg_basis_base () = default;
+
+  dg_basis_base (int _num_quad_points, int _dunavant_rule)
+    : num_quad_points (t8_mra::dunavant_order_num (_dunavant_rule)), dunavant_rule (_dunavant_rule),
+      ref_quad_points (2u * num_quad_points, 0.0), quad_weights (num_quad_points, 0.0)
+  {
+    t8_mra::dunavant_rule (dunavant_rule, num_quad_points, ref_quad_points.data (), quad_weights.data ());
+  }
+
+  std::pair<t8_mra::mat, std::vector<size_t>>
+  trafo_matrix_to_ref_element (const double physical_vertices[3][3])
+  {
+    t8_mra::mat transform_to_ref (3, 3);
+    std::vector<size_t> permuation_vec (3, 0u);
+
+    for (auto i = 0; i < 3; ++i)
+      for (auto j = 0; j < 3; ++j)
+        transform_to_ref (i, j) = i == 2 ? 1.0 : physical_vertices[j][i];
+    t8_mra::lu_factors (transform_to_ref, permuation_vec);
+
+    return { transform_to_ref, permuation_vec };
+  }
+
+  /// returns list of dereferenced quad points [x_0, y_0, x_1, y_1, ...]
+  std::vector<double>
+  deref_quad_points (const double physical_vertices[3][3])
+  {
+    std::vector<double> deref_quad_points (ref_quad_points.size (), 0.0);
+
+    std::array<double, 6> corners { physical_vertices[0][0], physical_vertices[0][1], physical_vertices[1][0],
+                                    physical_vertices[1][1], physical_vertices[2][0], physical_vertices[2][1] };
+
+    t8_mra::reference_to_physical_t3 (corners.data (), num_quad_points, ref_quad_points.data (),
+                                      deref_quad_points.data ());
+
+    return deref_quad_points;
+  }
+
+  std::vector<double>
+  ref_point (const t8_mra::mat& trafo_mat, const std::vector<size_t>& permuation_vec,
+             const std::vector<double>& grid_point)
+  {
+    std::vector<double> ret = { grid_point[0], grid_point[1], 1.0 };
+    t8_mra::lu_solve (trafo_mat, permuation_vec, ret);
+
+    return ret;
+  }
+};
+
+template <typename TElement>
+class dg_basis: public dg_basis_base<TElement::Shape> {
+  using Element = TElement;
+  using Base = dg_basis_base<TElement::Shape>;
+
+  static constexpr unsigned int DIM = Element::DIM;
+  static constexpr auto Shape = TElement::Shape;
+
+  static constexpr unsigned int P_DIM = Element::P_DIM;
+  static constexpr unsigned int DOF = Element::DOF;
+  static constexpr unsigned int W_DOF = Element::W_DOF;
+
+ public:
+  dg_basis () = default;
+
+  dg_basis (int _num_quad_points, int _dunavant_rule)
+    requires (Shape == T8_ECLASS_TRIANGLE)
+    : Base (_num_quad_points, _dunavant_rule)
+  {
+  }
+};
+
+}  // namespace t8_mra
+#endif
