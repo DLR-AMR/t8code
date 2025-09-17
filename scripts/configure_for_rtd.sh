b3d967852a79484b131b905ab7c3cc34cc16ee8c
David Knapp <david.knapp@dlr.de>
2025-09-17 15:11:40 +0200

    enable vtk, netcdf and occ

--- scripts/configure_for_rtd.sh
+++ scripts/configure_for_rtd.sh
@@ -9,8 +9,8 @@ mkdir build
 # Navigate into the build directory
 cd build
 
-# Run cmake with the specified options
-cmake .. -DT8CODE_BUILD_DOCUMENTATION=ON -DT8CODE_BUILD_DOCUMENTATION_SPHINX=ON -DT8CODE_ENABLE_MPI=OFF
+
+cmake .. -DT8CODE_BUILD_DOCUMENTATION=ON -DT8CODE_BUILD_DOCUMENTATION_SPHINX=ON -DT8CODE_ENABLE_MPI=OFF -DT8CODE_ENABLE_VTK=ON -DT8CODE_ENABLE_OCC=ON -DT8CODE_ENABLE_NETCDF=ON
 
 # Return to the parent directory
 cd ..
