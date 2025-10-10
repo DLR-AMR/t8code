131a295a2e94e4a47917f263116723e520bce878
lenaploetzke <70579874+lenaploetzke@users.noreply.github.com>
2025-10-10 12:06:28 +0200

    flat vector and delete iterator

--- test/t8_unstructured_mesh/t8_gtest_unstructured_mesh.cxx
+++ test/t8_unstructured_mesh/t8_gtest_unstructured_mesh.cxx
@@ -63,11 +63,11 @@ TEST_P (t8_unstructured_mesh_test, test_iterator)
   ASSERT_TRUE (t8_forest_is_committed (forest));
 
   // --- Check default functionality. ---
-  t8_unstructured_mesh<t8_unstructured_mesh_element<>> unstructured_mesh_calculate
+  t8_unstructured_mesh<t8_unstructured_mesh_element<>> unstructured_mesh
     = t8_unstructured_mesh<t8_unstructured_mesh_element<>> (forest);
 
   // Iterate with the iterator over all unstructured mesh elements and check some functionality.
-  for (auto it = unstructured_mesh_calculate.begin (); it != unstructured_mesh_calculate.end (); ++it) {
+  for (auto it = unstructured_mesh.begin (); it != unstructured_mesh.end (); ++it) {
     EXPECT_EQ (level, it->get_level ());
     for (int coord = 0; coord < T8_ECLASS_MAX_DIM; ++coord) {
       EXPECT_GE (1, it->get_centroid ()[coord]);
@@ -82,7 +82,7 @@ TEST_P (t8_unstructured_mesh_test, test_iterator)
     }
   }
   // Test dereference operator.
-  for (auto it = unstructured_mesh_calculate.begin (); it != unstructured_mesh_calculate.end (); ++it) {
+  for (auto it = unstructured_mesh.begin (); it != unstructured_mesh.end (); ++it) {
     EXPECT_EQ (level, (*it).get_level ());
   }
 }
