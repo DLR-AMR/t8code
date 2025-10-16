6c7eddb623ef423a91549098047f332d0b039464
Adrian Kolb <kolb@eddy.rwth-aachen.de>
2025-10-16 16:23:27 +0200

    lmi_map now uses levelmultiindex-template as Key-Type

--- src/t8_mra/data/levelindex_map.hpp
+++ src/t8_mra/data/levelindex_map.hpp
@@ -14,18 +14,19 @@ namespace t8_mra
 /**
  * @brief Stores corresponding data of an adaptive grid as an vector of
  * hash_maps. Guarantees O(1) search, insertion, deletion. For performance
- * reason we use a "dense hashmap" provided by the unordered_dense-library 
+ * reason we use a "dense hashmap" provided by the unordered_dense-library
  * (see https://github.com/martinus/unordered_dense)
  *
  * The cell data is given as a levelmultiindex = (level, multiindex) describing
  * the index of a cell on a given level
  *
+ * @tparam TLmi Levelmultiindex type
  * @tparam T Datatype
  */
-template <typename T>
+template <lmi_type TLmi, typename T>
 class levelindex_map {
  public:
-  using map = ankerl::unordered_dense::map<size_t, T>;
+  using map = ankerl::unordered_dense::map<TLmi, T>;
   using Value_type = typename map::mapped_type;
 
   using iterator = typename map::iterator;
@@ -63,7 +64,6 @@ class levelindex_map {
    * @param lmi levelmultiindex
    * @param data Given data
    */
-  template <lmi_type TLmi>
   void
   insert (const TLmi& lmi, const T& data);
 
@@ -81,7 +81,6 @@ class levelindex_map {
    *
    * @param lmi levelmultiindex
    */
-  template <lmi_type TLmi>
   void
   erase (const TLmi& lmi);
 
@@ -132,7 +131,6 @@ class levelindex_map {
    * @return If lmi exists return given data otherwise
    * std::nullopt
    */
-  template <lmi_type TLmi>
   std::optional<T>
   find (const TLmi& lmi) const;
 
@@ -154,7 +152,6 @@ class levelindex_map {
    *
    * @return Does cell exist?
    */
-  template <lmi_type TLmi>
   bool
   contains (const TLmi& lmi) const;
 
@@ -187,14 +184,12 @@ class levelindex_map {
   T&
   get (unsigned int level, size_t key);  // Access data at specific level and key
   //
-  template <lmi_type TLmi>
   T&
   get (const TLmi& lmi);
 
   const T&
   get (unsigned int level, size_t key) const;  // Access data at specific level and key
 
-  template <lmi_type TLmi>
   const T&
   get (const TLmi& lmi) const;
 
@@ -203,106 +198,110 @@ class levelindex_map {
   check_level (unsigned int level) const;
 };
 
-template <typename T>
-levelindex_map<T>::levelindex_map (unsigned int _max_level): max_level (_max_level)
+template <lmi_type TLmi, typename T>
+levelindex_map<TLmi, T>::levelindex_map (unsigned int _max_level): max_level (_max_level)
 {
   level_map.resize (max_level + 1);
 }
 
-template <typename T>
+template <lmi_type TLmi, typename T>
 void
-levelindex_map<T>::insert (unsigned int level, size_t key, const T& data)
+levelindex_map<TLmi, T>::insert (unsigned int level, size_t key, const T& data)
 {
   check_level (level);
 
-  level_map[level][key] = data;
+  TLmi lmi;
+  lmi.index = key;
+  level_map[level][lmi] = data;
 }
 
-template <typename T>
-template <lmi_type TLmi>
+template <lmi_type TLmi, typename T>
 void
-levelindex_map<T>::insert (const TLmi& lmi, const T& data)
+levelindex_map<TLmi, T>::insert (const TLmi& lmi, const T& data)
 {
   insert (lmi.level (), lmi.index, data);
 }
 
-template <typename T>
+template <lmi_type TLmi, typename T>
 void
-levelindex_map<T>::erase (unsigned int level, size_t key)
+levelindex_map<TLmi, T>::erase (unsigned int level, size_t key)
 {
   check_level (level);
 
-  level_map[level].erase (key);
+  TLmi lmi;
+  lmi.index = key;
+  level_map[level].erase (lmi);
 }
 
-template <typename T>
-template <lmi_type TLmi>
+template <lmi_type TLmi, typename T>
 void
-levelindex_map<T>::erase (const TLmi& lmi)
+levelindex_map<TLmi, T>::erase (const TLmi& lmi)
 {
   erase (lmi.level (), lmi.index);
 }
 
-template <typename T>
+template <lmi_type TLmi, typename T>
 void
-levelindex_map<T>::erase (unsigned int level)
+levelindex_map<TLmi, T>::erase (unsigned int level)
 {
   check_level (level);
 
   level_map[level].clear ();
 }
 
-template <typename T>
+template <lmi_type TLmi, typename T>
 void
-levelindex_map<T>::erase_all ()
+levelindex_map<TLmi, T>::erase_all ()
 {
   for (auto& map : level_map)
     map.clear ();
 }
 
-template <typename T>
-typename levelindex_map<T>::iterator
-levelindex_map<T>::begin (unsigned int level)
+template <lmi_type TLmi, typename T>
+typename levelindex_map<TLmi, T>::iterator
+levelindex_map<TLmi, T>::begin (unsigned int level)
 {
   check_level (level);
 
   return level_map[level].begin ();
 }
 
-template <typename T>
-typename levelindex_map<T>::const_iterator
-levelindex_map<T>::begin (unsigned int level) const
+template <lmi_type TLmi, typename T>
+typename levelindex_map<TLmi, T>::const_iterator
+levelindex_map<TLmi, T>::begin (unsigned int level) const
 {
   check_level (level);
 
   return level_map[level].begin ();
 }
 
-template <typename T>
-typename levelindex_map<T>::iterator
-levelindex_map<T>::end (unsigned int level)
+template <lmi_type TLmi, typename T>
+typename levelindex_map<TLmi, T>::iterator
+levelindex_map<TLmi, T>::end (unsigned int level)
 {
   check_level (level);
 
   return level_map[level].end ();
 }
 
-template <typename T>
-typename levelindex_map<T>::const_iterator
-levelindex_map<T>::end (unsigned int level) const
+template <lmi_type TLmi, typename T>
+typename levelindex_map<TLmi, T>::const_iterator
+levelindex_map<TLmi, T>::end (unsigned int level) const
 {
   check_level (level);
 
   return level_map[level].end ();
 }
 
-template <typename T>
+template <lmi_type TLmi, typename T>
 std::optional<T>
-levelindex_map<T>::find (unsigned int level, size_t key) const
+levelindex_map<TLmi, T>::find (unsigned int level, size_t key) const
 {
   check_level (level);
 
-  const auto search = level_map[level].find (key);
+  TLmi lmi;
+  lmi.index = key;
+  const auto search = level_map[level].find (lmi);
 
   if (search != level_map[level].end ())
     return search->second;
@@ -310,34 +309,34 @@ levelindex_map<T>::find (unsigned int level, size_t key) const
   return std::nullopt;
 }
 
-template <typename T>
-template <lmi_type TLmi>
+template <lmi_type TLmi, typename T>
 std::optional<T>
-levelindex_map<T>::find (const TLmi& lmi) const
+levelindex_map<TLmi, T>::find (const TLmi& lmi) const
 {
   return find (lmi.level (), lmi.index);
 }
 
-template <typename T>
+template <lmi_type TLmi, typename T>
 bool
-levelindex_map<T>::contains (unsigned int level, size_t key) const
+levelindex_map<TLmi, T>::contains (unsigned int level, size_t key) const
 {
   check_level (level);
 
-  return level_map[level].contains (key);
+  TLmi lmi;
+  lmi.index = key;
+  return level_map[level].contains (lmi);
 }
 
-template <typename T>
-template <lmi_type TLmi>
+template <lmi_type TLmi, typename T>
 bool
-levelindex_map<T>::contains (const TLmi& lmi) const
+levelindex_map<TLmi, T>::contains (const TLmi& lmi) const
 {
   return contains (lmi.level (), lmi.index);
 }
 
-template <typename T>
+template <lmi_type TLmi, typename T>
 size_t
-levelindex_map<T>::size () const noexcept
+levelindex_map<TLmi, T>::size () const noexcept
 {
   auto res = 0u;
 
@@ -347,60 +346,62 @@ levelindex_map<T>::size () const noexcept
   return res;
 }
 
-template <typename T>
-typename levelindex_map<T>::map&
-levelindex_map<T>::operator[] (unsigned int level)
+template <lmi_type TLmi, typename T>
+typename levelindex_map<TLmi, T>::map&
+levelindex_map<TLmi, T>::operator[] (unsigned int level)
 {
   check_level (level);
 
   return level_map[level];
 }
 
-template <typename T>
-const typename levelindex_map<T>::map&
-levelindex_map<T>::operator[] (unsigned int level) const
+template <lmi_type TLmi, typename T>
+const typename levelindex_map<TLmi, T>::map&
+levelindex_map<TLmi, T>::operator[] (unsigned int level) const
 {
   check_level (level);
 
   return level_map[level];
 }
 
-template <typename T>
+template <lmi_type TLmi, typename T>
 T&
-levelindex_map<T>::get (unsigned int level, size_t key)
+levelindex_map<TLmi, T>::get (unsigned int level, size_t key)
 {
   check_level (level);
 
-  return level_map[level][key];
+  TLmi lmi;
+  lmi.index = key;
+  return level_map[level][lmi];
 }
 
-template <typename T>
-template <lmi_type TLmi>
+template <lmi_type TLmi, typename T>
 T&
-levelindex_map<T>::get (const TLmi& lmi)
+levelindex_map<TLmi, T>::get (const TLmi& lmi)
 {
   return get (lmi.level (), lmi.index);
 }
 
-template <typename T>
+template <lmi_type TLmi, typename T>
 const T&
-levelindex_map<T>::get (unsigned int level, size_t key) const
+levelindex_map<TLmi, T>::get (unsigned int level, size_t key) const
 {
   check_level (level);
-  return level_map[level].at (key);
+  TLmi lmi;
+  lmi.index = key;
+  return level_map[level].at (lmi);
 }
 
-template <typename T>
-template <lmi_type TLmi>
+template <lmi_type TLmi, typename T>
 const T&
-levelindex_map<T>::get (const TLmi& lmi) const
+levelindex_map<TLmi, T>::get (const TLmi& lmi) const
 {
   return get (lmi.level (), lmi.index);
 }
 
-template <typename T>
+template <lmi_type TLmi, typename T>
 void
-levelindex_map<T>::check_level (unsigned int level) const
+levelindex_map<TLmi, T>::check_level (unsigned int level) const
 {
 #if T8_ENABLE_DEBUG
   if (level >= level_map.size ()) {
