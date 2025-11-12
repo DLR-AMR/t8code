2c56595888ff4b83374de44e1d25961361d69a90
Johannes Holke <johannes.holke@dlr.de>
2025-11-12 15:13:55 +0100

    Start implementation of ancestor_face helper function

--- src/t8_schemes/t8_scheme_helpers.hxx
+++ src/t8_schemes/t8_scheme_helpers.hxx
@@ -63,6 +63,52 @@ class t8_scheme_helpers: public t8_crtp_basic<TUnderlyingEclassScheme> {
   {
     return TEclass;
   }
+
+  /** Given a face of an element and a level coarser than (or equal to)
+   * the element's level, return the face number
+   * of the ancestor of the element that matches the element's face. Or return -1 if
+   * no face of the ancestor matches the face.
+   * \param [in] tree_class    The eclass of the current tree.
+   * \param [in]  element    The element.
+   * \param [in]  ancestor_level A refinement level smaller than (or equal to) \a element's level.
+   * \param [in]  face    Then number of a face of \a element.
+   * \return              If \a face of \a element is a subface of a face of \a element's ancestor at level \a ancestor_level,
+   *                      the face number of this face. Otherwise -1.
+   * \note For the root element this function always returns \a face.
+   * // TODO Check this note. is it still true?
+   */
+  inline int
+  element_face_get_ancestor_face (const t8_eclass_t tree_class, const t8_element_t *element, const int ancestor_level,
+                                  const int face) const
+  {
+
+    const int element_level = scheme->element_get_level (tree_class, element);
+    T8_ASSERT (element_level >= ancestor_level);
+    if (element_level == ancestor_level) {
+      // On the same level, the return value is the face itself
+      return face;
+    }
+    // Allocate memory for a temporary element.
+    t8_element_t *parent;
+    auto underlying_impl = this->underlying ();  // Reference to the underlying scheme implementation
+    underlying_impl->element_new (1, &parent);
+    // Pointer to a temoporary element, that will move up the refinement hierarchy
+    const t8_element_t *temp_element = element;
+    int temp_face;
+    for (int ilevel = element_level; ilevel > ancestor_level; --ilevel) {
+      // Go one level up in the refinement hierarchy with the face
+      temp_face = underlying_impl->element_face_get_parent_face (temp_neigh, temp_dual_face);
+      if (temp_face == -1) {
+        // This face is not a subface of an ancestor face.
+        underlying_impl->element_destroy (1, &parent);
+        return -1;
+      }
+      underlying_impl->element_get_parent (temp_neigh, parent);
+      temp_element = parent;
+    }
+    underlying_impl->element_destroy (1, &parent);
+    return temp_face;
+  }
 };
 
 #endif /* T8_SCHEME_HELPERS_HXX */
