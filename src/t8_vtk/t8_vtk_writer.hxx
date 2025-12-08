1725210f2f2128a5c1600c8aa1123bcad0289918
David Knapp <david.knapp@dlr.de>
2025-12-08 10:21:52 +0100

    indentation

--- src/t8_vtk/t8_vtk_writer.hxx
+++ src/t8_vtk/t8_vtk_writer.hxx
@@ -392,7 +392,7 @@ class vtk_writer {
       // if we have cells. If the local bounds cannot be computed,
       // merge points will be disabled.
       merge_points = grid_get_local_bounds (grid, bounds);
-      if (!merge_points){
+      if (!merge_points) {
         t8_errorf (
           "Computation of bounding box failed. VTK merge points will be disabled. VTK output is still valid. \n");
       }
