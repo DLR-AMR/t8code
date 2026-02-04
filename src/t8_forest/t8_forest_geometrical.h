f382ff919b93d4301a56586b416601fa7967b616
Lena Ploetzke <lena.ploetzke@dlr.de>
2026-02-04 14:36:10 +0100

    typos

--- src/t8_forest/t8_forest_geometrical.h
+++ src/t8_forest/t8_forest_geometrical.h
@@ -123,7 +123,7 @@ t8_forest_element_diam (t8_forest_t forest, t8_locidx_t ltreeid, const t8_elemen
  * \param [in]      forest     The forest.
  * \param [in]      ltreeid    The forest local id of the tree in which the element is.
  * \param [in]      element    The element.
- * \return                     The diameter of the element.
+ * \return                     The volume of the element.
  * \note                       This function assumes d-linear interpolation for the
  *                             tree vertex coordinates.
  *                             \a forest must be committed when calling this function.
