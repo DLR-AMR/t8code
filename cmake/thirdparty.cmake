000aa1a59fa3b125ffc782d8bd40f0e4b8f01eae
Sandro Elsweijer <sandro.elsweijer@dlr.de>
2026-04-30 11:22:55 +0200

    rename dependencies to thirdparty

--- cmake/thirdparty.cmake
+++ cmake/thirdparty.cmake
@@ -23,40 +23,40 @@ set(FETCHCONTENT_QUIET FALSE)
 
 
 # 1. Read and parse the JSON
-file(READ "cmake/dependencies.json" DEPS_JSON)
-# 2. Get the number of elements in the "dependencies" array
-string(JSON DEPS_COUNT LENGTH "${DEPS_JSON}" "dependencies")
+file(READ "cmake/thirdparty.json" DEPS_JSON)
+# 2. Get the number of elements in the "thirdparty" array
+string(JSON DEPS_COUNT LENGTH "${DEPS_JSON}" "thirdparty")
 # 3. Loop through the array (subtract 1 because it's 0-indexed)
 math(EXPR DEPS_RANGE "${DEPS_COUNT} - 1")
 
 foreach(INDEX RANGE ${DEPS_RANGE})
     # Extract fields
-    string(JSON DEP_NAME GET "${DEPS_JSON}" "dependencies" ${INDEX} "name")
-    string(JSON DEP_CMAKE_OPTION GET "${DEPS_JSON}" "dependencies" ${INDEX} "depends_on_cmake_option")
-    string(JSON DEP_TYPE GET "${DEPS_JSON}" "dependencies" ${INDEX} "source" "type")
-    string(JSON DEP_URL  GET "${DEPS_JSON}" "dependencies" ${INDEX} "source" "url")
-    string(JSON DEP_REF  GET "${DEPS_JSON}" "dependencies" ${INDEX} "source" "ref")
-    string(JSON DEP_SHALLOW  GET "${DEPS_JSON}" "dependencies" ${INDEX} "source" "shallow")
+    string(JSON DEP_NAME GET "${DEPS_JSON}" "thirdparty" ${INDEX} "name")
+    string(JSON DEP_CMAKE_OPTION GET "${DEPS_JSON}" "thirdparty" ${INDEX} "depends_on_cmake_option")
+    string(JSON DEP_TYPE GET "${DEPS_JSON}" "thirdparty" ${INDEX} "source" "type")
+    string(JSON DEP_URL  GET "${DEPS_JSON}" "thirdparty" ${INDEX} "source" "url")
+    string(JSON DEP_REF  GET "${DEPS_JSON}" "thirdparty" ${INDEX} "source" "ref")
+    string(JSON DEP_SHALLOW  GET "${DEPS_JSON}" "thirdparty" ${INDEX} "source" "shallow")
 
     # If the DEP_CMAKE_OPTION field is non-empty, check the CMake option.
     if(NOT DEP_CMAKE_OPTION STREQUAL "")
         # If the named option variable does not exist in the CMake cache, warn and proceed.
         if(NOT DEFINED ${DEP_CMAKE_OPTION})
-            message(FATAL_ERROR "Loading dependency ${DEP_NAME} at index ${INDEX} references unknown CMake option '${DEP_CMAKE_OPTION}'. Aborting.")
+            message(FATAL_ERROR "Loading thirdparty library ${DEP_NAME} at index ${INDEX} references unknown CMake option '${DEP_CMAKE_OPTION}'. Aborting.")
         else()
-            # If the named option is defined but set to ON, skip this dependency.
+            # If the named option is defined but set to ON, skip this thirdparty library.
             if(${${DEP_CMAKE_OPTION}})
-                message(STATUS "Skipping FetchContent-step for dependency ${DEP_NAME} because CMake option '${DEP_CMAKE_OPTION}' is '${${DEP_CMAKE_OPTION}}'")
+                message(STATUS "Skipping FetchContent-step for thirdparty library ${DEP_NAME} because CMake option '${DEP_CMAKE_OPTION}' is '${${DEP_CMAKE_OPTION}}'")
                 continue()
             endif()
         endif()
     endif()
 
-    message(STATUS "Configuring dependency: ${DEP_NAME} (Type: ${DEP_TYPE}, SHALLOW: ${DEP_SHALLOW})")
+    message(STATUS "Configuring thirdparty library: ${DEP_NAME} (Type: ${DEP_TYPE}, SHALLOW: ${DEP_SHALLOW})")
 
     #If Dep_SHALLOW is not a valid boolean, set it to FALSE and print a warning.
     if(NOT DEP_SHALLOW STREQUAL "TRUE" AND NOT DEP_SHALLOW STREQUAL "FALSE")
-        message(STATUS "Invalid value for 'shallow' in dependency ${DEP_NAME}: '${DEP_SHALLOW}'. Expected 'true' or 'false'. Defaulting to 'false'.")
+        message(STATUS "Invalid value for 'shallow' in thirdparty library ${DEP_NAME}: '${DEP_SHALLOW}'. Expected 'true' or 'false'. Defaulting to 'false'.")
         set(DEP_SHALLOW FALSE)
     endif()
 
@@ -70,9 +70,17 @@ foreach(INDEX RANGE ${DEPS_RANGE})
             GIT_SHALLOW    ${DEP_SHALLOW}
         )
     else()
-        message(WARNING "Unknown dependency type '${DEP_TYPE}' for ${DEP_NAME}")
+        message(WARNING "Unknown thirdparty library type '${DEP_TYPE}' for ${DEP_NAME}")
     endif()
 
-    # 3. Populate the dependency
+    # 3. Populate the thirdparty library
     FetchContent_MakeAvailable(${DEP_NAME})
+endforeach()
+
+# Mark all cmake options starting with FETCHCONTENT as advanced.
+get_cmake_property(_cmake_vars VARIABLES)
+foreach(_var ${_cmake_vars})
+    if(_var MATCHES "^FETCHCONTENT_.*")
+	    mark_as_advanced(${_var})
+    endif()
 endforeach()
