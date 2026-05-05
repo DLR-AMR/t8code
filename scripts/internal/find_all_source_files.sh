5e525b42c06faaeacd5e877162f5f16621570abf
Sandro Elsweijer <sandro.elsweijer@dlr.de>
2026-05-05 14:41:52 +0200

    find all source files can now be called from anywhere inside the repo

--- scripts/internal/find_all_source_files.sh
+++ scripts/internal/find_all_source_files.sh
@@ -25,24 +25,18 @@
 #
 
 #
-# This script must be executed from the scripts/ folder.
+# This script must be executed from within the repository.
 #
-if [ `basename $PWD` != scripts ]
-then
-  if [ -d scripts ]
-  then
-    # The directory stack is automatically reset on script exit.
-    pushd scripts/ > /dev/null
-  else
-    echo ERROR: scripts/ directory not found.
-    exit 1
-  fi
+repo_main_dir=$(git rev-parse --show-toplevel 2>/dev/null)
+
+if [ $? -ne 0 ]; then
+  echo "ERROR: No git repository found."
+  exit 1
 fi
 
 # All valid file suffixes.
 # Separated by '|' in order to be directly used
 # as a regex in the find command.
-
 suffixes="c|cxx|h|hxx"
 
 # Find a suitable find program. Either 'find' or 'gfind'.
@@ -61,7 +55,6 @@ fi
 
 # Find all files with the appropriate suffix in the
 # src/, example/, test/, tutorials/, benchmark/, /api and /mesh_handle subfolders.
-repo_main_dir=`git rev-parse --show-toplevel`
 folders="\
 $repo_main_dir/src \
 $repo_main_dir/example \
