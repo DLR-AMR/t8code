b0cdfbb62a5260bc4604ad9c8510a7be4d61b176
Adrian Kolb <kolb@eddy.rwth-aachen.de>
2026-06-22 16:32:56 +0200

    adding class for easy tests with mra + forest

--- test/t8_mra/t8_gtest_mra_forest.hxx
+++ test/t8_mra/t8_gtest_mra_forest.hxx
@@ -47,6 +47,65 @@ struct ConfigNames
 };
 
 /* Smooth test function */
+/* (cmesh, scheme, multiscale) triple on a unit hypercube */
+template <t8_eclass TShape, int U, int P>
+class mra_example {
+ public:
+  using multiscale = t8_mra::multiscale<TShape, U, P>;
+  using levelmultiindex = typename multiscale::levelmultiindex;
+  static constexpr int DIM = t8_mra::shape_traits<TShape>::DIM;
+
+  explicit mra_example (int max_level): mra (max_level, sc_MPI_COMM_WORLD), max_level (max_level)
+  {
+    cmesh = t8_cmesh_new_hypercube (TShape, sc_MPI_COMM_WORLD, 0, 0, 0);
+    scheme = t8_scheme_new_default ();
+    t8_cmesh_ref (cmesh);
+    t8_scheme_ref (const_cast<t8_scheme *> (scheme));
+  }
+
+  ~mra_example ()
+  {
+    mra.cleanup ();
+    t8_cmesh_destroy (&cmesh);
+    t8_scheme_unref (const_cast<t8_scheme **> (&scheme));
+  }
+
+  mra_example (const mra_example &) = delete;
+
+  mra_example &
+  operator= (const mra_example &) = delete;
+
+  template <typename F>
+  void
+  init (F &&f)
+  {
+    mra.initialize_data (cmesh, scheme, max_level, std::forward<F> (f));
+  }
+
+  template <typename F>
+  void
+  init_adaptive (F &&f)
+  {
+    mra.initialize_data_adaptive (cmesh, scheme, max_level, std::forward<F> (f));
+  }
+
+  multiscale *
+  operator->()
+  {
+    return &mra;
+  }
+
+  multiscale &
+  operator* ()
+  {
+    return mra;
+  }
+
+  multiscale mra;
+  t8_cmesh_t cmesh;
+  const t8_scheme *scheme;
+  int max_level;
+};
 
 }  // namespace mra_test
 
