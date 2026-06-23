1e5b7c4e837115fefb22df9dceeaaf0e6f7dd6a3
Thomas Spenke <thomas.spenke@dlr.de>
2026-06-23 16:01:36 +0200

    Successively add lines

--- test/t8_cmesh/t8_gtest_compute_first_element.cxx
+++ test/t8_cmesh/t8_gtest_compute_first_element.cxx
@@ -206,7 +206,7 @@ TEST_P (t8_gtest_rank_times_global_num_elems_over_size, large_numbers)
 
         check_result = (rank == size) ? num_elems : check_result;
 
-        ASSERT_EQ (computed_result, check_result)
+        ASSERT_EQ (computed_result, check_result);
         // << "rank: " << rank << " num_elems: " << num_elems << " size: " << size;
 
         /* Update the result with respect to the updated rank */
