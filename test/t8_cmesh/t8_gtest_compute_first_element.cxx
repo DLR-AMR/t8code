2a3b13982b891b2db0893e0dcc5002f4065107eb
Thomas Spenke <thomas.spenke@dlr.de>
2026-06-23 10:16:31 +0200

    Try to identify error source

--- test/t8_cmesh/t8_gtest_compute_first_element.cxx
+++ test/t8_cmesh/t8_gtest_compute_first_element.cxx
@@ -185,47 +185,43 @@ TEST_P (t8_gtest_rank_times_global_num_elems_over_size, large_numbers)
 
       // -----------------------------------
       /* Cast everything into uint64_t */
-      const uint64_t process_64 = static_cast<uint64_t> (rank);
-      const uint64_t mpisize_64 = static_cast<uint64_t> (size);
+      // const uint64_t process_64 = static_cast<uint64_t> (rank);
+      // const uint64_t mpisize_64 = static_cast<uint64_t> (size);
 
-      /* Split the uint64_t */
-      const uint64_t elem_over_size = num_elems / mpisize_64;
-      const uint64_t remainder_0 = num_elems % mpisize_64;
+      // /* Split the uint64_t */
+      // const uint64_t elem_over_size = num_elems / mpisize_64;
+      // const uint64_t remainder_0 = num_elems % mpisize_64;
 
-      const uint64_t proc_over_size = process_64 / mpisize_64;
-      const uint64_t remainder_1 = process_64 % mpisize_64;
+      // const uint64_t proc_over_size = process_64 / mpisize_64;
+      // const uint64_t remainder_1 = process_64 % mpisize_64;
 
-      const uint64_t sum_0 = (elem_over_size * proc_over_size) * mpisize_64;
-      const uint64_t sum_1 = elem_over_size * (process_64 % mpisize_64);
-      const uint64_t sum_2 = proc_over_size * (num_elems % mpisize_64);
-      const uint64_t sum_3 = (remainder_0 * remainder_1) / mpisize_64;
+      // const uint64_t sum_0 = (elem_over_size * proc_over_size) * mpisize_64;
+      // const uint64_t sum_1 = elem_over_size * (process_64 % mpisize_64);
+      // const uint64_t sum_2 = proc_over_size * (num_elems % mpisize_64);
+      // const uint64_t sum_3 = (remainder_0 * remainder_1) / mpisize_64;
 
-      const uint64_t computed_result = (sum_0 + sum_1 + sum_2 + sum_3);
-
+      // const uint64_t computed_result = (sum_0 + sum_1 + sum_2 + sum_3);
+      const uint64_t computed_result = 0;
 
       // ------------------------------------------
 
+      // check_result = (rank == size) ? num_elems : check_result;
 
+      // ASSERT_EQ (computed_result, check_result) << "rank: " << rank << " num_elems: " << num_elems << " size: " << size;
 
-
-
-      check_result = (rank == size) ? num_elems : check_result;
-
-      ASSERT_EQ (computed_result, check_result) << "rank: " << rank << " num_elems: " << num_elems << " size: " << size;
-
-      /* Update the result with respect to the updated rank */
-      check_result *= rank_growth;
-      check_result += rank_growth * rank_remainder / size;
-      rank_remainder = (rank_growth * rank_remainder) % size;
-      /* Update the rank */
-      rank *= rank_growth;
-      // }
-      /* Update the result with respect to the updated number of elements. */
-      check_result_elem *= elem_growth;
-      check_result_elem += elem_growth * check_result_elem_remain / size;
-      check_result_elem_remain = (elem_growth * check_result_elem_remain) % size;
-      /* Update the number of elements */
-      num_elems *= elem_growth;
+      // /* Update the result with respect to the updated rank */
+      // check_result *= rank_growth;
+      // check_result += rank_growth * rank_remainder / size;
+      // rank_remainder = (rank_growth * rank_remainder) % size;
+      // /* Update the rank */
+      // rank *= rank_growth;
+      // // }
+      // /* Update the result with respect to the updated number of elements. */
+      // check_result_elem *= elem_growth;
+      // check_result_elem += elem_growth * check_result_elem_remain / size;
+      // check_result_elem_remain = (elem_growth * check_result_elem_remain) % size;
+      // /* Update the number of elements */
+      // num_elems *= elem_growth;
     }
     /* Update mpisize */
     size *= size_growth;
