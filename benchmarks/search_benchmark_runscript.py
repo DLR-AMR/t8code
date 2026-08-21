f6d9d2849698ad7b93cb70e210d6dc51d3b7a025
Lukas Dreyer <dreyer@ifam.uni-hannover.de>
2026-08-21 11:54:20 +0200

    change out directory structure

--- benchmarks/search_benchmark_runscript.py
+++ benchmarks/search_benchmark_runscript.py
@@ -15,8 +15,6 @@ dim=3
 #CARO has 128 =2^^7 procs per node
 log_procs_per_node=7
 
-out_directory = "out"
-
 for eclass in eclasses:
     for scheme in schemes:
         for proc_log in range(max_proc_log+1):
@@ -28,13 +26,13 @@ for eclass in eclasses:
                 for level in range(maxlevel+1,-1,-1):
                     if (max_elem_log >= dim*level - proc_log >= min_elem_log):
                         string += f"echo srun ./t8_time_search -s{scheme} -e{eclass} -l{level} -r{repititions}\n"
-                        string += f"srun -o {out_directory}/e{eclass}/s{scheme}/p{proc_log}_l{level}_%j ./t8_time_search -s{scheme} -e{eclass} -l{level} -r{repititions}\n"
+                        string += f"srun -o e{eclass}/s{scheme}/p{proc_log}_l{level}_%j ./t8_time_search -s{scheme} -e{eclass} -l{level} -r{repititions}\n"
 
-                file = Path(f"{out_directory}/e{eclass}/s{scheme}/scripts/p{proc_log}.sh")
+                file = Path(f"e{eclass}/s{scheme}/scripts/p{proc_log}.sh")
                 file.parent.mkdir(exist_ok=True, parents=True)
                 file.write_text(string)
 
 for eclass in eclasses:
     for scheme in schemes:
         for proc_log in range(max_proc_log+1):
-            subprocess.call(f"sbatch {out_directory}/e{eclass}/s{scheme}/scripts/p{proc_log}.sh", shell=True)
+            subprocess.call(f"sbatch e{eclass}/s{scheme}/scripts/p{proc_log}.sh", shell=True)
