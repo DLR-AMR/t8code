5df41a7601bbabbbd5893370fa04243297badd86
Sandro Elsweijer <sandro.elsweijer@dlr.de>
2026-09-10 10:53:45 +0200

    fix filename in doc

--- src/t8_forest/t8_forest_ghost/t8_forest_ghost_definition_helpers.cxx
+++ src/t8_forest/t8_forest_ghost/t8_forest_ghost_definition_helpers.cxx
@@ -20,7 +20,7 @@
   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
-/** \file t8_forest_ghost_definition_helpers.hxx
+/** \file t8_forest_ghost_definition_helpers.cxx
  * Implementations for t8_forest_ghost_definition_helpers.hxx
  */
 
