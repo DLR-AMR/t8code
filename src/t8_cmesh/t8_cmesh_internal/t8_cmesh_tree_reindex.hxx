2cb21a9392d29ce5b9061aff5981d1a1ea829e09
Tu Nguyen Xuan <tunguyenxuan152@googlemail.com>
2026-09-23 13:38:34 +0200

    Added docstrings in .h-file

--- src/t8_cmesh/t8_cmesh_internal/t8_cmesh_tree_reindex.hxx
+++ src/t8_cmesh/t8_cmesh_internal/t8_cmesh_tree_reindex.hxx
@@ -20,13 +20,45 @@
   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
+#ifndef T8_CMESH_REINDEX_TREES_H
+#define T8_CMESH_REINDEX_TREES_H
+
 #include <t8.h>
 #include <t8_cmesh/t8_cmesh.h>
 #include <t8_cmesh/t8_cmesh_internal/t8_cmesh_types.h>
 #include <map>
 
+/**
+ * Compute a spatially coherent reindexing of the coarse mesh trees.
+ *
+ * Constructs an auxiliary bounding box forest containing the centers of the
+ * original coarse mesh trees. The forest is adaptively refined until each
+ * leaf element contains at most one tree center. The leaves are then traversed
+ * in SFC order to assign new indices to the original trees.
+ *
+ * \param [in] cmesh  The original coarse mesh whose trees are to be reindexed.
+ * \param [in] comm   MPI communicator used to construct the auxiliary forest.
+ *
+ * \return A map from the original global tree IDs to their new SFC-based indices.
+ *
+ * \note This function only computes the reindexing and does not modify the
+ *       original coarse mesh.
+ */
+
 std::map<t8_gloidx_t, t8_gloidx_t>
 t8_cmesh_reindex_tree (t8_cmesh_t cmesh, sc_MPI_Comm comm = sc_MPI_COMM_SELF);
 
+/**
+ * Apply a tree reindexing to the coarse mesh stash in place.
+ *
+ * Updates the tree IDs stored in the stash according to the given mapping,
+ * allowing the coarse mesh to use the newly computed tree ordering.
+ *
+ * \param [in,out] stash         The coarse mesh stash to be modified.
+ * \param [in]     tree_reindex  Mapping from original global tree IDs to
+ *                              their new indices.
+ */
 void
 t8_cmesh_tree_perform_reindex_inplace (t8_stash_t &stash, const std::map<t8_gloidx_t, t8_gloidx_t> &tree_reindex);
+
+#endif /* !T8_CMESH_REINDEX_TREES_H */
